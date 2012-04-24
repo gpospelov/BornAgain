df069497ccc58d407cc7c80400a4b9d9643c08f5
Walter Van Herck <w.van.herck@fz-juelich.de>
2012-04-24 15:43:29 +0200

    Bugfix: wrong declaration of template class OutputData in ISimulation

--- Core/inc/ISimulation.h
+++ Core/inc/ISimulation.h
@@ -1,17 +1,17 @@
 #ifndef ISIMULATION_H
 #define ISIMULATION_H
 
-class OutputData;
+// class OutputData;
 
 class ISimulation
 {
 public:
     virtual ~ISimulation() {}
     void run();
-    void setOutputData(OutputData* p_data) { mp_output_data = p_data; }
+ //    void setOutputData(OutputData* p_data) { mp_output_data = p_data; }
 
 protected:
-    OutputData* mp_output_data;
+//     OutputData* mp_output_data;
 };
 
 #endif // ISIMULATION_H

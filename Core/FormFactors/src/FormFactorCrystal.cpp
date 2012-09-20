afd94aeae57e98df8c95c43bcfa369035274641c
pospelov <pospelov@fz-juelich.de>
2012-09-20 16:56:44 +0200

    Lattice back as it was, fix in DWBADiffuseSimulation with getCurrentValueOfAxis inside loop, new GISASFW_DEBUG variable in pro file

--- Core/FormFactors/src/FormFactorCrystal.cpp
+++ Core/FormFactors/src/FormFactorCrystal.cpp
@@ -46,10 +46,8 @@ complex_t FormFactorCrystal::evaluate_for_q(const cvector_t &q) const
     // calculate the used radius in function of the reciprocal lattice scale
     double radius = 1.1*m_max_rec_length;
     // retrieve nearest reciprocal lattice vectors
-//    std::vector<kvector_t> rec_vectors =
-//            m_lattice.getReciprocalLatticeVectorsWithinRadius(q_real, radius);
-    std::vector<kvector_t> rec_vectors;
-    m_lattice.getReciprocalLatticeVectorsWithinRadius(q_real, radius, rec_vectors);
+    std::vector<kvector_t> rec_vectors =
+            m_lattice.getReciprocalLatticeVectorsWithinRadius(q_real, radius);
 
     // perform convolution on these lattice vectors
     complex_t result(0.0, 0.0);

959e26d76797bdb0c095e9e0e31c10f51ddcbdb4
Walter Van Herck <w.van.herck@fz-juelich.de>
2013-02-26 17:59:32 +0100

    added some comments

--- GUI/coregui/Models/PythonScriptSampleBuilder.cpp
+++ GUI/coregui/Models/PythonScriptSampleBuilder.cpp
@@ -7,10 +7,11 @@ PythonScriptSampleBuilder::PythonScriptSampleBuilder(QDir script_path)
 
 PythonScriptSampleBuilder::~PythonScriptSampleBuilder()
 {
-
+    // release python objects
 }
 
 ISample *PythonScriptSampleBuilder::buildSample() const
 {
-
+    // use predefined function in python module to create a pyobject
+    // which is then 'extracted' to ISample* by boost::python
 }

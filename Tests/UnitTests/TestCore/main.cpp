3ad382739a480aa7b3b7970dbef904a20eb1b497
Walter Van Herck <w.van.herck@fz-juelich.de>
2013-06-06 14:45:41 +0200

    Redirected error stream programmatically during running of unit tests

--- Tests/UnitTests/TestCore/main.cpp
+++ Tests/UnitTests/TestCore/main.cpp
@@ -15,9 +15,29 @@
 #include "ParameterPoolTest.h"
 #include "RealParameterWrapperTest.h"
 
+struct ErrorStreamRedirect {
+    ErrorStreamRedirect( std::streambuf * new_buffer )
+        : old( std::cerr.rdbuf( new_buffer ) )
+    { }
+
+    ~ErrorStreamRedirect( ) {
+        std::cerr.rdbuf( old );
+    }
+
+private:
+    std::streambuf * old;
+};
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
-     return RUN_ALL_TESTS();
+
+    // redirect std::cerr stream
+    std::stringstream oss;
+    ErrorStreamRedirect redirecter( oss.rdbuf() );
+    (void)redirecter;
+
+    // run all google tests
+    return RUN_ALL_TESTS();
 }
 

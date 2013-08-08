e459c60f0c2e468bf4d31e2ba71cd4a608dce8b0
Walter Van Herck <w.van.herck@fz-juelich.de>
2013-08-08 17:08:03 +0200

    Added calculation of the 16 DWBA matrix coefficients

--- Core/FormFactors/inc/FormFactorDWBAPol.h
+++ Core/FormFactors/inc/FormFactorDWBAPol.h
@@ -44,7 +44,8 @@ public:
 
     //! Calculates and returns a polarized form factor in DWBA
     virtual Eigen::Matrix2cd evaluatePol(const cvector_t& k_i,
-            const Bin1DCVector& k_f_bin, double alpha_i, double alpha_f) const;
+            const Bin1DCVector& k_f_bin, double alpha_i, double alpha_f,
+            double phi_f) const;
 
     //! Sets magnetic reflection/transmission info for polarized DWBA
     void setRTInfo(const MagneticCoefficientsMap& magnetic_coeff_map);
@@ -59,15 +60,46 @@ public:
         mp_ambient_material = p_material;
     }
 protected:
+    const SpecularMagnetic::LayerMatrixCoeff& getOutCoeffs(double alpha_f,
+            double phi_f) const;
     void calculateTerms(const cvector_t& k_i, const Bin1DCVector& k_f_bin,
-            double alpha_i, double alpha_f) const;
+            double alpha_i, double alpha_f, double phi_f) const;
 
     Eigen::Matrix2cd getScatteringPotential(const kvector_t& k) const;
 
     MagneticCoefficientsMap *mp_magnetic_coeffs;
     const IMaterial *mp_material;
     const IMaterial *mp_ambient_material;
+    //! The following matrices each contain the four polarization conditions
+    //! (p->p, p->m, m->p, m->m)
+    //! The first two indices indicate a scattering from the 1/2 eigenstate into
+    //! the 1/2 eigenstate, while the capital indices indicate a reflection
+    //! before and/or after the scattering event (first index is in-state,
+    //! second is out-state; this also applies to the internal matrix indices)
+    mutable Eigen::Matrix2cd m_M11_S;
+    mutable Eigen::Matrix2cd m_M11_RS;
+    mutable Eigen::Matrix2cd m_M11_SR;
+    mutable Eigen::Matrix2cd m_M11_RSR;
+    mutable Eigen::Matrix2cd m_M12_S;
+    mutable Eigen::Matrix2cd m_M12_RS;
+    mutable Eigen::Matrix2cd m_M12_SR;
+    mutable Eigen::Matrix2cd m_M12_RSR;
+    mutable Eigen::Matrix2cd m_M21_S;
+    mutable Eigen::Matrix2cd m_M21_RS;
+    mutable Eigen::Matrix2cd m_M21_SR;
+    mutable Eigen::Matrix2cd m_M21_RSR;
+    mutable Eigen::Matrix2cd m_M22_S;
+    mutable Eigen::Matrix2cd m_M22_RS;
+    mutable Eigen::Matrix2cd m_M22_SR;
+    mutable Eigen::Matrix2cd m_M22_RSR;
 };
 
+inline const SpecularMagnetic::LayerMatrixCoeff& FormFactorDWBAPol::getOutCoeffs(
+        double alpha_f, double phi_f) const
+{
+    MagneticCoefficientsMap::container_phi_t &phi_coeffs =
+            (*mp_magnetic_coeffs)[alpha_f];
+    return phi_coeffs[phi_f];
+}
 
 #endif /* FORMFACTORDWBAPOL_H_ */
--- Core/FormFactors/src/FormFactorDWBAPol.cpp
+++ Core/FormFactors/src/FormFactorDWBAPol.cpp
@@ -47,21 +47,116 @@ void FormFactorDWBAPol::setRTInfo(
 }
 
 Eigen::Matrix2cd FormFactorDWBAPol::evaluatePol(const cvector_t& k_i,
-        const Bin1DCVector& k_f_bin, double alpha_i, double alpha_f) const
+        const Bin1DCVector& k_f_bin, double alpha_i, double alpha_f,
+        double phi_f) const
 {
-    calculateTerms(k_i, k_f_bin, alpha_i, alpha_f);
+    calculateTerms(k_i, k_f_bin, alpha_i, alpha_f, phi_f);
     //TODO: replace with real result!
     return Eigen::Matrix2cd::Zero();
 }
 
 void FormFactorDWBAPol::calculateTerms(const cvector_t& k_i,
-        const Bin1DCVector& k_f_bin, double alpha_i, double alpha_f) const
+        const Bin1DCVector& k_f_bin, double alpha_i, double alpha_f,
+        double phi_f) const
 {
-    (void)k_i;
-    (void)k_f_bin;
-    (void)alpha_i;
-    (void)alpha_f;
-    //TODO: here comes the bulk of the DWBA calculation
+    SpecularMagnetic::LayerMatrixCoeff in_coeff =
+            mp_magnetic_coeffs->incomingCoeff();
+    SpecularMagnetic::LayerMatrixCoeff out_coeff = getOutCoeffs(alpha_f, phi_f);
+    // the conjugated linear part of time reversal operator T
+    // (T=UK with K complex conjugate operator and U is linear)
+    Eigen::Matrix2cd time_reverse_conj = Eigen::Matrix2cd::Zero();
+    time_reverse_conj(0,1) = 1.0;
+    time_reverse_conj(1,0) = -1.0;
+    // the interaction and time reversal taken together:
+    kvector_t k_i_real(k_i.x().real(), k_i.y().real(),
+            ((complex_t)in_coeff.kz(0)).real());
+    Eigen::Matrix2cd V_eff = time_reverse_conj *
+            (mp_material->getScatteringMatrix(k_i_real) -
+                    mp_ambient_material->getScatteringMatrix(k_i_real));
+    // now each of the 16 matrix terms of the polarized DWBA is calculated:
+    // eigenmode 1 -> eigenmode 1: direct scattering
+    m_M11_S(0,0) = - out_coeff.T1min().conjugate() * V_eff * in_coeff.T1plus();
+    m_M11_S(0,1) =  out_coeff.T1plus().conjugate() * V_eff * in_coeff.T1plus();
+    m_M11_S(1,0) = - out_coeff.T1min().conjugate() * V_eff * in_coeff.T1min();
+    m_M11_S(1,1) =  out_coeff.T1plus().conjugate() * V_eff * in_coeff.T1min();
+    // eigenmode 1 -> eigenmode 1: reflection and then scattering
+    m_M11_RS(0,0) = - out_coeff.T1min().conjugate() * V_eff * in_coeff.R1plus();
+    m_M11_RS(0,1) =  out_coeff.T1plus().conjugate() * V_eff * in_coeff.R1plus();
+    m_M11_RS(1,0) = - out_coeff.T1min().conjugate() * V_eff * in_coeff.R1min();
+    m_M11_RS(1,1) =  out_coeff.T1plus().conjugate() * V_eff * in_coeff.R1min();
+    // eigenmode 1 -> eigenmode 1: scattering and then reflection
+    m_M11_SR(0,0) = - out_coeff.R1min().conjugate() * V_eff * in_coeff.T1plus();
+    m_M11_SR(0,1) =  out_coeff.R1plus().conjugate() * V_eff * in_coeff.T1plus();
+    m_M11_SR(1,0) = - out_coeff.R1min().conjugate() * V_eff * in_coeff.T1min();
+    m_M11_SR(1,1) =  out_coeff.R1plus().conjugate() * V_eff * in_coeff.T1min();
+    // eigenmode 1 -> eigenmode 1: reflection, scattering and again reflection
+    m_M11_RSR(0,0) = - out_coeff.R1min().conjugate() * V_eff * in_coeff.R1plus();
+    m_M11_RSR(0,1) =  out_coeff.R1plus().conjugate() * V_eff * in_coeff.R1plus();
+    m_M11_RSR(1,0) = - out_coeff.R1min().conjugate() * V_eff * in_coeff.R1min();
+    m_M11_RSR(1,1) =  out_coeff.R1plus().conjugate() * V_eff * in_coeff.R1min();
+
+    // eigenmode 1 -> eigenmode 2: direct scattering
+    m_M12_S(0,0) = - out_coeff.T2min().conjugate() * V_eff * in_coeff.T1plus();
+    m_M12_S(0,1) =  out_coeff.T2plus().conjugate() * V_eff * in_coeff.T1plus();
+    m_M12_S(1,0) = - out_coeff.T2min().conjugate() * V_eff * in_coeff.T1min();
+    m_M12_S(1,1) =  out_coeff.T2plus().conjugate() * V_eff * in_coeff.T1min();
+    // eigenmode 1 -> eigenmode 2: reflection and then scattering
+    m_M12_RS(0,0) = - out_coeff.T2min().conjugate() * V_eff * in_coeff.R1plus();
+    m_M12_RS(0,1) =  out_coeff.T2plus().conjugate() * V_eff * in_coeff.R1plus();
+    m_M12_RS(1,0) = - out_coeff.T2min().conjugate() * V_eff * in_coeff.R1min();
+    m_M12_RS(1,1) =  out_coeff.T2plus().conjugate() * V_eff * in_coeff.R1min();
+    // eigenmode 1 -> eigenmode 2: scattering and then reflection
+    m_M12_SR(0,0) = - out_coeff.R2min().conjugate() * V_eff * in_coeff.T1plus();
+    m_M12_SR(0,1) =  out_coeff.R2plus().conjugate() * V_eff * in_coeff.T1plus();
+    m_M12_SR(1,0) = - out_coeff.R2min().conjugate() * V_eff * in_coeff.T1min();
+    m_M12_SR(1,1) =  out_coeff.R2plus().conjugate() * V_eff * in_coeff.T1min();
+    // eigenmode 1 -> eigenmode 2: reflection, scattering and again reflection
+    m_M12_RSR(0,0) = - out_coeff.R2min().conjugate() * V_eff * in_coeff.R1plus();
+    m_M12_RSR(0,1) =  out_coeff.R2plus().conjugate() * V_eff * in_coeff.R1plus();
+    m_M12_RSR(1,0) = - out_coeff.R2min().conjugate() * V_eff * in_coeff.R1min();
+    m_M12_RSR(1,1) =  out_coeff.R2plus().conjugate() * V_eff * in_coeff.R1min();
+
+    // eigenmode 2 -> eigenmode 1: direct scattering
+    m_M21_S(0,0) = - out_coeff.T1min().conjugate() * V_eff * in_coeff.T2plus();
+    m_M21_S(0,1) =  out_coeff.T1plus().conjugate() * V_eff * in_coeff.T2plus();
+    m_M21_S(1,0) = - out_coeff.T1min().conjugate() * V_eff * in_coeff.T2min();
+    m_M21_S(1,1) =  out_coeff.T1plus().conjugate() * V_eff * in_coeff.T2min();
+    // eigenmode 2 -> eigenmode 1: reflection and then scattering
+    m_M21_RS(0,0) = - out_coeff.T1min().conjugate() * V_eff * in_coeff.R2plus();
+    m_M21_RS(0,1) =  out_coeff.T1plus().conjugate() * V_eff * in_coeff.R2plus();
+    m_M21_RS(1,0) = - out_coeff.T1min().conjugate() * V_eff * in_coeff.R2min();
+    m_M21_RS(1,1) =  out_coeff.T1plus().conjugate() * V_eff * in_coeff.R2min();
+    // eigenmode 2 -> eigenmode 1: scattering and then reflection
+    m_M21_SR(0,0) = - out_coeff.R1min().conjugate() * V_eff * in_coeff.T2plus();
+    m_M21_SR(0,1) =  out_coeff.R1plus().conjugate() * V_eff * in_coeff.T2plus();
+    m_M21_SR(1,0) = - out_coeff.R1min().conjugate() * V_eff * in_coeff.T2min();
+    m_M21_SR(1,1) =  out_coeff.R1plus().conjugate() * V_eff * in_coeff.T2min();
+    // eigenmode 2 -> eigenmode 1: reflection, scattering and again reflection
+    m_M21_RSR(0,0) = - out_coeff.R1min().conjugate() * V_eff * in_coeff.R2plus();
+    m_M21_RSR(0,1) =  out_coeff.R1plus().conjugate() * V_eff * in_coeff.R2plus();
+    m_M21_RSR(1,0) = - out_coeff.R1min().conjugate() * V_eff * in_coeff.R2min();
+    m_M21_RSR(1,1) =  out_coeff.R1plus().conjugate() * V_eff * in_coeff.R2min();
+
+    // eigenmode 2 -> eigenmode 2: direct scattering
+    m_M22_S(0,0) = - out_coeff.T2min().conjugate() * V_eff * in_coeff.T2plus();
+    m_M22_S(0,1) =  out_coeff.T2plus().conjugate() * V_eff * in_coeff.T2plus();
+    m_M22_S(1,0) = - out_coeff.T2min().conjugate() * V_eff * in_coeff.T2min();
+    m_M22_S(1,1) =  out_coeff.T2plus().conjugate() * V_eff * in_coeff.T2min();
+    // eigenmode 2 -> eigenmode 2: reflection and then scattering
+    m_M22_RS(0,0) = - out_coeff.T2min().conjugate() * V_eff * in_coeff.R2plus();
+    m_M22_RS(0,1) =  out_coeff.T2plus().conjugate() * V_eff * in_coeff.R2plus();
+    m_M22_RS(1,0) = - out_coeff.T2min().conjugate() * V_eff * in_coeff.R2min();
+    m_M22_RS(1,1) =  out_coeff.T2plus().conjugate() * V_eff * in_coeff.R2min();
+    // eigenmode 2 -> eigenmode 2: scattering and then reflection
+    m_M22_SR(0,0) = - out_coeff.R2min().conjugate() * V_eff * in_coeff.T2plus();
+    m_M22_SR(0,1) =  out_coeff.R2plus().conjugate() * V_eff * in_coeff.T2plus();
+    m_M22_SR(1,0) = - out_coeff.R2min().conjugate() * V_eff * in_coeff.T2min();
+    m_M22_SR(1,1) =  out_coeff.R2plus().conjugate() * V_eff * in_coeff.T2min();
+    // eigenmode 2 -> eigenmode 2: reflection, scattering and again reflection
+    m_M22_RSR(0,0) = - out_coeff.R2min().conjugate() * V_eff * in_coeff.R2plus();
+    m_M22_RSR(0,1) =  out_coeff.R2plus().conjugate() * V_eff * in_coeff.R2plus();
+    m_M22_RSR(1,0) = - out_coeff.R2min().conjugate() * V_eff * in_coeff.R2min();
+    m_M22_RSR(1,1) =  out_coeff.R2plus().conjugate() * V_eff * in_coeff.R2min();
 }
 
 Eigen::Matrix2cd FormFactorDWBAPol::getScatteringPotential(

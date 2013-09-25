db5ebceec4958baf29a6e20287a71913180e71fa
Walter Van Herck <w.van.herck@fz-juelich.de>
2013-09-25 17:03:33 +0200

    Refactor: moved some methods of IInterferenceFunctionStrategy to private

--- Core/Algorithms/inc/IInterferenceFunctionStrategy.h
+++ Core/Algorithms/inc/IInterferenceFunctionStrategy.h
@@ -46,27 +46,36 @@ protected:
     virtual double evaluateForList(const cvector_t& k_i,
             const Bin1DCVector& k_f_bin,
             const std::vector<complex_t> &ff_list) const=0;
+
+    //! Returns q-vector from k_i and the bin of k_f
+    cvector_t getQ(const cvector_t& k_i, const Bin1DCVector& k_f_bin) const;
+
+    SafePointerVector<FormFactorInfo> m_ff_infos; //!< form factor info
+    SafePointerVector<IInterferenceFunction> m_ifs; //!< interference functions
+    SimulationParameters m_sim_params; //!< simulation parameters
+
+private:
     //! Returns mean form factor, possibly including their position information
     complex_t meanFormFactor(const cvector_t& k_i, const Bin1DCVector& k_f_bin,
             Bin1D alpha_f_bin, bool use_position=false) const;
+
     //! Returns mean squared form factor
     double meanSquaredFormFactor(const cvector_t& k_i,
             const Bin1DCVector& k_f_bin, Bin1D alpha_f_bin) const;
+
     //! Constructs one list of evaluated form factors to be used in subsequent
     //! calculations
     void calculateFormFactorList(const cvector_t& k_i,
             const Bin1DCVector& k_f_bin, Bin1D alpha_f_bin) const;
+
     //! Constructs lists of evaluated form factors to be used in subsequent
     //! calculations
     void calculateFormFactorLists(const cvector_t& k_i,
             const Bin1DCVector& k_f_bin, Bin1D alpha_f_bin, Bin1D phi_f_bin) const;
+
     //! Clears the cached form factor lists
     void clearFormFactorLists() const;
-    //! Returns q-vector from k_i and the bin of k_f
-    cvector_t getQ(const cvector_t& k_i, const Bin1DCVector& k_f_bin) const;
-    SafePointerVector<FormFactorInfo> m_ff_infos; //!< form factor info
-    SafePointerVector<IInterferenceFunction> m_ifs; //!< interference functions
-    SimulationParameters m_sim_params; //!< simulation parameters
+
     //! cached form factor evaluations
     mutable std::vector<complex_t> m_ff00, m_ff01, m_ff10, m_ff11;
 };

e66d9a7f1ddc266872cc24c51320ac9d196f7a07
Gennady Pospelov <g.pospelov@fz-juelich.de>
2013-10-25 14:59:10 +0200

    Picture with number of lines of code

--- cmake/modules/SearchInstalledSoftware.cmake
+++ cmake/modules/SearchInstalledSoftware.cmake
@@ -25,9 +25,9 @@ if(BORNAGAIN_PYTHON OR BORNAGAIN_GUI)
     list(APPEND boost_libraries_required python)
 endif()
 find_package(Boost 1.48.0 COMPONENTS ${boost_libraries_required} REQUIRED)
-#message(STATUS "Boost_INCLUDE_DIRS: ${Boost_INCLUDE_DIRS}")
-#message(STATUS "Boost_LIBRARY_DIRS: ${Boost_LIBRARY_DIRS}")
-#message(STATUS "Boost_LIBRARIES: ${Boost_LIBRARIES}")
+message(STATUS "Boost_INCLUDE_DIRS: ${Boost_INCLUDE_DIRS}")
+message(STATUS "Boost_LIBRARY_DIRS: ${Boost_LIBRARY_DIRS}")
+message(STATUS "Boost_LIBRARIES: ${Boost_LIBRARIES}")
 
 # --- GSL ---
 find_package(GSL REQUIRED)

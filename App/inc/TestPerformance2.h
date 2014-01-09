ef99c947f732f2b1f530c1410014907c5ab34e2d
Gennady Pospelov <g.pospelov@fz-juelich.de>
2014-01-09 09:21:29 +0100

    Minor changes in TestPerformance

--- App/inc/TestPerformance2.h
+++ App/inc/TestPerformance2.h
@@ -17,20 +17,50 @@
 #define TESTPERFORMANCE2_H
 
 #include "IApplicationTest.h"
+#include "INamed.h"
 
 #include <string>
 #include <vector>
 #include <map>
 #include <iostream>
 
+class PerformanceTest;
+
 //! Run standard tests to trace changes in the performance.
 
 class TestPerformance2 : public IApplicationTest
 {
 public:
-    TestPerformance2(){}
+    TestPerformance2();
+    virtual ~TestPerformance2();
     virtual void execute();
+    std::vector<PerformanceTest *> m_tests;
+private:
+    void save_sysinfo(PerformanceTest *test);
+};
 
+
+//! class for performance measurements
+class PerformanceTest : public IApplicationTest {
+public:
+    PerformanceTest(const std::string &name, int nrepetitions)
+        : IApplicationTest(name)
+        , m_nrepetitions(nrepetitions)
+        , m_nthreads(0)
+        , m_cpu_time(0)
+        , m_real_time(0){}
+
+    virtual ~PerformanceTest(){}
+
+    virtual void execute(){}
+
+    double m_nrepetitions;
+    int m_nthreads;
+    double m_cpu_time;
+    double m_real_time;
+    std::string m_datime;
+    std::string m_hostname;
+    std::string m_sysinfo;
 };
 
 
--- App/src/TestPerformance2.cpp
+++ App/src/TestPerformance2.cpp
@@ -1,6 +1,59 @@
 #include "TestPerformance2.h"
+#include "TDatime.h"
+#include "TSystem.h"
+#include <sstream>
+
+
+TestPerformance2::TestPerformance2()
+{
+    m_tests.push_back(new PerformanceTest("isgisaxs09a",20));
+    m_tests.push_back(new PerformanceTest("isgisaxs09b",20));
+    m_tests.push_back(new PerformanceTest("mesocrystal01",1));
+
+}
+
+
+TestPerformance2::~TestPerformance2()
+{
+    for(size_t i=0; i<m_tests.size(); ++i) delete m_tests[i];
+}
+
 
 void TestPerformance2::execute()
 {
+    for(size_t i=0; i<m_tests.size(); ++i) {
+        PerformanceTest *test = m_tests[i];
+        save_sysinfo(test);
+        test->execute();
+    }
+}
+
+
+void TestPerformance2::save_sysinfo(PerformanceTest *test)
+{
+    // saving date and time
+    TDatime td;
+    test->m_datime = std::string(td.AsSQLString());
+
+    // saving host name
+    std::string hostname(gSystem->HostName());
+    // stripping host name after first '.' (somehost.jcns.frm2 -> somehost)
+    std::string::size_type pos = hostname.find_first_of('.');
+    if(pos != std::string::npos) {
+        hostname.erase(pos,hostname.size()-pos);
+    }
+    test->m_hostname = hostname;
+
+    // saving hardware information
+    SysInfo_t sys_info;
+    int status = gSystem->GetSysInfo(&sys_info);
+    if( status == -1) {
+        test->m_sysinfo = std::string("failed");
+    }else{
+        std::ostringstream os;
+        os << std::string(gSystem->GetBuildArch()) << ", "<< sys_info.fCpuSpeed << " MHz";
+        //os << ", " << 	sys_info.fL2Cache << " Kb";
+        test->m_sysinfo = os.str();
+    }
 
 }

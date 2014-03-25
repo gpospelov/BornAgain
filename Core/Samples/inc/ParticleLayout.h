9517413a09dc4c950af933fb6d3d475a36c89093
Gennady Pospelov <g.pospelov@fz-juelich.de>
2014-03-25 15:29:38 +0100

    Two forgotten file

---
+++ Core/Samples/inc/ParticleLayout.h
@@ -0,0 +1,111 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Samples/inc/ParticleLayout.h
+//! @brief     Defines class ParticleLayout.
+//!
+//! @homepage  http://apps.jcns.fz-juelich.de/BornAgain
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2013
+//! @authors   Scientific Computing Group at MLZ Garching
+//! @authors   C. Durniak, G. Pospelov, W. Van Herck, J. Wuttke
+//
+// ************************************************************************** //
+
+#ifndef PARTICLELAYOUT_H
+#define PARTICLELAYOUT_H
+
+#include "ILayout.h"
+#include "Particle.h"
+#include "ParticleInfo.h"
+#include "IInterferenceFunction.h"
+
+//! @class ParticleLayout
+//! @ingroup samples
+//! @brief Decorator class that adds particles to ISample objects
+
+class BA_CORE_API_ ParticleLayout : public ILayout
+{
+public:
+    ParticleLayout();
+    ParticleLayout(
+        Particle* p_particle, double depth=0., double abundance=1.);
+    ParticleLayout(
+        const Particle& p_particle, double depth=0., double abundance=1.);
+
+    virtual ~ParticleLayout();
+
+    virtual ParticleLayout *clone() const;
+
+    //! Returns a clone with inverted magnetic fields
+    virtual ParticleLayout *cloneInvertB() const;
+
+    //! calls the ISampleVisitor's visit method
+    virtual void accept(ISampleVisitor *visitor) const { visitor->visit(this); }
+
+    //! Adds generic particle
+    void addParticle(
+        Particle *p_particle, const Geometry::Transform3D& transform,
+        double depth=0, double abundance=1.0);
+
+    void addParticle(
+        const Particle& p_particle, const Geometry::Transform3D& transform,
+        double depth=0, double abundance=1.0);
+
+    //! Adds particle without rotation
+    void addParticle(Particle *p_particle, double depth=0.0, double abundance=1.0);
+
+    //! Adds particle without rotation
+    void addParticle(const Particle& particle, double depth=0.0, double abundance=1.0);
+
+    //! Adds particle info
+    void addParticleInfo(const ParticleInfo& info);
+
+    //! Returns number of particles
+    virtual size_t getNumberOfParticles() const { return m_particles.size(); }
+
+    //! get information about particle with index
+    virtual const ParticleInfo *getParticleInfo(size_t index) const;
+
+    //! Get abundance fraction of particle with index
+    double getAbundanceFractionOfParticle(size_t index) const;
+
+    //! Returns number of interference functions
+    virtual size_t getNumberOfInterferenceFunctions() const
+    { return m_interference_functions.size(); }
+
+    //! Returns interference functions
+    virtual SafePointerVector<IInterferenceFunction> getInterferenceFunctions() const
+    { return m_interference_functions; }
+
+    //! Adds interference function
+    void addInterferenceFunction(IInterferenceFunction* p_interference_function);
+
+    void addInterferenceFunction(const IInterferenceFunction& interference_function);
+
+    //! Returns interference function with index
+    const IInterferenceFunction* getInterferenceFunction(size_t index) const;
+
+private:
+    //! Adds particle information with simultaneous registration in parent class.
+    void addAndRegisterParticleInfo(ParticleInfo *child);
+
+    //! Adds interference function with simultaneous registration in parent class
+    void addAndRegisterInterferenceFunction(IInterferenceFunction *child);
+
+    void print(std::ostream& ostr) const;
+
+    //! Vector of the types of particles
+    SafePointerVector<ParticleInfo> m_particles;
+
+    //! Vector of interference functions
+    SafePointerVector<IInterferenceFunction> m_interference_functions;
+
+    //! Currently only a scalar interference function (instead of matrix)
+    double m_total_abundance;
+};
+
+#endif // PARTICLEDECORATION_H
+
+

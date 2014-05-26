46b74b98f46c256fc7b39e19eba929d1519f2bfa
Mohammad Mahadi Hasan <mahadi.hasan@tum.de>
2014-05-26 10:44:43 +0200

    Removed unnecessary code that were used for link buttons

--- GUI/coregui/Views/WelcomeView.cpp
+++ GUI/coregui/Views/WelcomeView.cpp
@@ -17,17 +17,14 @@ WelcomeView::WelcomeView(MainWindow *parent)
     Q_ASSERT(m_projectManager);
 
     newProjectButton = new QPushButton(tr("New Project"));
-    //newProjectButton->setIcon(QIcon(":/images/main_simulation.png"));
     newProjectButton->setMinimumWidth(150);
     newProjectButton->setMinimumHeight(50);
 
     openProjectButton = new QPushButton(tr("Open Project"));
-    //openProjectButton->setIcon(QIcon(":/images/main_simulation.png"));
     openProjectButton->setMinimumWidth(150);
     openProjectButton->setMinimumHeight(50);
 
     newUsertButton = new QPushButton(tr("New to BornAgain?"));
-    //newUsertButton->setIcon(QIcon(":/images/main_simulation.png"));
     newUsertButton->setMinimumWidth(150);
     newUsertButton->setMinimumHeight(50);
 
@@ -45,22 +42,9 @@ WelcomeView::WelcomeView(MainWindow *parent)
     buttonPanel->addLayout(buttonLayout);
 
 
-
-//    QLabel *recentLabel = new QLabel(tr("Recent Projects:"));
-//    recentLabel->setAlignment(Qt::AlignVCenter | Qt::AlignLeft);
-//    QFont font = recentLabel->font();
-//    font.setPointSize(12);
-//    recentLabel->setFont(font);
-
     m_recentProjectLayout = new QVBoxLayout;
     //generateRecentProjectList();
 
-
-
-
-//    QHBoxLayout *recentProjectPanel = new QHBoxLayout;
-//    recentProjectPanel->addLayout(recentProjectLayout);
-//    recentProjectPanel->addStretch();
 
     QGroupBox *recentProjectsBox = new QGroupBox(tr("Recent Projects:"));
     recentProjectsBox->setLayout(m_recentProjectLayout);
@@ -85,30 +69,11 @@ WelcomeView::WelcomeView(MainWindow *parent)
 
 void WelcomeView::generateRecentProjectList()
 {
-    /*m_recentProjectsMenu->clear();
-
-    bool hasRecentProjects = false;
-    foreach(QString file, m_mainWindow->getProjectManager()->getRecentProjects() ) {
-        hasRecentProjects = true;
-        qDebug() << file << QDir::toNativeSeparators(Utils::withTildeHomePath(file));
-        QAction *action = m_recentProjectsMenu->addAction(QDir::toNativeSeparators(Utils::withTildeHomePath(file)));
-        action->setData(qVariantFromValue(file));
-        connect(action, SIGNAL(triggered()), m_mainWindow, SLOT(openRecentProject()));
-
-    }
-
-    m_recentProjectsMenu->setEnabled(hasRecentProjects);
-
-    if (hasRecentProjects) {
-        m_recentProjectsMenu->addSeparator();
-        QAction *action = m_recentProjectsMenu->addAction("Clear Menu");
-        connect(action, SIGNAL(triggered()), m_mainWindow->getProjectManager(), SLOT(clearRecentProjects()));
-    }*/
 
     int count = m_projectManager->getRecentProjects().size();
     bool hasRecentProjects = false;
 
-    QCommandLinkButton *slotButtons[count];
+    //QCommandLinkButton *slotButtons[count];
     QLabel *myLabel[count];
     m_signalMapper = new QSignalMapper(this);
 
@@ -116,14 +81,14 @@ void WelcomeView::generateRecentProjectList()
     foreach(QString file, m_projectManager->getRecentProjects() ) {
         hasRecentProjects = true;
 
-        slotButtons[i] = new QCommandLinkButton;
+        /*slotButtons[i] = new QCommandLinkButton;
         slotButtons[i]->setText(Utils::withTildeHomePath(file));
         slotButtons[i]->setText("<a href=\"#\">Click Here!</a>");
         //slotButtons[i]->setDescription("Recent description");
         slotButtons[i]->setFixedHeight(35);
-        //m_signalMapper->setMapping(slotButtons[i], file);
-        //connect(slotButtons[i], SIGNAL(clicked()), m_signalMapper, SLOT (map()));
-        //m_recentProjectLayout->addWidget(slotButtons[i]);
+        m_signalMapper->setMapping(slotButtons[i], file);
+        connect(slotButtons[i], SIGNAL(clicked()), m_signalMapper, SLOT (map()));
+        m_recentProjectLayout->addWidget(slotButtons[i]);*/
 
         QFont font;
         font.setPointSize(10);
@@ -165,7 +130,7 @@ void WelcomeView::onWebLinkClicked(const QUrl &url)
 
 void WelcomeView::onNewUser()
 {
-    QUrl url(tr("http://www.google.com"));
+    QUrl url(tr("http://apps.jcns.fz-juelich.de/doku/sc/bornagain:start"));
     QDesktopServices::openUrl(url);
 }
 

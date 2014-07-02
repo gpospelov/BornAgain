18de39b990542405402fa69b23c8fced4e4e4d36
Gennady Pospelov <g.pospelov@fz-juelich.de>
2014-07-02 13:06:15 +0200

    Attempt to run QTests in headless mode.

--- Tests/UnitTests/TestGUI/TestGUI.cpp
+++ Tests/UnitTests/TestGUI/TestGUI.cpp
@@ -1,14 +1,16 @@
 #include <QtTest>
 #include <QString>
 #include <QDebug>
+#include <QCoreApplication>
 #include "TestFormFactorItems.h"
 #include "TestFTDistributionItems.h"
 #include "TestParameterizedItem.h"
 #include "TestParticleItems.h"
 
 int main(int argc, char** argv) {
-    QApplication app(argc, argv);
-    Q_UNUSED(app);
+    //QApplication app(argc, argv);
+    //Q_UNUSED(app);
+    QCoreApplication app(argc, argv);
 
     TestFormFactorItems testFormFactorItems;
     TestFTDistributionItems testFTDistributionItems;

fc06359c7fe91ecc1d2b42a1751d161688653379
Gennady Pospelov <g.pospelov@fz-juelich.de>
2014-07-15 11:05:16 +0200

    Update in OpenMPI scripts.

--- dev-tools/openmpi/pytests/job1.cmd
+++ dev-tools/openmpi/pytests/job1.cmd
@@ -4,6 +4,7 @@
 #@ class = test
 #@ node = 2
 #@ total_tasks=32
+#@ island_count=1
 #@ wall_clock_limit = 0:20:00
 #@ job_name = mytest
 #@ network.MPI = sn_all,not_shared,us
@@ -11,11 +12,13 @@
 #@ output = job$(jobid).out
 #@ error = job$(jobid).err
 #@ notification=always
+#@ notify_user=guennadi.pospelov@gmail.com
 #@ queue
 . /etc/profile
 . /etc/profile.d/modules.sh
+module load python/2.7_anaconda
+module load fftw/mpi/3.3
 export LD_LIBRARY_PATH=/home/hpc/pr87me/di29sok/software/boost_1_55_0.gcc47/lib:/home/hpc/pr87me/di29sok/development/BornAgain/installed/lib:$LD_LIBRARY_PATH
 export PYTHONPATH=/home/hpc/pr87me/di29sok/development/BornAgain/installed/lib:$PYTHONPATH
-module load python/2.7_anaconda
 poe python ompi_meso_cpp.py
 

23c6b22c59ad449356a4c8ab6ab960949021331d
Gennady Pospelov <g.pospelov@fz-juelich.de>
2014-07-15 10:50:35 +0200

    Updates in OpenMPI scripts.

--- dev-tools/openmpi/pytests/job1.cmd
+++ dev-tools/openmpi/pytests/job1.cmd
@@ -14,5 +14,8 @@
 #@ queue
 . /etc/profile
 . /etc/profile.d/modules.sh
+export LD_LIBRARY_PATH=/home/hpc/pr87me/di29sok/software/boost_1_55_0.gcc47/lib:/home/hpc/pr87me/di29sok/development/BornAgain/installed/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=/home/hpc/pr87me/di29sok/development/BornAgain/installed/lib:$PYTHONPATH
+module load python/2.7_anaconda
 poe python ompi_meso_cpp.py
 

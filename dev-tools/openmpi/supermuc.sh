6df41855a30ab95e98972db69682de5356d7a271
Gennady Pospelov <g.pospelov@fz-juelich.de>
2014-07-25 13:10:56 +0200

    Update in OpenMPI script.

--- dev-tools/openmpi/supermuc.sh
+++ dev-tools/openmpi/supermuc.sh
@@ -8,6 +8,8 @@ module load fftw/mpi/3.3
 module load gsl
 #module load python/2.7_anaconda
 
+export LD_LIBRARY_PATH=/home/hpc/pr87me/di29sok/software/boost_1_55_0.gcc47/lib:$LD_LIBRARY_PATH
+
 export FFTW3=$FFTW_BASE
 export GSL_DIR=$GSL_BASE
 export BOOST_ROOT=/home/hpc/pr87me/di29sok/software/boost_1_55_0.gcc47

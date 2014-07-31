191f0e50358ae852bde628e6cdf6570a32b04f49
Gennady Pospelov <g.pospelov@fz-juelich.de>
2014-07-31 15:30:56 +0200

    Release 0.9.7

--- Core/Samples/inc/BAVersion.h
+++ Core/Samples/inc/BAVersion.h
@@ -23,7 +23,7 @@
 namespace BornAgain {
     const int major_version_number = 0;
     const int minor_version_number = 9;
-    const int patch_version_number = 6;
+    const int patch_version_number = 7;
 
     inline int GetMajorVersionNumber() { return major_version_number; }
     inline int GetMinorVersionNumber() { return minor_version_number; }

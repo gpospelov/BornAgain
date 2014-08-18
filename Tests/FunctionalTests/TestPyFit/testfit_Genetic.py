34c594cfa6bd72e42b887d8f61d657c95fcc5dd0
Gennady Pospelov <g.pospelov@fz-juelich.de>
2014-08-18 11:38:26 +0200

    GeneticMinimizer is recovered.

--- Tests/FunctionalTests/TestPyFit/testfit_Genetic.py
+++ Tests/FunctionalTests/TestPyFit/testfit_Genetic.py
@@ -11,12 +11,22 @@ from libBornAgainCore import *
 from libBornAgainFit import *
 
 
+
 class MinimizersTest_Genetic(unittest.TestCase):
 
     def test_Genetic(self):
-        fitpars, refpars = fitting_utils.run_fit_suite("Genetic")
+        fit_suite = fitting_utils.get_fit_suite("Genetic","")
+        fit_suite.getMinimizer().getOptions().setMaxIterations(5)
+
+        fit_suite.initPrint(100 )
+        fit_suite.runFit()
+        fitting_utils.print_fit_results(fit_suite)
+
+        fitpars = fit_suite.getFitParameters().getValues()
+        refpars = fitting_utils.getReferenceParameters()
+
         for i in range(0, len(fitpars)):
-            self.assertAlmostEqual(fitpars[i], refpars[i], delta=1e-3)
+            self.assertAlmostEqual(fitpars[i], refpars[i], delta=0.1)
 
 
 if __name__ == '__main__':

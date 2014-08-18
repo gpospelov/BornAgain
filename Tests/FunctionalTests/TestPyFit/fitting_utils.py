b88bb0b4c43fa8387cca1b532a21a8a537ea7f40
Gennady Pospelov <g.pospelov@fz-juelich.de>
2014-08-18 14:01:48 +0200

    Another bug in TestPyFit tests.

--- Tests/FunctionalTests/TestPyFit/fitting_utils.py
+++ Tests/FunctionalTests/TestPyFit/fitting_utils.py
@@ -1,9 +1,9 @@
 import sys
 import os
 
-#sys.path.append(os.path.abspath(
-#                os.path.join(os.path.split(__file__)[0],
-#                '..', '..', '..', 'lib')))
+sys.path.append(os.path.abspath(
+                os.path.join(os.path.split(__file__)[0],
+                '..', '..', '..', 'lib')))
 
 from libBornAgainCore import *
 from libBornAgainFit import *

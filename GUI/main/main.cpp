dbb769241b779231ca85e91fbddfded0f020050f
Mohammad Mahadi Hasan <mahadi.hasan@tum.de>
2014-09-16 17:41:43 +0200

    added files

--- GUI/main/main.cpp
+++ GUI/main/main.cpp
@@ -1,11 +1,23 @@
+#include "../crashhandler/crashhandlersetup.h"
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
+
     QApplication a(argc, argv);
+
+    setupCrashHandler();
+
     MainWindow w;
     w.show();
     
-    return a.exec();
+
+
+    //return a.exec();
+
+    const int r = a.exec();
+    cleanupCrashHandler();
+    return r;
 }

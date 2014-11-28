c96a5c7da0f5d8d5482a5b711d36b2db3fa49c1f
Gennady Pospelov <g.pospelov@fz-juelich.de>
2014-11-28 13:47:22 +0100

    Few Python examples refactored

---
+++ Examples/python/simulation2/ex01_BasicParticles/CylindersInBA.py
@@ -0,0 +1,63 @@
+"""
+Cylinder formfactor in Born approximation (IsGISAXS example #3)
+"""
+import numpy
+import matplotlib
+import pylab
+from bornagain import *
+
+phi_min, phi_max = -2.0, 2.0
+alpha_min, alpha_max = 0.0, 2.0
+
+
+def get_sample():
+    """
+    Build and return the sample to calculate cylinder formfactor in Born approximation.
+    """
+    m_ambience = HomogeneousMaterial("Air", 0.0, 0.0)
+    m_particle = HomogeneousMaterial("Particle", 6e-4, 2e-8)
+
+    # collection of particles
+    cylinder_ff = FormFactorCylinder(5*nanometer, 5*nanometer)
+    cylinder = Particle(m_particle, cylinder_ff)
+    particle_layout = ParticleLayout()
+    particle_layout.addParticle(cylinder, 0.0, 1.0)
+
+    air_layer = Layer(m_ambience)
+    air_layer.addLayout(particle_layout)
+
+    multi_layer = MultiLayer()
+    multi_layer.addLayer(air_layer)
+    return multi_layer
+
+
+def get_simulation():
+    """
+    Create and return GISAXS simulation with beam and detector defined
+    """
+    simulation = Simulation()
+    simulation.setDetectorParameters(200, phi_min*deg, phi_max*deg, 200, alpha_min*deg, alpha_max*deg)
+    simulation.setBeamParameters(1.0*angstrom, 0.2*degree, 0.0*degree)
+    return simulation
+
+
+def run_simulation():
+    """
+    Run simulation and plot results
+    """
+    sample = get_sample()
+    simulation = get_simulation()
+    simulation.setSample(sample)
+    simulation.runSimulation()
+    result = simulation.getIntensityData().getArray() + 1  # for log scale
+
+    # showing the result
+    pylab.imshow(numpy.rot90(result, 1), norm=matplotlib.colors.LogNorm(),
+                 extent=[phi_min, phi_max, alpha_min, alpha_max], aspect='auto')
+    pylab.xlabel(r'$\phi_f$', fontsize=16)
+    pylab.ylabel(r'$\alpha_f$', fontsize=16)
+    pylab.show()
+
+
+if __name__ == '__main__':
+    run_simulation()

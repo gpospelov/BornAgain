9f56c160a1ff7e6c8e33edf84797bcc0ce7fc4d8
Gennady Pospelov <g.pospelov@fz-juelich.de>
2015-01-08 17:43:01 +0100

    Testing target_link_libraries order due to Qt5.4 and libicu problem

--- cmake/modules/SearchInstalledSoftware.cmake
+++ cmake/modules/SearchInstalledSoftware.cmake
@@ -32,7 +32,14 @@ endif()
 find_package(Boost 1.48.0 COMPONENTS ${boost_libraries_required} REQUIRED)
 message(STATUS "Boost_INCLUDE_DIRS: ${Boost_INCLUDE_DIRS}")
 message(STATUS "Boost_LIBRARY_DIRS: ${Boost_LIBRARY_DIRS}")
-message(STATUS "Boost_LIBRARIES: ${Boost_LIBRARIES}")
+
+#find_package(ICU)
+#set(Boost_LIBRARIES ${Boost_LIBRARIES} /usr/lib64/libicui18n.so)
+#set(Boost_LIBRARIES ${Boost_LIBRARIES} ${ICU_LIBRARIES})
+
+message(STATUS "ICU ${ICU_LIBRARIES}")
+#message(STATUS "Boost_LIBRARIES: ${Boost_LIBRARIES}")
+
 
 # --- GSL ---
 find_package(GSL REQUIRED)

38388a1a5f83d6ba3da70e5cb5fb7affdc0af4ba
Joachim Wuttke (l) <j.wuttke@fz-juelich.de>
2015-05-21 19:02:08 +0200

    Comments to bornplot.

--- Doc/UserManual/fig/ff2/bornplot.py
+++ Doc/UserManual/fig/ff2/bornplot.py
@@ -27,12 +27,16 @@ class Result:
         self.idx = idx
         self.data = data
         self.title = title
-        
+
 def make_plot( results, det, name, nrow=1 ):
+    '''
+    Make a plot consisting of one detector image for each Result in results,
+    plus one common color scale.
+    '''
     mpl.rcParams['image.interpolation'] = 'none'
     n = len(results)
     ncol = 1+(n-1)//nrow
-    # parameters as fraction of subfig size
+    # Parameters as fraction of subfig size.
     yskip      = 0.2 #+ncol*0.02
     bottomskip = yskip
     topskip    = yskip/2
@@ -41,29 +45,25 @@ def make_plot( results, det, name, nrow=1 ):
     rightskip  = 0.28+ncol*0.03
     xtot = ncol*1.0 + (ncol-1)*xskip + leftskip + rightskip
     ytot = nrow*1.0 + (nrow-1)*yskip + bottomskip + topskip
-    # we need parameters as fraction of total fig size
+    # We need parameters as fraction of total fig size.
     xskip      /= xtot
     leftskip   /= xtot
     rightskip  /= xtot
     yskip      /= ytot
     bottomskip /= ytot
     topskip    /= ytot
-    # set total figure dimensions
+    # Set total figure dimensions.
     ftot = 5
     fontsize = 18+36.0/(ncol+2)
+    # Create the figure 'fig' and its subplots axes ('tmp'->'axes').
     fig,tmp = plt.subplots(nrow, ncol, figsize=(ftot*xtot, ftot*ytot))
     if n>1:
         axes = tmp.flat
     else:
         axes = [tmp]
-    if False:
-        vmin = None
-        vmax = None
-        for res in results:
-            vmin = vmin and min(vmin,res.data.min()) or res.data.min()
-            vmax = vmax and max(vmin,res.data.max()) or res.data.max()
-        norm = mpl.colors.LogNorm(vmin,vmax)
+    # Always the same color scale, to facilitate comparisons between figures.
     norm = mpl.colors.LogNorm(1e-10,1)
+    # Plot the subfigures.
     for res in results:
         data = res.data
         ax = axes[res.idx]
@@ -77,13 +77,16 @@ def make_plot( results, det, name, nrow=1 ):
         if res.title!="":
             ax.set_title(res.title, fontsize=fontsize)
         ax.tick_params(axis='both', which='major', labelsize=fontsize*21/24)
+    # Adjust whitespace around and between subfigures.
     plt.subplots_adjust(wspace=xskip, hspace=yskip,
                         left=leftskip, right=1-rightskip,
                         bottom=bottomskip, top=1-topskip )
+    # Plot the color scale.
     cbar_ax = fig.add_axes([1-rightskip+0.4*xskip, bottomskip,
                             0.25*xskip, 1-bottomskip-topskip])
     cb = fig.colorbar(im, cax=cbar_ax)
     cb.set_label(r'$\left|F(q)\right|^2/V^{\,2}$', fontsize=fontsize)
+    # Output to file or display.
     plt.savefig( name+".pdf", format="pdf", bbox_inches = 'tight')
     plt.show()
 

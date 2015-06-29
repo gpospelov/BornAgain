9a09f89e975990cce872df9a048d5644e0df6ffe
Gennady Pospelov <g.pospelov@fz-juelich.de>
2015-06-29 16:49:41 +0200

    Fixes in functional test to complete merging

--- Core/StandardSamples/RipplesBuilder.cpp
+++ Core/StandardSamples/RipplesBuilder.cpp
@@ -57,7 +57,7 @@ ISample *CosineRippleBuilder::buildSample() const
     Particle ripple(particle_material, ff_ripple1);
 
     ParticleLayout particle_layout;
-    particle_layout.addParticle(ripple,0.0,1.0);
+    particle_layout.addParticle(ripple,1.0);
     InterferenceFunctionRadialParaCrystal *p_interference_function =
             new InterferenceFunctionRadialParaCrystal(m_interf_distance,
                     1e7*Units::nanometer); // peak_distance, corr_length
@@ -114,7 +114,7 @@ ISample *TriangularRippleBuilder::buildSample() const
     Particle ripple(particle_material, ff_ripple2 );
 
     ParticleLayout particle_layout;
-    particle_layout.addParticle(ripple,0.0,1.0);
+    particle_layout.addParticle(ripple,1.0);
     InterferenceFunctionRadialParaCrystal *p_interference_function =
             new InterferenceFunctionRadialParaCrystal(m_interf_distance,
                     1e7*Units::nanometer); // peak_distance, corr_length

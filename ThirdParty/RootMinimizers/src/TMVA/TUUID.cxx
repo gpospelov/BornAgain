b21a93abaf8ccac2acaa8b11b4ceebb803d71e07
Gennady Pospelov <g.pospelov@fz-juelich.de>
2015-09-02 15:12:50 +0200

    UUID from boost is used for Genetic minimizer

--- ThirdParty/RootMinimizers/src/TMVA/TUUID.cxx
+++ ThirdParty/RootMinimizers/src/TMVA/TUUID.cxx
@@ -1,14 +1,22 @@
 #include "TMVA/TUUID.h"
 #include <string.h>
 
+#include <iostream>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+//#include <boost/uuid/uuid_io.hpp>
+
 TUUID::TUUID() : fTimeLow(0)
 {
 
 }
 
+// ROOT's implementation is removed in the favor of boost implementation
 void TUUID::GetUUID(UChar_t uuid[16]) const
 {
-   // Return uuid in specified buffer (16 byte = 128 bits).
-
-   memcpy(uuid, &fTimeLow, 16);
+//   memcpy(uuid, fTimeLow, 16);
+   boost::uuids::random_generator generator;
+   boost::uuids::uuid boost_uuid = generator();
+   memcpy(uuid, boost_uuid.begin(), 16);
+//   std::cout << boost_uuid << "aaa" << uuid[0] << std::endl;
 }

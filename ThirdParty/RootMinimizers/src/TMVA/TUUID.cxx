57efd37247676269a5dd7536dcc9f4247b44280f
Gennady Pospelov <g.pospelov@fz-juelich.de>
2015-09-02 13:22:51 +0200

    Playing with TUUID

--- ThirdParty/RootMinimizers/src/TMVA/TUUID.cxx
+++ ThirdParty/RootMinimizers/src/TMVA/TUUID.cxx
@@ -1,6 +1,11 @@
 #include "TMVA/TUUID.h"
 #include <string.h>
 
+TUUID::TUUID() : fTimeLow(0)
+{
+
+}
+
 void TUUID::GetUUID(UChar_t uuid[16]) const
 {
    // Return uuid in specified buffer (16 byte = 128 bits).

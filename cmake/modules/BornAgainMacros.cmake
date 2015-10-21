e2ede1df4845069b49b61dec792724e8fcc5c474
Gennady Pospelov <g.pospelov@fz-juelich.de>
2015-10-21 17:07:45 +0200

    Cleanup in cmake macros

--- cmake/modules/BornAgainMacros.cmake
+++ cmake/modules/BornAgainMacros.cmake
@@ -1,56 +1,6 @@
 # --- Collection of scripts for BornAgain CMake infrastructure
 
 
-
-# --- end of the version setting ---
-
-#set(lib lib)
-#set(bin bin)
-
-#execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_BINARY_DIR}/lib)
-#execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_BINARY_DIR}/bin)
-
-##set(BORNAGAIN_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
-#set (CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
-#set (CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
-#set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG ${CMAKE_BINARY_DIR}/bin)
-#set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE ${CMAKE_BINARY_DIR}/bin)
-
-##set(BORNAGAIN_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
-##set (CMAKE_LIBRARY_OUTPUT_DIRECTORY ${BORNAGAIN_RUNTIME_OUTPUT_DIRECTORY})
-##set (CMAKE_RUNTIME_OUTPUT_DIRECTORY ${BORNAGAIN_RUNTIME_OUTPUT_DIRECTORY})
-##set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG ${BORNAGAIN_RUNTIME_OUTPUT_DIRECTORY})
-##set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE ${BORNAGAIN_RUNTIME_OUTPUT_DIRECTORY})
-##set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
-##set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
-
-
-#if(WIN32)
-#  set(ssuffix .bat)
-#  set(scomment rem)
-#  set(libprefix lib)
-#  set(ld_library_path PATH)
-#  set(libsuffix .dll)
-#  set(runtimedir bin)
-#  #set(CMAKE_FIND_LIBRARY_SUFFIXES ".a;.lib;.dll") # doesn't work anyway for MSVC generator
-#
-#elseif(APPLE)
-#  set(ld_library_path DYLD_LIBRARY_PATH)
-#  set(ssuffix .csh)
-#  set(scomment \#)
-#  set(libprefix lib)
-#  set(libsuffix .so)
-#  set(runtimedir lib)
-#else()
-#  set(ld_library_path LD_LIBRARY_PATH)
-#  set(ssuffix .csh)
-#  set(scomment \#)
-#  set(libprefix lib)
-#  set(libsuffix .so)
-#  set(runtimedir lib)
-#endif()
-
-
 # Returns the list of unique executables from the list of functional tests
 # Usage:
 # set(list_of_tests "exe1/arg1" "exe1/arg2" "exe1/arg3" "exe2" "exe4/arg1")
@@ -144,52 +94,6 @@ function (get_filename_component)
   endif ()
   set (${VAR} "${${VAR}}" PARENT_SCOPE)
 endfunction ()
-
-
-
-# -----------------------------------------------------------------------------
-# to disable in-source builds
-# -----------------------------------------------------------------------------
-#function(AssureOutOfSourceBuilds)
-
-#    # make sure the user doesn't play dirty with symlinks
-#    get_filename_component(srcdir "${CMAKE_SOURCE_DIR}" REALPATH)
-#    get_filename_component(bindir "${CMAKE_BINARY_DIR}" REALPATH)
-
-#    # disallow in-source builds
-#    message("XXX ${srcdir} ${bindir} "${CMAKE_SOURCE_DIR}" "${CMAKE_BINARY_DIR}" ")
-
-#    if(${srcdir} STREQUAL ${bindir})
-#        message("######################################################")
-#        message("You are attempting to build in your Source Directory.")
-#        message("You must run cmake from a build directory.")
-#        message("######################################################")
-
-
-#        # attempt to remove cache and cache files... this actually fails to work,
-#        # but no hurt trying incase it starts working..
-#        file(REMOVE_RECURSE "${CMAKE_SOURCE_DIR}/CMakeCache.txt" "${CMAKE_SOURCE_DIR}/CMakeFiles")
-
-#        message(FATAL_ERROR "In-source builds are forbidden!")
-#    endif()
-
-#    # check for polluted source tree
-
-#    if(EXISTS ${CMAKE_SOURCE_DIR}/CMakeCache.txt OR EXISTS
-#        ${CMAKE_SOURCE_DIR}/CMakeFiles)
-
-
-#        message("################################################################")
-#        message( "Found results from an in-source build in your sourcedirectory.")
-#        message("################################################################")
-
-#        # attempt to remove cache and cache files...
-#        file(REMOVE_RECURSE "${CMAKE_SOURCE_DIR}/CMakeCache.txt" "${CMAKE_SOURCE_DIR}/CMakeFiles")
-
-#        message(FATAL_ERROR "Source Directory Cleaned, please rerun CMake.")
-#  endif()
-#endfunction()
-
 
 
 function(ValidatePythonIntstallation)

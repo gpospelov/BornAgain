5f9d7e4559661c867cbc7f777aa62377d1ecc3ea
Gennady Pospelov <g.pospelov@fz-juelich.de>
2015-11-02 12:41:44 +0100

    Cmake: plot_utils.py are placed to separate file

--- dev-tools/python-setup/bornagain/plot_utils.py
+++ dev-tools/python-setup/bornagain/plot_utils.py
@@ -1,12 +1,6 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(
-                os.path.join(os.path.split(__file__)[0],
-                '..')))
-
-from libBornAgainCore import *
-from libBornAgainFit import *
+'''
+BornAgain plot utils
+'''
 
 try:
     import matplotlib

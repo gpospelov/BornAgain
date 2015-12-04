aa5e613c2c87076bbaf55d86b63db2bd1982213e
Gennady Pospelov <g.pospelov@fz-juelich.de>
2015-12-04 17:14:38 +0100

    Final cleanup in OS X fix bundle machinery

--- cmake/modules/BornAgainMacros.cmake
+++ cmake/modules/BornAgainMacros.cmake
@@ -153,7 +153,7 @@ print(s.get_config_var('LDVERSION') or s.get_config_var('VERSION'));
         endif()
 
         if(PYTHONLIBS_FOUND)
-            if(NOT PYTHON_VERSION_STRING STREQUAL PYTHONLIBS_VERSION_STRING)
+            #if(NOT PYTHON_VERSION_STRING STREQUAL PYTHONLIBS_VERSION_STRING)
                 message(STATUS "---> PYTHON_VERSION_STRING ${PYTHON_VERSION_STRING} differs from PYTHONLIBS_VERSION_STRING ${PYTHONLIBS_VERSION_STRING}")
                 if(APPLE)
                     set(ALT_PYTHON_LIBRARIES "${ALT_PYTHON_PREFIX}/lib/libpython${ALT_PYTHON_LIBRARY_SUFFIX}.dylib")
@@ -163,7 +163,7 @@ print(s.get_config_var('LDVERSION') or s.get_config_var('VERSION'));
                     message(STATUS "---> There is inconcistency between versions of interpreter and library. Don't know how to handle, compilation might fail.")
                 endif()
 
-            endif()
+            #endif()
         endif()
 
         if(NOT PYTHONLIBS_FOUND)
--- cmake/scripts/fix_apple_bundle.py
+++ cmake/scripts/fix_apple_bundle.py
@@ -1,3 +1,6 @@
+"""
+Script to fix all dependencies in OS X bundle. Runs automatically when -DBORNAGAIN_APPLE_BUNDLE
+"""
 import os
 import sys
 import platform
@@ -16,8 +19,6 @@ BUNDLE_DIR = ""
 def set_bundle_dir(bundle_dir):
     global BUNDLE_DIR
     BUNDLE_DIR = bundle_dir
-    if os.path.exists(bundle_frameworks_path()):
-        shutil.rmtree(bundle_frameworks_path())
 
 
 def bundle_dir():
@@ -74,43 +75,75 @@ def copy_file_to_dir(file_name, dest_dir):
 
 
 def otool(filename):
+    """
+    Parses dependencies of given binary file
+    """
     p = subprocess.Popen(['otool', '-XL', filename], stdout=subprocess.PIPE)
-    return iter(p.stdout.readline, b'')
+    # return iter(p.stdout.readline, b'')
+    for line in iter(p.stdout.readline, b''):
+        str = line.strip().split()[0]
+        yield str
 
 
 def setId(filename, newId):
+    """
+    Sets new id for binary file
+    """
     p = subprocess.Popen(['install_name_tool', '-id', newId, filename], stdout=subprocess.PIPE)
     p.communicate()
     return
 
 
-def setDependency(filename, old, new):
+def fixDependency(filename, old, new):
+    """
+    Replaces old dependency with new one for given binary file
+    """
     p = subprocess.Popen(['install_name_tool', '-change', old, new, filename], stdout=subprocess.PIPE)
     p.communicate()
     return
 
 
-def analyse_dependency(dependency_string):
+def is_python_framework_dependency(dependency):
     """
-    Analyse dependency line obtained from 'otool' command. Returns library full path file name
-    if it is a real third party dependency. Returns None if it is system file or rpath link.
+    Returns True if this dependecy is python library
     """
-    file_name = dependency_string.strip().split()[0]
-    if not os.path.exists(file_name):
-        return None
+    if not "boost" in dependency.lower():
+        if "python" in dependency.lower():
+            return True
+    return False
+
+
+def is_to_bundle_dependency(dependency):
+    """
+    Returns True if this dependency should be moved to the bundle
+    """
+    if not os.path.exists(dependency):
+        return False
     non_dependency_patterns = ['@', '/usr/lib', '/System']
     for pat in non_dependency_patterns:
-        if file_name.startswith(pat):
-            return None
-    return file_name
+        if dependency.startswith(pat):
+            return False
+
+    if is_python_framework_dependency(dependency):
+        return False
+
+    return True
+
+
+def get_special_dependency_id(dependency):
+    """
+    Returns libId if this dependency requires special attention (i.e. Python framework)
+    """
+    if is_python_framework_dependency(dependency):
+        return "@rpath/" + bundle_python_library()
+    return None
 
 
 def get_python_library_location():
     """
     Returns location of Python library. The library is deduced from interpreter itself
     """
-    for line in otool(sys.executable):
-        dependency = line.strip().split()[0]
+    for dependency in otool(sys.executable):
         if os.path.exists(dependency) and "Python.framework" in dependency:
             return dependency
     return None
@@ -140,20 +173,26 @@ def copy_python_framework():
 def copy_qt_libraries():
     print "--> Copying Qt libraries"
     libs = ['QtCore', 'QtDBus', 'QtDesigner', 'QtGui', 'QtPrintSupport', 'QtWidgets', 'QtXml', 'QtSvg']
+    print "   ",
     for libname in libs:
+        print libname,
         libpath = os.path.join(libname+".framework", "Versions", "5")
         srcfile = os.path.join(qtlibs_path(), libpath, libname)
         dstdir = os.path.join(bundle_frameworks_path(), libpath)
         copy_file_to_dir(srcfile, dstdir)
+    print
 
 
 def copy_qt_plugins():
     print "--> Copying Qt plugins"
     plugins = ['platforms/libqcocoa.dylib', 'iconengines/libqsvgicon.dylib']
+    print "   ",
     for name in plugins:
+        print name,
         srcfile = os.path.join(qtplugins_path(), name)
         dstdir = os.path.join(bundle_plugins_path(), os.path.dirname(name))
         copy_file_to_dir(srcfile, dstdir)
+    print
 
 
 def process_dependency(dependency):
@@ -165,7 +204,6 @@ def process_dependency(dependency):
     libId = "@rpath/" + libname
     new_location = os.path.join(bundle_frameworks_path(), libname)
     if os.path.exists(new_location):
-        print "skipping dependency", dependency
         return libId, None
     print "copying dependency", dependency
     copy_file_to_dir(dependency, bundle_frameworks_path())
@@ -174,30 +212,33 @@ def process_dependency(dependency):
 
 
 def walk_through_dependencies(file_name):
-    for line in otool(file_name):
-        dependency = analyse_dependency(line)
-        if dependency:
+    for dependency in otool(file_name):
+        if is_to_bundle_dependency(dependency):
             libId, new_location = process_dependency(dependency)
-            setDependency(file_name, dependency, libId)
+            fixDependency(file_name, dependency, libId)
             if new_location:
                 walk_through_dependencies(new_location)
-
-            # print "         dependency:", file_name, dependency, os.path.basename(dependency)
-            # dependency_list.append(dependency)
-            # get_file_dependencies(dependency, dependency_list)
+        else:
+            libId = get_special_dependency_id(dependency)
+            if libId:
+                fixDependency(file_name, dependency, libId)
 
 
 def copy_dependencies():
-    print "--> collecting dependencies"
+    print "--> Copying third party dependencies"
     for binfile in iter(bornagain_binaries()):
         walk_through_dependencies(binfile)
 
 
 def fix_apple_bundle():
+    print '-'*80
+    print "Fixing OS X bundle at '{0}'".format(bundle_dir())
+    print '-'*80
     copy_python_framework()
-    # copy_qt_libraries()
-    # copy_qt_plugins()
-    # copy_dependencies()
+    copy_qt_libraries()
+    copy_qt_plugins()
+    copy_dependencies()
+    print "Done!"
 
 
 if __name__ == '__main__':

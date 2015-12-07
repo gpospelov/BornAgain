1460e43c401fccb80ce612f5a392d42e7a87f11e
Gennady Pospelov <g.pospelov@fz-juelich.de>
2015-12-07 18:32:35 +0100

    Fix in vagrant mavericks-dev

--- dev-tools/deployment/mavericks-dev/build_bornagain.sh
+++ dev-tools/deployment/mavericks-dev/build_bornagain.sh
@@ -3,11 +3,13 @@
 export PATH=/usr/local/bin:/usr/local/opt/qt5/bin/:$PATH
 rm -r -f /Users/vagrant/build; mkdir -p /Users/vagrant/build; cd /Users/vagrant/build;
 
-git clone git://apps.jcns.fz-juelich.de/BornAgain.git
+git clone -b develop git://apps.jcns.fz-juelich.de/BornAgain.git
 mkdir BornAgain-build
 cd BornAgain-build
 export ANACONDA=/Users/vagrant/anaconda2
 cmake -DPYTHON_LIBRARY=$ANACONDA/lib/libpython2.7.dylib -DPYTHON_EXECUTABLE=$ANACONDA/bin/python2.7 -DBORNAGAIN_APPLE_BUNDLE=ON -DCMAKE_PREFIX_PATH=/usr/local ../BornAgain
 make -j4
 cpack -V
-cp *.dmg /vagrant/.
+#cp *.dmg /vagrant/.
+scp *.dmg scg@scgmini:./deployment/results/.
+

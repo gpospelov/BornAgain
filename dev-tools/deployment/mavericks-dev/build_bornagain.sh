bd183258736a99296226e31d468b73be85f36fd6
Gennady Pospelov <g.pospelov@fz-juelich.de>
2015-12-07 12:30:52 +0100

    Updates in Vagrantfile gpt mavericks-dev box

--- dev-tools/deployment/mavericks-dev/build_bornagain.sh
+++ dev-tools/deployment/mavericks-dev/build_bornagain.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 export PATH=/usr/local/bin:/usr/local/opt/qt5/bin/:$PATH
-rm -r -f ~/build; mkdir -p build; cd ~/build;
+rm -r -f /Users/vagrant/build; mkdir -p /Users/vagrant/build; cd /Users/vagrant/build;
 
 git clone git://apps.jcns.fz-juelich.de/BornAgain.git
 mkdir BornAgain-build
 cd BornAgain-build
-cmake ../BornAgain
+cmake -DBORNAGAIN_APPLE_BUNDLE=ON ../BornAgain
 make -j4
+cp *.dmg /vagrant/.

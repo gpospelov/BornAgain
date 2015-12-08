d98dfe66e6909fd3f3f3c18abd0ab8c07efbddd7
Gennady Pospelov <g.pospelov@fz-juelich.de>
2015-12-08 12:34:28 +0100

    Fixes in vagrant mavericks-dev build configuration to switch to non-priveleged build owner

--- dev-tools/deployment/mavericks-dev/build_bornagain.sh
+++ dev-tools/deployment/mavericks-dev/build_bornagain.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
+
 export QTDIR=/usr/local/opt/qt5
 export PATH=/usr/local/bin:$QTDIR/bin/:$PATH
 export WORKDIR=/Users/vagrant/build
 rm -r -f $WORKDIR; mkdir -p $WORKDIR; cd $WORKDIR;
 
-git clone -b develop git://apps.jcns.fz-juelich.de/BornAgain.git
+#git clone -b develop git://apps.jcns.fz-juelich.de/BornAgain.git
+git clone -b VagrantOSX git://apps.jcns.fz-juelich.de/BornAgain.git
 mkdir BornAgain-build
 cd BornAgain-build
 export ANACONDA=/Users/vagrant/anaconda2
 cmake -DPYTHON_LIBRARY=$ANACONDA/lib/libpython2.7.dylib -DPYTHON_EXECUTABLE=$ANACONDA/bin/python2.7 -DBORNAGAIN_APPLE_BUNDLE=ON -DCMAKE_PREFIX_PATH=/usr/local ../BornAgain
 make -j4
 cpack -V
-#cp *.dmg /vagrant/.
-scp -i /Users/vagrant/.ssh/id_rsa *.dmg scg@scgmini:./deployment/results/.
+
+echo 'Copying dmg installer back to build server'
+echo 'scp /Users/vagrant/build/BornAgain-build/*.dmg scg@scgmini:./deployment/results/.'
+scp /Users/vagrant/build/BornAgain-build/*.dmg scg@scgmini:./deployment/results/.
 

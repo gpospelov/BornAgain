4d91a92865371e89bfe80b5f4e9be6c48ef2ffe8
Gennady Pospelov <g.pospelov@fz-juelich.de>
2015-12-11 16:02:09 +0100

    Mainwindow returned to initial state (WelcomeView is shown by default)

--- GUI/coregui/mainwindow/mainwindow.cpp
+++ GUI/coregui/mainwindow/mainwindow.cpp
@@ -134,7 +134,7 @@ MainWindow::MainWindow(QWidget *parent)
     //m_tabWidget->insertTab(FitViewTab, m_fitView, QIcon(":/images/main_simulation.png"), "Fit");
     //m_tabWidget->insertTab(FIT_VIEW, new TestView(this), QIcon(":/images/main_simulation.png"), "Test");
 
-    m_tabWidget->setCurrentIndex(INSTRUMENT);
+    m_tabWidget->setCurrentIndex(WELCOME);
 
     m_progressBar = new Manhattan::ProgressBar(this);
     m_tabWidget->addBottomCornerWidget(m_progressBar);
@@ -151,7 +151,7 @@ MainWindow::MainWindow(QWidget *parent)
 
     m_projectManager->createNewProject();
 
-    testGUIObjectBuilder();
+//    testGUIObjectBuilder();
 }
 
 MainWindow::~MainWindow()

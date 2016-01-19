ae8abc0d08ea9579c740cbc24510ec6913c71cff
David Li <dawei.li@gmx.net>
2016-01-19 15:22:41 +0100

    preparations

--- GUI/coregui/mainwindow/mainwindow.h
+++ GUI/coregui/mainwindow/mainwindow.h
@@ -53,7 +53,7 @@ class BA_CORE_API_ MainWindow : public Manhattan::FancyMainWindow
     Q_OBJECT
 
 public:
-    enum ETabViewId { WELCOME, INSTRUMENT, SAMPLE, SIMULATION, JOB, TEST_VIEW};
+    enum ETabViewId { WELCOME, INSTRUMENT, SAMPLE, SIMULATION, JOB, TEST_VIEW, FIT_VIEW};
 
     explicit MainWindow(QWidget *parent = 0);
     virtual ~MainWindow();

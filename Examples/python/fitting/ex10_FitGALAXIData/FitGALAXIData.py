f15dbc331d86b2b53abbcb3be0584998ce3b8578
Gennady Pospelov <g.pospelov@fz-juelich.de>
2016-02-09 13:15:30 +0100

    Updates in FitGalaxy example while trying to improve exemplary fit results

--- Examples/python/fitting/ex10_FitGALAXIData/FitGALAXIData.py
+++ Examples/python/fitting/ex10_FitGALAXIData/FitGALAXIData.py
@@ -1,5 +1,5 @@
 """
-
+Real life example: experiment at GALAXY
 """
 import matplotlib
 from matplotlib import pyplot as plt
@@ -11,27 +11,6 @@ from SampleBuilder import MySampleBuilder
 
 wavelength = 1.34*ba.angstrom
 alpha_i = 0.463*ba.degree
-
-
-
-def plot_as_colormap(hist, zmin=None, zmax=None):
-    """
-    Simple plot of intensity data as color map
-    """
-    if not zmin:
-        zmin = 1.0
-
-    if not zmax:
-        zmax = hist.getMaximum()
-
-    im = plt.imshow(hist.getArray(),
-                    norm=matplotlib.colors.LogNorm(zmin, zmax),
-                    extent=[hist.getXmin(), hist.getXmax(), hist.getYmin(), hist.getYmax()],
-                    aspect='auto')
-    cb = plt.colorbar(im, pad = 0.025)
-    plt.xlabel(r'$\phi_f ^{\circ}$', fontsize=16)
-    plt.ylabel(r'$\alpha_f ^{\circ}$', fontsize=16)
-
 
 
 def create_detector():
@@ -59,6 +38,8 @@ def create_simulation():
     simulation.setDetector(create_detector())
     simulation.setBeamParameters(wavelength, alpha_i, 0.0)
     simulation.setBeamIntensity(1.2e7)
+    # mask on reflected beam
+    simulation.addMask(ba.Rectangle(101.9, 82.1, 103.7, 85.2), True)
     return simulation
 
 
@@ -68,10 +49,7 @@ def load_real_data(hist, filename="galaxi_data.tif.gz"):
     Returns cropped version of it, which represent the area we are interested in.
     """
     hist.load(filename)
-    # return ba.IntensityDataIOFactory.readIntensityData(filename)
-    # return hist
-    return hist.crop(104.0, 70.0, 120.0, 92.)
-    # return hist.crop(85.0, 64.0, 120.0, 92.)
+    return hist.crop(85.0, 70.0, 120.0, 92.)
 
 
 def run_fitting():
@@ -81,7 +59,6 @@ def run_fitting():
 
     real_data = load_real_data(simulation.getIntensityData())
 
-
     fit_suite = ba.FitSuite()
     draw_observer = ba.DefaultFitObserver(draw_every_nth=10)
     fit_suite.attachObserver(draw_observer)
@@ -89,25 +66,21 @@ def run_fitting():
     fit_suite.addSimulationAndRealData(simulation, real_data)
 
     # setting fitting parameters with starting values
-    fit_suite.addFitParameter("*radius", 5.5*ba.nanometer, ba.AttLimits.limited(4.0, 10.0), 0.1*ba.nanometer)
-    fit_suite.addFitParameter("*sigma", 0.4, ba.AttLimits.limited(0.1, 0.7), 0.01*ba.nanometer)
-    fit_suite.addFitParameter("*distance", 53.*ba.nanometer, ba.AttLimits.limited(20, 70), 0.1*ba.nanometer)
+    fit_suite.addFitParameter("*radius", 4.9*ba.nanometer, ba.AttLimits.limited(4.0, 6.0), 0.1*ba.nanometer)
+    fit_suite.addFitParameter("*sigma", 0.5, ba.AttLimits.limited(0.2, 0.8), 0.01*ba.nanometer)
+    fit_suite.addFitParameter("*distance", 47.*ba.nanometer, ba.AttLimits.limited(20, 70), 0.1*ba.nanometer)
 
-    # strategy1 = ba.FitStrategyAdjustMinimizer("Genetic")
-    # strategy1.getMinimizerOptions().setMaxIterations(10)
-    # fit_suite.addFitStrategy(strategy1)
-    #
-    # # Second fit strategy will use another algorithm. It will use best parameters found from previous minimization round.
-    # strategy2 = ba.FitStrategyAdjustMinimizer("Minuit2", "Migrad")
-    # fit_suite.addFitStrategy(strategy2)
+    strategy1 = ba.FitStrategyAdjustMinimizer("Genetic")
+    strategy1.getMinimizerOptions().setMaxIterations(3)
+    fit_suite.addFitStrategy(strategy1)
+
+    # Second fit strategy will use another algorithm. It will use best parameters found from previous minimization round.
+    strategy2 = ba.FitStrategyAdjustMinimizer("Minuit2", "Migrad")
+    fit_suite.addFitStrategy(strategy2)
 
     # running fit
     fit_suite.runFit()
 
-
-
-
-    plot_as_colormap(real_data, zmax=5e3)
     plt.show()
 
 if __name__ == '__main__':

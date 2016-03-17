2e45c5c7fc29ce0f5d00c4e68e3f20fee9104fc7
Walter Van Herck <w.van.herck@fz-juelich.de>
2016-03-17 14:35:23 +0100

    Fix cmake file after removal of inc/ and src/ folders

--- cmake/modules/SetUpWindows.cmake
+++ cmake/modules/SetUpWindows.cmake
@@ -4,5 +4,5 @@ set(BORNAGAIN_PLATFORM win32)
 add_definitions(-DEIGEN_DONT_ALIGN_STATICALLY=1)
 message(STATUS "Disabling static align for Eigen")
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP -nologo /EHsc /D_USE_MATH_DEFINES -I${CMAKE_SOURCE_DIR}/Core/Tools/inc -FIw32pragma.h -FIWinDllMacros.h")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP -nologo /EHsc /D_USE_MATH_DEFINES -I${CMAKE_SOURCE_DIR}/Core/Tools -FIw32pragma.h -FIWinDllMacros.h")
 set(CMAKE_SHARED_LINKER_FLAGS "/NODEFAULTLIB:libcmtd.lib")

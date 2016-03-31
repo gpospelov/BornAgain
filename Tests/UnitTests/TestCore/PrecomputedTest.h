5ba64b5cb9ccb26de361b09ddf8da8c2f1cc89ba
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2016-03-31 11:19:43 +0200

    Test of precomputed factorial vs tgamma outcommented because tgamma is too unprecise under glibc 2.12. Instead, test 150! vs value from Wolfram Alpha.

--- Tests/UnitTests/TestCore/PrecomputedTest.h
+++ Tests/UnitTests/TestCore/PrecomputedTest.h
@@ -19,8 +19,12 @@ TEST_F(PrecomputedTest, Factorial)
     EXPECT_DOUBLE_EQ(precomputed.factorial[1], 1.);
     EXPECT_DOUBLE_EQ(precomputed.factorial[2], 2.);
     EXPECT_DOUBLE_EQ(precomputed.factorial[3], 6.);
+    /* the following disabled because tgamma is too unprecise under
+       old versions of glibc (at leat up to 2.12, but less than 2.22)
     for( size_t k=4; k<precomputed.factorial.size(); ++k )
         EXPECT_NEAR(precomputed.factorial[k], tgamma(k+1.), 12*eps*tgamma(k+1.) );
+    */
+    EXPECT_NEAR(precomputed.factorial[150], 5.71338395644585459e262, 4*eps*precomputed.factorial[150]);
 }
 
 #endif

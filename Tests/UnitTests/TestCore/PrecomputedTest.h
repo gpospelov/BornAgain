7ca27c3f0b43c7cbbf84b79659aa4a1676b781b5
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2016-03-31 10:56:47 +0200

    increase tolerance in test of factorial vs tgamma; otherwise failure on build server

--- Tests/UnitTests/TestCore/PrecomputedTest.h
+++ Tests/UnitTests/TestCore/PrecomputedTest.h
@@ -20,7 +20,7 @@ TEST_F(PrecomputedTest, Factorial)
     EXPECT_DOUBLE_EQ(precomputed.factorial[2], 2.);
     EXPECT_DOUBLE_EQ(precomputed.factorial[3], 6.);
     for( size_t k=4; k<precomputed.factorial.size(); ++k )
-        EXPECT_NEAR(precomputed.factorial[k], tgamma(k+1.), 4*eps*tgamma(k+1.) );
+        EXPECT_NEAR(precomputed.factorial[k], tgamma(k+1.), 12*eps*tgamma(k+1.) );
 }
 
 #endif

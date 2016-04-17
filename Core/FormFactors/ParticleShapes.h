15eb60cb7c1fd466eee2a2c74a51f38d1ee063d2
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2016-04-17 15:55:46 +0200

    split FormFactors.h -> ParticleShapes.h

---
+++ Core/FormFactors/ParticleShapes.h
@@ -0,0 +1,48 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      FormFactors/inc/ParticleShapes.h
+//! @brief     Include all particle-shape form-factor include files.
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2015
+//! @authors   Scientific Computing Group at MLZ Garching
+//! @authors   C. Durniak, M. Ganeva, G. Pospelov, W. Van Herck, J. Wuttke
+//
+// ************************************************************************** //
+
+#ifndef PARTICLESHAPES_H_
+#define PARTICLESHAPES_H_
+
+#include "FormFactorAnisoPyramid.h"
+#include "FormFactorBox.h"
+#include "FormFactorCone.h"
+#include "FormFactorCone6.h"
+#include "FormFactorCuboctahedron.h"
+#include "FormFactorCylinder.h"
+#include "FormFactorDodecahedron.h"
+#include "FormFactorEllipsoidalCylinder.h"
+#include "FormFactorFullSphere.h"
+#include "FormFactorFullSpheroid.h"
+#include "FormFactorGauss.h"
+#include "FormFactorHemiEllipsoid.h"
+#include "FormFactorIcosahedron.h"
+#include "FormFactorLongBoxGauss.h"
+#include "FormFactorLongBoxLorentz.h"
+#include "FormFactorLorentz.h"
+#include "FormFactorPrism3.h"
+#include "FormFactorPrism6.h"
+#include "FormFactorPyramid.h"
+#include "FormFactorRipple1.h"
+#include "FormFactorRipple2.h"
+#include "FormFactorSphereGaussianRadius.h"
+#include "FormFactorSphereLogNormalRadius.h"
+#include "FormFactorTetrahedron.h"
+#include "FormFactorTrivial.h"
+#include "FormFactorTruncatedCube.h"
+#include "FormFactorTruncatedSphere.h"
+#include "FormFactorTruncatedSpheroid.h"
+
+#endif /* PARTICLESHAPES_H_ */

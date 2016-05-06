dd0746d4ebcb06aa983d11c5ed9c0d668243202d
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2016-05-06 14:48:35 +0200

    new test mode: loop over |q| for given direction

--- dev-tools/math/fftest/runff.cpp
+++ dev-tools/math/fftest/runff.cpp
@@ -96,33 +96,58 @@ void bisect(
 
 //! Computes form factor, and prints result according to outfilter.
 
-void run(const IFormFactorBorn* polyh, const int ishape, const cvector_t q, const int outfilter )
+void run(const IFormFactorBorn* polyh, const int ishape, const cvector_t q, int outfilter )
 {
     complex_t ret = polyh->evaluate_for_q(q);
-
-    if( outfilter==9 )
-        return;
-    cout <<
-        ishape << " " <<
-        std::setprecision(16) <<
-        q[0].real() << " " << q[0].imag() << " " <<
-        q[1].real() << " " << q[1].imag() << " " <<
-        q[2].real() << " " << q[2].imag() << " " <<
-        std::setprecision(17) <<
-        q.mag() << " ";
+    cout<<std::fixed<<std::setprecision(16)<<std::setfill('0');
     if     ( outfilter==0 )
-        cout << ret;
+        cout<<q.mag()<<" "<<std::abs(ret)<<" "<<ret.real()<<" "<<ret.imag()<<
+            diagnosis.nExpandedFaces<<" "<<diagnosis.maxOrder;
     else if( outfilter==1 )
-        cout << ret.real();
+        cout<<ret.real();
     else if( outfilter==2 )
-        cout << ret.imag();
-    else if( outfilter==7 || outfilter==8 )
-        cout << ret.real() << " " << ret.imag() << " " << std::abs(ret);
-    else
-        throw "invalid outfilter";
-#ifdef POLYHEDRAL_DIAGNOSTIC
-    cout << " " << diagnosis.nExpandedFaces << " " << diagnosis.maxOrder << "\n";
-#endif
+        cout<<ret.imag();
+    cout<<"\n";
+}
+
+
+void test_matching( int ishape, const vector<vector<cvector_t>>& scans )
+{
+    IFormFactorBorn* polyh( make_particle( ishape ) );
+    int n_mag = 11;
+    double mag_i = 1e-4;
+    double mag_f = 1e-2;
+    for( int i=1; i<n_mag; ++i ) {
+        double mag = mag_i*pow(mag_f/mag_i,i/(n_mag-1.));
+        double res[3];
+        res[0] = res[1] = res[2] = 0;
+        for( const vector<cvector_t>& q_scan: scans ) {
+            complex_t ff[3];
+            assert( q_scan.size()== 1 );
+            const cvector_t q = mag * q_scan[0];
+            FormFactorPolyhedron::setLimits( 1e99, 20 );
+            PolyhedralFace::setLimits( 1e99, 20 );
+            ff[0] = polyh->evaluate_for_q( q );
+            FormFactorPolyhedron::setLimits( 1e-99, 20 );
+            PolyhedralFace::setLimits( 1e99, 20 );
+            ff[1] = polyh->evaluate_for_q( q );
+            FormFactorPolyhedron::setLimits( 1e-99, 20 );
+            PolyhedralFace::setLimits( 1e-99, 20 );
+            ff[2] = polyh->evaluate_for_q( q );
+            double dev[3];
+            dev[2] = std::abs(ff[0]-ff[1])*2/(std::abs(ff[0])+std::abs(ff[1]));
+            dev[1] = std::abs(ff[0]-ff[2])*2/(std::abs(ff[0])+std::abs(ff[2]));
+            dev[0] = std::abs(ff[1]-ff[2])*2/(std::abs(ff[1])+std::abs(ff[2]));
+            for( int m=0; m<3; ++m ) {
+                res[m] = std::max(res[m], dev[m] );
+                if( dev[m]>1e-4 )
+                    cout<<ishape<<" "<<mag<<" "<<std::setprecision(8)<<
+                        dev[0]<<" "<<dev[1]<<" "<<dev[2]<<" "<<
+                        ff[0]<<" "<<ff[1]<<" "<<ff[2]<<" @ "<<q<<"\n";
+            }
+        }
+        cout<<ishape<<" "<<mag<<" "<<std::setprecision(8)<<res[0]<<" "<<res[1]<<" "<<res[2]<<"\n";
+    }
 }
 
 double test_continuity( int ishape, const vector<vector<cvector_t>>& scans )
@@ -144,19 +169,32 @@ double test_continuity( int ishape, const vector<vector<cvector_t>>& scans )
         }
     }
     fprintf( stderr, "\n" );
-    cout << "shape " << ishape << " => max rel step = " << maxrelstep << "\n";
+    cout<<"shape "<<ishape<<" => max rel step = "<<maxrelstep<<"\n";
     return maxrelstep;
 }
 
-void loop_one_shape( int outfilter, int ishape, const vector<vector<cvector_t>>& scans )
+void loop_one_shape( int ishape, const vector<vector<cvector_t>>& scans )
 {
     IFormFactorBorn* polyh( make_particle( ishape ) );
-    for( const vector<cvector_t>& q_scan: scans ) {
-        for( const cvector_t q: q_scan )
-            run( polyh, ishape, q, outfilter );
+    for( size_t j=0; j<scans.size(); ++j ) {
+        if( !(j%100) )
+            fprintf( stderr, "%2i: %8li/%li\r", ishape, j, scans.size() );
+        const vector<cvector_t>& q_scan = scans[j];
+        for( const cvector_t q: q_scan ) {
+            complex_t ret = polyh->evaluate_for_q(q);
+            cout <<
+                ishape<<" " <<
+                std::setprecision(16) <<
+                q[0].real()<<" "<<q[0].imag()<<" " <<
+                q[1].real()<<" "<<q[1].imag()<<" " <<
+                q[2].real()<<" "<<q[2].imag()<<" " <<
+                std::setprecision(17) <<
+                q.mag()<<" " <<
+                ret.real()<<" "<<ret.imag()<<" "<<std::abs(ret)<<" " <<
+                diagnosis.nExpandedFaces<<" "<<diagnosis.maxOrder<<"\n";
+        }
     }
 }
-
 
 vector<vector<cvector_t>> create_scans( int mode )
 {
@@ -173,21 +211,9 @@ vector<vector<cvector_t>> create_scans( int mode )
         { 1., 1., 1. }
     };
 
-    static int n_difdir = 7;
-    cvector_t difdir[n_difdir] = {
-        { 1., 0., 0. },
-        { 0., 1., 0. },
-        { 0., 0., 1. },
-
-        { 0., 1., 1. },
-        { 1., 0., 1. },
-        { 1., 1., 0. },
-
-        { 1., 1., 1. }
-    };
-
-    static int n_absdir = 7; // absorption component
+    static int n_absdir = 8; // absorption component
     cvector_t absdir[n_absdir] = {
+        { 0., 0., 0. },
         { 1e-15*I, 0., 0. },
         { 0., 1e-10*I, 0. },
         { 0., 0., 1e-5*I },
@@ -198,26 +224,32 @@ vector<vector<cvector_t>> create_scans( int mode )
     };
 
     vector<double> mag;
-    if ( mode==1 ) {
+    if ( mode==0 ) {
+        mag.resize(1);
+        mag[0] = 1;
+    } else if ( mode==1 ) {
         mag = vector<double>( { 0, 1e-12, 1e-10, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3,
                     .01, .06, .2, .5, 1, 2, 5, 10, 20, 50, 100 } );
     } else if (mode==2 ) {
-        mag.resize(2001);
+        mag.resize(1001);
         mag[0] = 0.;
         for( size_t i=1; i<mag.size(); ++i )
             mag[i] = 1e-10*pow(1e13,(i-1.)/(mag.size()-2));
     }
 
-    static const int n_difmag = 16;
-    static double difmag[n_difmag] = { 3e-16, 1e-15, 3e-15, 1e-14, 1e-13, 1e-12, 1e-11, 1e-9, 1e-7,
-                             1e-5, 1e-3, .01, .03, .1, .2, .3 };
+    static const int n_difmag = 17;
+    static double difmag[n_difmag] =
+        { 0., 3e-16, 1e-15, 3e-15, 1e-14, 1e-13, 1e-12, 1e-11, 1e-9, 1e-7,
+          1e-5, 1e-3, .01, .03, .1, .2, .3 };
 
     vector<vector<cvector_t>> scans;
     for( int i=0; i<n_dir; ++i ) {
-        for( int j=0; j<n_difdir; ++j ) {
+        for( int j=0; j<n_dir; ++j ) {
+            if( i==j )
+                continue;
             for( int k=0; k<n_difmag; ++k ) {
                 for( int l=0; l<n_absdir; ++l ) {
-                    cvector_t uq = ( dir[i].unit() + difmag[k]*difdir[j] + absdir[l] ).unit();
+                    cvector_t uq = ( dir[i].unit() + difmag[k]*dir[j] + absdir[l] ).unit();
                     vector<cvector_t> scan;
                     for( size_t m=0; m<mag.size(); ++m )
                         scan.push_back( mag[m] * uq );
@@ -231,14 +263,14 @@ vector<vector<cvector_t>> create_scans( int mode )
 
 void help_and_exit()
 {
-    cerr << "fftest limits inmode outfilter shape qxr qxi qyr qyi qzr qzi [q]\n";
-    cerr << "  limits:    \"def\" | qlim qpalim nlim\n";
-    cerr << "  inmode:    get q from 0:stdin | 1:cmdline\n";
-    cerr << "  outfilter: return 0:all | 1:real | 2:imag\n";
-    cerr << "fftest limits loop shape [outfilter]\n";
-    cerr << "  limits:    \"def\" | qlim qpalim nlim\n";
-    cerr << "  loop:      2[cont_test] | 3[for_plot]\n";
-    cerr << "  shape:     0[all] | 1..[specific]\n";
+    cerr<<"fftest limits inmode outfilter shape qxr qxi qyr qyi qzr qzi [q]\n";
+    cerr<<"  limits:    \"def\" | qlim qpalim nlim\n";
+    cerr<<"  inmode:    get q from 0:stdin | 1:cmdline | 2:loop\n";
+    cerr<<"  outfilter: return 0:all | 1:real | 2:imag\n";
+    cerr<<"fftest limits loop shape\n";
+    cerr<<"  limits:    \"def\" | qlim qpalim nlim\n";
+    cerr<<"  loop:      3[cont_test] | 4[matching_test] | 5[for_plot]\n";
+    cerr<<"  shape:     0[all] | 1..[specific]\n";
     exit(0);
 }
 
@@ -263,7 +295,7 @@ int main (int argc, const char *argv[])
         }
         NEXTARG;
         int inmode = atoi( *arg );
-        if( inmode==0 || inmode==1 ) {
+        if( inmode<=2 ) {
             NEXTARG;
             int outfilter = atoi( *arg );
             NEXTARG;
@@ -274,17 +306,25 @@ int main (int argc, const char *argv[])
                 complex_t( atof(*(arg-5)), atof(*(arg-4)) ),
                 complex_t( atof(*(arg-3)), atof(*(arg-2)) ),
                 complex_t( atof(*(arg-1)), atof(*(arg-0)) ) );
-            double qmag;
-            if( inmode==1 ) {
-                NEXTARG;
-                qmag = atof(argv[10]);
-                run( P, ishape, qmag*uq, outfilter );
-            } else if( inmode==0 ) {
+            double mag;
+            if( inmode==0 ) {
                 // for use through Frida
                 int nop;
                 std::cin >> nop;
-                while( std::cin >> qmag )
-                    run( P, ishape, qmag*uq, outfilter );
+                while( std::cin >> mag )
+                    run( P, ishape, mag*uq, outfilter );
+            } else if( inmode==1 ) {
+                NEXTARG;
+                mag = atof(argv[10]);
+                run( P, ishape, mag*uq, outfilter );
+            } else if( inmode==2 ) {
+                int n_mag = 2001;
+                double mag_i = 1e-9;
+                double mag_f = 1e2;
+                for( int i=1; i<n_mag; ++i ) {
+                    mag = mag_i*pow(mag_f/mag_i,i/(n_mag-1.));
+                    run( P, ishape, mag*uq, outfilter );
+                }
             }
             exit(0);
         }
@@ -292,31 +332,38 @@ int main (int argc, const char *argv[])
         NEXTARG;
         int ishapepar = atoi( *arg );
 
-        if( inmode==2 ) { // continuity test
+        if( inmode==3 ) { // continuity test
             vector<vector<cvector_t>> scans = create_scans( 1 );
             double totmaxrelstep = 0;
             if( ishapepar==0 ) {
                 for( int ishape=1; ishape<=nshape; ++ishape )
                     totmaxrelstep = std::max( totmaxrelstep, test_continuity( ishape, scans ) );
-                cout << "grand total max rel step = " << totmaxrelstep << "\n";
+                cout<<"grand total max rel step = "<<totmaxrelstep<<"\n";
             } else {
                 test_continuity( ishapepar, scans );
             }
             exit (0);
         }
+        if( inmode==4 ) { // compare computation methods
+            vector<vector<cvector_t>> scans = create_scans( 0 );
+            if( ishapepar==0 ) {
+                for( int ishape=1; ishape<=nshape; ++ishape )
+                    test_matching( ishape, scans );
+            } else
+                test_matching( ishapepar, scans );
+            exit (0);
+        }
         // it's a straight loop over q
         vector<vector<cvector_t>> scans = create_scans( 2 );
-        NEXTARG;
-        int outfilter = atoi( *arg );
         if( ishapepar==0 ) {
             for( int ishape=1; ishape<=nshape; ++ishape )
-                loop_one_shape( outfilter, ishape, scans );
+                loop_one_shape( ishape, scans );
         } else
-            loop_one_shape( outfilter, ishapepar, scans );
+            loop_one_shape( ishapepar, scans );
         exit(0);
 
     } catch( const char* ex ) {
-        cerr << "F(q) failed: " << ex << "\n";
+        cerr<<"F(q) failed: "<<ex<<"\n";
         exit(0);
     }
 }

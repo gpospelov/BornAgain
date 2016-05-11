dc72bf9b91e2715dd9d47f0e3df6fc5d5f8ce3b3
Joachim Wuttke (l) <j.wuttke@fz-juelich.de>
2016-05-11 09:57:18 +0200

    correct/improve comments

--- Core/Tools/IParameterized.h
+++ Core/Tools/IParameterized.h
@@ -3,7 +3,7 @@
 //  BornAgain: simulate and fit scattering at grazing incidence
 //
 //! @file      Tools/IParameterized.h
-//! @brief     Declares class IParameterized.
+//! @brief     Declares classes IParameterized and ParameterPattern
 //!
 //! @homepage  http://www.bornagainproject.org
 //! @license   GNU General Public License v3 or higher (see COPYING)

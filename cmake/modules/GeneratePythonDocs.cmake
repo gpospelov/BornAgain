901e907de98fdc3c43a547e2865f198fb09391cc
Joachim Wuttke (l) <j.wuttke@fz-juelich.de>
2016-06-03 10:31:51 +0200

    Code that is common to Core and Fit moved to new cmake module

---
+++ cmake/modules/GeneratePythonDocs.cmake
@@ -0,0 +1,21 @@
+# Autogenerate a .i file from doxygen comments, provided -DBORNAGAIN_GENERATE_PYTHON_DOCS=ON.
+
+function(GeneratePythonDocs PD_TARGET SWIG_DIR)
+    if(BORNAGAIN_GENERATE_PYTHON_DOCS)
+
+        configure_file(${SWIG_DIR}/DoxyfileSwig.in ${TMP_DIR}/Doxyfile @ONLY)
+
+        add_custom_command(
+            OUTPUT ${TMP_DIR}/xml/index.xml
+            COMMAND ${DOXYGEN_EXECUTABLE} ${TMP_DIR}/Doxyfile
+            DEPENDS ${include_files}
+            )
+        add_custom_command(
+            OUTPUT ${PD_TARGET}
+            COMMAND ${PYTHON_EXECUTABLE} ${SWIG_DIR}/doxy2swig.py
+            ${TMP_DIR}/xml/index.xml ${PD_TARGET}
+            DEPENDS ${TMP_DIR}/xml/index.xml
+            )
+
+    endif(BORNAGAIN_GENERATE_PYTHON_DOCS)
+endfunction()

f8ecb092fe78e2fc322a49bd61747e990331bfdf
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2016-07-13 16:40:02 +0200

    Repair previous commit. It seems, SCALARRTCOEFFICIENTS_H was used in two different meanings

--- Core/Algorithms/ScalarRTCoefficients.h
+++ Core/Algorithms/ScalarRTCoefficients.h
@@ -13,8 +13,8 @@
 //
 // ************************************************************************** //
 
-#ifndef SCALARRTCOEFFICIENTS_H
-#define SCALARRTCOEFFICIENTS_H
+#ifndef SCALARRTCOEFFICIENTS_H2
+#define SCALARRTCOEFFICIENTS_H2
 
 #include "ILayerRTCoefficients.h"
 #include "Complex.h"
@@ -148,4 +148,4 @@ inline complex_t ScalarRTCoefficients::getScalarT() const
     return t_r(0);
 }
 
-#endif // SCALARRTCOEFFICIENTS_H
+#endif // SCALARRTCOEFFICIENTS_H2

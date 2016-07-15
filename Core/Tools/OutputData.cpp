b6e03d2adaf0f9f8d080d2484b48286ad906d080
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2016-07-15 11:20:31 +0200

    try #include <pyconfig.h>

--- Core/Tools/OutputData.cpp
+++ Core/Tools/OutputData.cpp
@@ -18,6 +18,7 @@
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #define PY_ARRAY_UNIQUE_SYMBOL BORNAGAIN_PYTHONAPI_ARRAY
 #define NO_IMPORT_ARRAY
+#include <pyconfig.h>
 #include <numpy/arrayobject.h>
 
 #include "OutputData.h"

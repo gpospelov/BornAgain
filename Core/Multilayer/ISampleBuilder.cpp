9edd351f2af7d16e1683e9a5720514103a33977e
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2016-07-17 00:14:16 +0200

    missing in previous ca 3 commits

---
+++ Core/Multilayer/ISampleBuilder.cpp
@@ -0,0 +1,28 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Core/Samples/ISampleBuilder.cpp
+//! @brief     Implements pure virtual base class ISampleBuilder.
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2015
+//! @authors   Scientific Computing Group at MLZ Garching
+//! @authors   C. Durniak, M. Ganeva, G. Pospelov, W. Van Herck, J. Wuttke
+//
+// ************************************************************************** //
+
+#include "ISampleBuilder.h"
+#include "FTDistributions.h"
+#include "IFormFactor.h"
+
+const IFormFactor* ISampleBuilder::getFormFactor() const
+{
+    return dynamic_cast<const IFormFactor*>(m_subtest_item);
+}
+
+const IFTDistribution2D* ISampleBuilder::getFTDistribution2D() const
+{
+    return dynamic_cast<const IFTDistribution2D*>(m_subtest_item);
+}

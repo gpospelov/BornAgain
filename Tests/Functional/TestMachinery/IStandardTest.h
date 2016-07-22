51497d948b90398a0c99ecd94c9b9b7b2f694e16
Joachim Wuttke (l) <j.wuttke@fz-juelich.de>
2016-07-22 11:30:56 +0200

    IStandardTest: class header comment summarizes architecture of standard functional tests.

--- Tests/Functional/TestMachinery/IStandardTest.h
+++ Tests/Functional/TestMachinery/IStandardTest.h
@@ -24,13 +24,33 @@ class FunctionalTestInfo;
 class IParameterized;
 
 //! @class IStandardTest
-//! @brief Base class for Core/PyCore/GUI tests.
+//! @brief Base class for Core/PyCore/GUI tests that involve standard simulations.
 
-//! Used in functional tests (Core|Py|GUI)Suite, where it is subclassed
-//! as a singleton, and called through instance().execute(argc, argv).
-
-//! When processing execute, dependent classes will call back getTest().
-//! Certain tests have subtests; they will call back getFormFactor() etc.
+//! For Foo in {Core, PyCore, GUI}, the functional test mechanism is as follows:
+//!
+//! FooTestSuite is the executable obtained from
+//!   main(argv,argc) in FooTestSuite.cpp,
+//! which runs
+//!   FooTestSuite().execute(argv,argc),
+//! which is inherited from here. This function,
+//!   IStandardTest::execute(argv,argc)
+//! uses argv[1] to retrieve a set of parameters from FunctionalTestRegistry.
+//! Then it bifurcates into
+//!   IStandardTest::execute_onetest(argv,argc) or
+//!   IStandardTest::execute_subtests(argv,argc)
+//! which will call
+//!   IFunctionalTest::getTest(),
+//! which is the only nontrivial member of FooTestSuite, implemented in FooTestSuite.cpp.
+//! The return value
+//!   test = new FooTest( <parameters> )
+//! is provided by FooTest.cpp.
+//! The arguments in this constructor are callback functions provided by IStandardTest;
+//! they contain information gathered from FunctionalTestRegistry, and possibly from
+//! a subtest registry.
+//! Finally, IStandardTest::execute_... will call
+//!   test->runTest()        // implemented in FooTest
+//!   test->analyseResults() // implemented in FooTest
+//!   test->getTestResult()  // implemented in IFunctionalTest
 
 class BA_CORE_API_ IStandardTest : public INamed
 {

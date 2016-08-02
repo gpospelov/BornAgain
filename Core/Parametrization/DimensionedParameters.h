a7f52ef60b20139a0ebf59fb31a9620b69ba270c
Joachim Wuttke (l) <j.wuttke@fz-juelich.de>
2016-08-02 11:04:57 +0200

    pack Angle, Length, Scalar all in DimensionedParameters.h

--- Core/Parametrization/DimensionedParameters.h
+++ Core/Parametrization/DimensionedParameters.h
@@ -3,7 +3,7 @@
 //  BornAgain: simulate and fit scattering at grazing incidence
 //
 //! @file      Core/Parametrization/DimensionedParameters.h
-//! @brief     Inludes all dimensioned parameter definitions.
+//! @brief     Defines the dimensioned parameter classes Angle, Length, Scalar
 //!
 //! @homepage  http://www.bornagainproject.org
 //! @license   GNU General Public License v3 or higher (see COPYING)
@@ -13,6 +13,54 @@
 //
 // ************************************************************************** //
 
-#include "Angle.h"
-#include "Length.h"
-#include "Scalar.h"
+#ifndef DIMPAR_H
+#define DIMPAR_H
+
+#include "RealParameter.h" // inheriting from
+
+//! A parameter of physical dimension "angle". Values are in rad? deg? TODO clarify
+
+class Angle : public RealParameter
+{
+public:
+    Angle(const std::string& name, ParameterPool* parent, volatile double* par,
+          const AttLimits& limits)
+        : RealParameter( name, parent, par, limits ) {}
+
+    virtual Angle* clone( const std::string& new_name="" ) const {
+        return new Angle( new_name!="" ? new_name : m_name, m_parent, m_data, m_limits ); }
+
+    std::string unit() const final { return "deg"; }
+};
+
+//! A parameter of physical dimension "length". Values are in nm.
+
+class Length : public RealParameter
+{
+public:
+    Length(const std::string& name, ParameterPool* parent, volatile double* par,
+           const AttLimits& limits)
+        : RealParameter( name, parent, par, limits ) {}
+
+    virtual Length* clone( const std::string& new_name="" ) const {
+        return new Length( new_name!="" ? new_name : m_name, m_parent, m_data, m_limits ); }
+
+    std::string unit() const final { return "nm"; }
+};
+
+//! A parameter of that is physically dimensionless.
+
+class Scalar : public RealParameter
+{
+public:
+    Scalar(const std::string& name, ParameterPool* parent, volatile double* par,
+           const AttLimits& limits)
+        : RealParameter( name, parent, par, limits ) {}
+
+    virtual Scalar* clone( const std::string& new_name="" ) const {
+        return new Scalar( new_name!="" ? new_name : m_name, m_parent, m_data, m_limits ); }
+
+    std::string unit() const final { return ""; }
+};
+
+#endif // DIMPAR_H

85a47f51e21d75b7cc36883760421b93e7478946
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2016-08-09 11:29:49 +0200

    correct previous commit; remove last M_PI in our own code (there are still a few in third-party libraries)

---
+++ Core/Basics/Pi.h
@@ -0,0 +1,28 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Core/Basics/Pi.h
+//! @brief     Defines PI and related mathematical constants.
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2015
+//! @authors   Scientific Computing Group at MLZ Garching
+//! @authors   C. Durniak, M. Ganeva, G. Pospelov, W. Van Herck, J. Wuttke
+//
+// ************************************************************************** //
+
+#ifndef PI_H
+#define PI_H
+
+namespace Pi {
+
+static const double PI   = 3.14159265358979323846264338327950288;
+static const double PI2  = 6.28318530717958647692528676655900577;
+static const double PID2 = 1.57079632679489661923132169163975144;
+static const double PID4 = PI/4.0;
+
+} // namespace Pi
+
+#endif // PI_H

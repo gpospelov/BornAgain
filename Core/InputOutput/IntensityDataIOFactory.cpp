4765dcdd9b5f95656c5fa90e4903d497ac2edb9c
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2016-09-01 00:07:38 +0200

    fix long-standing memory leak in I/O machinery

--- Core/InputOutput/IntensityDataIOFactory.cpp
+++ Core/InputOutput/IntensityDataIOFactory.cpp
@@ -18,10 +18,12 @@
 #include "OutputDataReadFactory.h"
 #include "OutputDataWriteFactory.h"
 
-
 OutputData<double>* IntensityDataIOFactory::readOutputData(const std::string& file_name)
 {
-    return OutputDataReadFactory::getReader(file_name)->getOutputData();
+    auto* reader = OutputDataReadFactory::getReader(file_name);
+    auto* result = reader->getOutputData();
+    delete reader;
+    return result;
 }
 
 IHistogram* IntensityDataIOFactory::readIntensityData(const std::string& file_name)
@@ -32,7 +34,9 @@ IHistogram* IntensityDataIOFactory::readIntensityData(const std::string& file_na
 void IntensityDataIOFactory::writeOutputData(
     const OutputData<double>& data, const std::string& file_name)
 {
-    return OutputDataWriteFactory::getWriter(file_name)->writeOutputData(data);
+    auto *writer = OutputDataWriteFactory::getWriter(file_name);
+    writer->writeOutputData(data);
+    delete writer;
 }
 
 void IntensityDataIOFactory::writeIntensityData(
--- Core/InputOutput/OutputDataReadFactory.cpp
+++ Core/InputOutput/OutputDataReadFactory.cpp
@@ -17,7 +17,6 @@
 #include "Exceptions.h"
 #include "OutputDataIOHelper.h"
 
-
 OutputDataReader* OutputDataReadFactory::getReader(const std::string &file_name)
 {
     OutputDataReader* result = new OutputDataReader(file_name);
@@ -28,20 +27,16 @@ OutputDataReader* OutputDataReadFactory::getReader(const std::string &file_name)
 IOutputDataReadStrategy* OutputDataReadFactory::getReadStrategy(const std::string &file_name)
 {
     IOutputDataReadStrategy* result(nullptr);
-    if(OutputDataIOHelper::isIntFile(file_name)) {
+    if(OutputDataIOHelper::isIntFile(file_name))
         result = new OutputDataReadINTStrategy();
-    }
-    else if(OutputDataIOHelper::isTxtFile(file_name)) {
+    else if(OutputDataIOHelper::isTxtFile(file_name))
         result = new OutputDataReadNumpyTXTStrategy();
-    }
 #ifdef BORNAGAIN_TIFF_SUPPORT
-    else if(OutputDataIOHelper::isTiffFile(file_name)) {
+    else if(OutputDataIOHelper::isTiffFile(file_name))
        result = new OutputDataReadTiffStrategy();
-    }
 #endif // BORNAGAIN_TIFF_SUPPORT
-    else {
+    else
         throw Exceptions::LogicErrorException("OutputDataReadFactory::getReader() -> Error. "
                 "Don't know how to read file '" + file_name+std::string("'"));
-    }
     return result;
 }

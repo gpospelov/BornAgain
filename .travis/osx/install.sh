eb9aa488d32aac15ae4abb803cbc59ed25f0aca4
Gennady Pospelov <g.pospelov@fz-juelich.de>
2016-09-06 14:30:58 +0200

    Update install.sh

--- .travis/osx/install.sh
+++ .travis/osx/install.sh
@@ -1,5 +1,6 @@
 brew update
 brew install git fftw gsl python homebrew/python/matplotlib numpy boost-python qt5 eigen libyaml yaml-cpp
 brew tap homebrew/versions
+brew link xz
 brew install llvm38
 pip install pyyaml

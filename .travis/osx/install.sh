399f6aeded7a4958c54896ab94adca56aabb32a3
Gennady Pospelov <g.pospelov@fz-juelich.de>
2016-09-06 17:23:01 +0200

    MacOS Travis configuration is switched to xcode7.3

--- .travis/osx/install.sh
+++ .travis/osx/install.sh
@@ -1,5 +1,3 @@
 brew update
 brew install git fftw gsl python homebrew/python/matplotlib numpy boost-python qt5 eigen libyaml yaml-cpp
-brew tap homebrew/versions
-brew install llvm37
 pip install pyyaml

7a3b11b651cd835bc33305a1f828e12ea4fa8aa3
Gennady Pospelov <g.pospelov@fz-juelich.de>
2016-09-06 14:53:08 +0200

    MacOS travis build switched to llvm38

--- .travis/osx/build.sh
+++ .travis/osx/build.sh
@@ -1,5 +1,5 @@
 export PATH=/usr/local/opt/qt5/bin/:$PATH
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=$BUILD_TYPE ..
-make -j9
+cmake -DCMAKE_C_COMPILER=/usr/local/opt/llvm38/bin/clang-3.8 -DCMAKE_CXX_COMPILER=/usr/local/opt/llvm38/bin/clang++-3.8 -DCMAKE_PREFIX_PATH=/usr/local -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
+make -j4

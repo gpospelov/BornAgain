0cdf39542bdf4107ea95ead36f7ecc306e2ca76b
Gennady Pospelov <g.pospelov@fz-juelich.de>
2016-09-06 15:29:27 +0200

    llvm37 installation is required in MacOS TravisCI

--- .travis/osx/install.sh
+++ .travis/osx/install.sh
@@ -1,5 +1,5 @@
 brew update
 brew install git fftw gsl python homebrew/python/matplotlib numpy boost-python qt5 eigen libyaml yaml-cpp
 brew tap homebrew/versions
-brew install llvm38
+brew install llvm37
 pip install pyyaml

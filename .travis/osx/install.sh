7507c72afb49e5a8159e8e0ae97915e4f2bfb392
gpospelov <g.pospelov@fz-juelich.de>
2016-09-06 13:46:13 +0200

    Update in MacOS Travis configuration to use latest llvm

--- .travis/osx/install.sh
+++ .travis/osx/install.sh
@@ -1,3 +1,5 @@
 brew update
 brew install git fftw gsl python homebrew/python/matplotlib numpy boost-python qt5 eigen libyaml yaml-cpp
+brew tap homebrew/versions
+brew install llvm38
 pip install pyyaml

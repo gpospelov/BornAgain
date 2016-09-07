0e7f295102116f36838dbb3d7f34f04f15f58c74
Gennady Pospelov <g.pospelov@fz-juelich.de>
2016-09-07 11:24:14 +0200

    Fixing cmake test machinery for windows

--- Tests/Functional/TestMachinery/IReferencedTest.cpp
+++ Tests/Functional/TestMachinery/IReferencedTest.cpp
@@ -44,8 +44,16 @@ bool IReferencedTest::compareIntensityMaps(
 //! Runs a python command, prints messages, returns true unless the system call failed.
 bool IReferencedTest::runPython(const std::string& py_command)
 {
+#ifndef _WIN32
     std::string sys_command = std::string("PYTHONPATH=") + BUILD_LIB_DIR + " " +
         PYTHON_EXECUTABLE + " " + py_command;
+#else
+    std::string sys_command = std::string("set PYTHONPATH=") + BUILD_LIB_DIR + " & " +
+        PYTHON_EXECUTABLE + " " + py_command;
+#endif
+
+
+
     std::cout << sys_command << std::endl/*sic*/; // flush output before calling std::system
     int ret = std::system(sys_command.c_str());
     if (ret!=0) {

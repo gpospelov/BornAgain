6c9ea38821affd5c48ffcd0a6ef46e065a6dfe21
Gennady Pospelov <g.pospelov@fz-juelich.de>
2016-09-07 11:37:31 +0200

    CheckCode scripts are excluded from Windows build

--- cmake/bornagain/modules/CheckCode.cmake
+++ cmake/bornagain/modules/CheckCode.cmake
@@ -1,5 +1,7 @@
 set(SRC_LEN_LIM 100) # maximum line length of code written by MLZ-SCG
 
+if(NOT MSVC)
+
 file(GLOB_RECURSE SRC1 "Core/*.cpp")
 file(GLOB_RECURSE SRC2 "Core/*.h")
 
@@ -7,3 +9,4 @@ add_test(NAME "CodeLineLength"
     COMMAND  ${PYTHON_EXECUTABLE}
     ${CMAKE_SOURCE_DIR}/dev-tools/analyze/count-line-length.py ${SRC_LEN_LIM}
     ${SRC1} ${SRC2})
+endif()
--- cmake/bornagain/modules/PyExamplesLineLength.cmake
+++ cmake/bornagain/modules/PyExamplesLineLength.cmake
@@ -2,9 +2,13 @@ set(WEB_LEN_LIM 85) # maximum line length of code for display in Drupal web page
 
 # Check whether the line length of all python examples remains below WEB_LEN_LIM
 
+if(NOT MSVC)
+
 file(GLOB PY_EXAMPLES "${PY_EXAMPLES_DIR}/*/ex*/*.py")
 
 add_test(NAME "PyExamplesLineLength"
     COMMAND ${PYTHON_EXECUTABLE}
     ${CMAKE_SOURCE_DIR}/dev-tools/analyze/count-line-length.py ${WEB_LEN_LIM}
     ${PY_EXAMPLES})
+
+endif()

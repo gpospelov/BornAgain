a01e6a4a8e06ffaf86d9d4c00c507c1d82919d81
Joachim Wuttke (l) <j.wuttke@fz-juelich.de>
2016-11-20 13:15:59 +0100

    rm duplication of hard-coded default values in comments.

--- Fit/RootAdapter/SimAnMinimizer.h
+++ Fit/RootAdapter/SimAnMinimizer.h
@@ -20,9 +20,8 @@
 
 namespace BA_ROOT { namespace Math { class GSLSimAnMinimizer; } }
 
-//! @class SimAnMinimizer
+//! Wrapper for ROOT GSL simmulated annealing minimizer
 //! @ingroup fitting_internal
-//! @brief Wrapper for ROOT GSL simmulated annealing minimizer
 
 class BA_CORE_API_ SimAnMinimizer : public RootMinimizerAdapter {
 public:
@@ -35,37 +34,30 @@ public:
     int printLevel() const;
 
     //! Sets maximum number of iterations to try at each step.
-    //! Default values is 100.
     void setMaxIterations(int value);
     int maxIterations() const;
 
     //! Sets number of iterations at each temperature.
-    //! Default value is 10.
     void setIterationsAtEachTemp(int value);
     int iterationsAtEachTemp() const;
 
     //! Sets max step size used in random walk.
-    //! Default value is 1.0.
     void setStepSize(double value);
     double stepSize() const;
 
     //! Sets Boltzmann distribution parameter: k.
-    //! Default value 1.0.
     void setBoltzmannK(double value);
     double boltzmannK() const;
 
     //! Sets Boltzmann distribution parameter: initial temperature.
-    //! Default value 50.0.
     void setBoltzmannInitialTemp(double value);
     double boltzmannInitialTemp() const;
 
     //! Sets Boltzmann distribution parameter: mu.
-    //! Default value 1.05.
     void setBoltzmannMu(double value);
     double boltzmannMu() const;
 
     //! Sets Boltzmann distribution parameter: minimal temperature.
-    //! Default value 0.1.
     void setBoltzmannMinTemp(double value);
     double boltzmannMinTemp() const;
 

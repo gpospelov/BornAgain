d7a1762b9d0c230a2cf4fa805e9c6c23102779d3
Gennady Pospelov <g.pospelov@fz-juelich.de>
2016-12-01 13:02:40 +0100

    Another wording in Qt5-not-found complain

--- cmake/bornagain/modules/SearchQt.cmake
+++ cmake/bornagain/modules/SearchQt.cmake
@@ -13,7 +13,7 @@ endif()
 
 message(STATUS "Found Qt5 version ${Qt5Widgets_VERSION}")
 if(${Qt5Widgets_VERSION_MINOR} LESS 4)
-    message(FATAL_ERROR "Qt5 version 5.4 required")
+    message(FATAL_ERROR "Minimal Qt5 version 5.4 required")
 endif()
 get_target_property(Qt5Widgets_location Qt5::Widgets LOCATION_Release)
 message(STATUS "  ${Qt5Widgets_LIBRARIES} ${Qt5Widgets_location}")

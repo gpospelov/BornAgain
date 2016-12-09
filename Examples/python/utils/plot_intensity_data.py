62df3f76a65179bc1262b8941b01980cb3b066d9
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2016-12-09 16:17:10 +0100

    Equip plot_intensity_data.py with optional argument intensity_max to facilitate comparison between simulations.

--- Examples/python/utils/plot_intensity_data.py
+++ Examples/python/utils/plot_intensity_data.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
-# Plots intensity data stored in BornAgain "*.int" or "*.int.gz" format
-# Usage: python plot_intensity_data.py intensity_file.int.gz
+'''
+Plots intensity data stored in BornAgain "*.int" or "*.int.gz" format
+Usage: python plot_intensity_data.py intensity_file.int.gz [intensity_max]
+'''
 
 import matplotlib, numpy, sys
 from matplotlib import pyplot as plt
@@ -11,11 +13,14 @@ import bornagain as ba
 from bornagain import deg, angstrom, nm
 
 
-def plot_intensity_data(file_name):
+def plot_intensity_data(file_name, intensity_max=None):
     result = ba.IntensityDataIOFactory.readIntensityData(file_name)
+    if intensity_max is None:
+        intensity_max = result.getMaximum()
     im = plt.imshow(result.getArray(),
-                    norm=matplotlib.colors.LogNorm(1.0, result.getMaximum()),
-                    extent=[result.getXmin()/deg, result.getXmax()/deg, result.getYmin()/deg, result.getYmax()/deg],
+                    norm=matplotlib.colors.LogNorm(1.0, intensity_max),
+                    extent=[result.getXmin()/deg, result.getXmax()/deg,
+                            result.getYmin()/deg, result.getYmax()/deg],
                     aspect='auto')
     cb = plt.colorbar(im)
     cb.set_label(r'Intensity (arb. u.)', size=16)
@@ -25,7 +30,10 @@ def plot_intensity_data(file_name):
 
 
 if __name__ == '__main__':
-    if len(sys.argv)!=2:
-        exit("Usage: plot_intensity_data.py intensity_file.int.gz")
+    if len(sys.argv)<2 or len(sys.argv)>3:
+        exit("Usage: plot_intensity_data.py intensity_file.int.gz [intensity_max]")
 
-    plot_intensity_data(sys.argv[1])
+    if len(sys.argv)==2:
+        plot_intensity_data(sys.argv[1])
+    else:
+        plot_intensity_data(sys.argv[1], float(sys.argv[2]))

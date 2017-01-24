4e49dca6782767130a7d7b8dc83b47cc7337b36a
Gennady Pospelov <g.pospelov@fz-juelich.de>
2017-01-24 16:41:34 +0100

    Python example adopted for new Lattice2D child in InterferenceFunction2D

--- Examples/python/fitting/ex03_FitSpheresInHexLattice/FitSpheresInHexLattice.py
+++ Examples/python/fitting/ex03_FitSpheresInHexLattice/FitSpheresInHexLattice.py
@@ -95,9 +95,8 @@ def run_fitting():
     draw_observer = ba.DefaultFitObserver(draw_every_nth=10)
     fit_suite.attachObserver(draw_observer)
 
-    # this fit parameter will change both length_1 and length_2 simultaneously
     fit_suite.addFitParameter(
-        "*2DLattice/LatticeLength*", 8.*nm, ba.AttLimits.limited(4., 12.))
+        "*HexagonalLattice/LatticeLength", 8.*nm, ba.AttLimits.limited(4., 12.))
     fit_suite.addFitParameter(
         "*/FullSphere/Radius", 8.*nm, ba.AttLimits.limited(4., 12.))
 

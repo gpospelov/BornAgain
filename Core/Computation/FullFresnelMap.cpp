cc5b6fd2d1f66356f3b26486955d6ca6ea0e96c6
Walter Van Herck <w.van.herck@fz-juelich.de>
2017-02-01 17:05:17 +0100

    New class for holding Fresnel maps of all layers

---
+++ Core/Computation/FullFresnelMap.cpp
@@ -0,0 +1,27 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Core/Computation/FullFresnelMap.cpp
+//! @brief     Implements class FullFresnelMap.
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2017
+//! @authors   Scientific Computing Group at MLZ Garching
+//! @authors   J. Burle, J. M. Fisher, M. Ganeva, G. Pospelov, W. Van Herck, J. Wuttke
+//
+// ************************************************************************** //
+
+#include "FullFresnelMap.h"
+#include "ILayerSpecularInfo.h"
+
+void FullFresnelMap::push_back(ILayerSpecularInfo* p_layer_map)
+{
+    m_full_map.push_back(p_layer_map);
+}
+
+const ILayerSpecularInfo*FullFresnelMap::layerFresnelMap(size_t index) const
+{
+    return m_full_map[index];
+}

4a55ff40cc486ca9df88c38ffe0e1aff83526611
Gennady Pospelov <g.pospelov@fz-juelich.de>
2017-02-02 16:00:27 +0100

    ParameterTreeBuilder turned from static class to namespace.

--- GUI/coregui/Models/ParameterTreeBuilder.h
+++ GUI/coregui/Models/ParameterTreeBuilder.h
@@ -3,7 +3,7 @@
 //  BornAgain: simulate and fit scattering at grazing incidence
 //
 //! @file      GUI/coregui/Models/ParameterTreeBuilder.h
-//! @brief     Defines class ParameterTreeBuilder
+//! @brief     Defines ParameterTreeBuilder namespace
 //!
 //! @homepage  http://www.bornagainproject.org
 //! @license   GNU General Public License v3 or higher (see COPYING)
@@ -17,23 +17,22 @@
 #ifndef PARAMETERTREEBUILDER_H
 #define PARAMETERTREEBUILDER_H
 
-#include <QString>
 #include "WinDllMacros.h"
+#include <QString>
 
 class JobItem;
 class SessionItem;
 
-//! The ParameterTreeBuilder class helps JobModel to create a container with ParameterItem's.
-//! The ParameterItem appears in RealTimeView and provides real time tuning of MultiLayerItem
-//! and InstrumentItem.
+//! The ParameterTreeBuilder namespace contains helper functions to create container
+//! with ParameterItems. The ParameterItem appears in RealTimeView and provides real
+//! time tuning of MultiLayerItem and InstrumentItem.
 
-class BA_CORE_API_ ParameterTreeBuilder
+namespace ParameterTreeBuilder
 {
-public:
-    static void createParameterTree(JobItem *item, const QString &tag = QString());
-    static void populateDomainLinks(JobItem *jobItem,  const QString &tag);
-private:
-    static void handleItem(SessionItem *tree, SessionItem *source);
-};
+
+BA_CORE_API_ void createParameterTree(JobItem* item, const QString& tag = QString());
+BA_CORE_API_ void populateDomainLinks(JobItem* jobItem, const QString& tag);
+
+}
 
 #endif // PARAMETERTREEBUILDER_H

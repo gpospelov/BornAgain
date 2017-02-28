563939f6af38bc929d128a65bdb851fd40182344
Gennady Pospelov <g.pospelov@fz-juelich.de>
2017-02-28 16:01:38 +0100

    JobItem status switched from ComboProperty to ordinary string.

--- GUI/coregui/Models/JobItem.cpp
+++ GUI/coregui/Models/JobItem.cpp
@@ -29,16 +29,6 @@
 #include "JobItemFunctions.h"
 #include "MaskUnitsConverter.h"
 
-namespace {
-    QVariant createStatusVariant() {
-        ComboProperty status;
-        status << Constants::STATUS_IDLE << Constants::STATUS_RUNNING
-               << Constants::STATUS_FITTING << Constants::STATUS_COMPLETED
-               << Constants::STATUS_CANCELED << Constants::STATUS_FAILED;
-        return status.getVariant();
-    }
-}
-
 const QString JobItem::P_IDENTIFIER = "Identifier";
 const QString JobItem::P_SAMPLE_NAME = "Sample";
 const QString JobItem::P_INSTRUMENT_NAME = "Instrument";
@@ -66,10 +56,7 @@ JobItem::JobItem()
     addProperty(P_INSTRUMENT_NAME, QString())->setEditable(false);
     addProperty(P_WITH_FITTING, false)->setVisible(false);
 
-//    ComboProperty status;
-//    status << Constants::STATUS_IDLE << Constants::STATUS_RUNNING << Constants::STATUS_FITTING << Constants::STATUS_COMPLETED
-//           << Constants::STATUS_CANCELED << Constants::STATUS_FAILED;
-    addProperty(P_STATUS, createStatusVariant())->setEditable(false);
+    addProperty(P_STATUS, Constants::STATUS_IDLE)->setEditable(false);
 
     addProperty(P_BEGIN_TIME, QString())->setEditable(false);
     addProperty(P_END_TIME, QString())->setEditable(false);
@@ -85,8 +72,6 @@ JobItem::JobItem()
     registerTag(T_INSTRUMENT, 1, 1, QStringList() << Constants::InstrumentType);
     registerTag(T_OUTPUT, 1, 1, QStringList() << Constants::IntensityDataType);
     registerTag(T_REALDATA, 1, 1, QStringList() << Constants::RealDataType);
-//    registerTag(T_PARAMETER_TREE, 0, -1, QStringList() << Constants::ParameterLabelType
-//                << Constants::ParameterType);
     registerTag(T_PARAMETER_TREE, 0, -1, QStringList() << Constants::ParameterContainerType);
 
     registerTag(T_SIMULATION_OPTIONS, 1, 1, QStringList() << Constants::SimulationOptionsType);
@@ -108,7 +93,7 @@ JobItem::JobItem()
             converter.convertFromNbins(intensityDataItem());
 
         }
-    });    
+    });
 
     mapper()->setOnPropertyChange(
         [this](const QString &name){
@@ -140,15 +125,12 @@ IntensityDataItem *JobItem::intensityDataItem()
 
 QString JobItem::getStatus() const
 {
-    ComboProperty combo_property = getItemValue(P_STATUS).value<ComboProperty>();
-    return combo_property.getValue();
+    return getItemValue(P_STATUS).toString();
 }
 
 void JobItem::setStatus(const QString &status)
 {
-    ComboProperty combo_property = getItemValue(P_STATUS).value<ComboProperty>();
-    combo_property.setValue(status);
-    setItemValue(P_STATUS, combo_property.getVariant());
+    setItemValue(P_STATUS, status);
     if(status == Constants::STATUS_FAILED) {
         if(IntensityDataItem *intensityItem = intensityDataItem()) {
             if(intensityItem->getOutputData())
@@ -160,32 +142,27 @@ void JobItem::setStatus(const QString &status)
 
 bool JobItem::isIdle() const
 {
-    ComboProperty combo_property = getItemValue(P_STATUS).value<ComboProperty>();
-    return combo_property.getValue() == Constants::STATUS_IDLE;
+    return getStatus() == Constants::STATUS_IDLE;
 }
 
 bool JobItem::isRunning() const
 {
-    ComboProperty combo_property = getItemValue(P_STATUS).value<ComboProperty>();
-    return combo_property.getValue() == Constants::STATUS_RUNNING;
+    return getStatus() == Constants::STATUS_RUNNING;
 }
 
 bool JobItem::isCompleted() const
 {
-    ComboProperty combo_property = getItemValue(P_STATUS).value<ComboProperty>();
-    return combo_property.getValue() == Constants::STATUS_COMPLETED;
+    return getStatus() == Constants::STATUS_COMPLETED;
 }
 
 bool JobItem::isCanceled() const
 {
-    ComboProperty combo_property = getItemValue(P_STATUS).value<ComboProperty>();
-    return combo_property.getValue() == Constants::STATUS_CANCELED;
+    return getStatus() == Constants::STATUS_CANCELED;
 }
 
 bool JobItem::isFailed() const
 {
-    ComboProperty combo_property = getItemValue(P_STATUS).value<ComboProperty>();
-    return combo_property.getValue() == Constants::STATUS_FAILED;
+    return getStatus() == Constants::STATUS_FAILED;
 }
 
 bool JobItem::isValidForFitting()

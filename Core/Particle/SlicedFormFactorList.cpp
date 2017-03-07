c7e924cc4b32f9615b5f1f1b896dcb85d9bcf058
Walter Van Herck <w.van.herck@fz-juelich.de>
2017-03-07 11:20:36 +0100

    Switch to sliced form factors and fix bug in MultiLayer::topZToLayerIndex

--- Core/Particle/SlicedFormFactorList.cpp
+++ Core/Particle/SlicedFormFactorList.cpp
@@ -32,20 +32,21 @@ void SlicedFormFactorList::addParticle(IParticle& particle,
                                        const MultiLayer& multilayer, size_t ref_layer_index)
 {
     size_t bottom_layer_index = LayerIndexBottom(particle, multilayer, ref_layer_index);
-//    size_t top_layer_index = LayerIndexTop(particle, multilayer, ref_layer_index);
-//    for (size_t i=top_layer_index; i<bottom_layer_index+1; ++i)
-//    {
-//        kvector_t translation(0.0, 0.0, -ZDifference(multilayer, i, ref_layer_index));
-//        particle.applyTranslation(translation);
-//        ZLimits limits = LayerZLimits(multilayer, i);
-//        m_ff_list.emplace_back(std::unique_ptr<IFormFactor>(
-//                                   particle.createSlicedFormFactor(limits)), i);
-//        ref_layer_index = i;  // particle now has coordinates relative to layer i
-//    }
-    kvector_t translation(0.0, 0.0, -ZDifference(multilayer, bottom_layer_index, ref_layer_index));
-    particle.applyTranslation(translation);
-    m_ff_list.emplace_back(std::unique_ptr<IFormFactor>(particle.createFormFactor()),
-                           bottom_layer_index);
+    size_t top_layer_index = LayerIndexTop(particle, multilayer, ref_layer_index);
+    top_layer_index = std::min(bottom_layer_index, top_layer_index);  // Handle zero size shapes
+    for (size_t i=top_layer_index; i<bottom_layer_index+1; ++i)
+    {
+        kvector_t translation(0.0, 0.0, -ZDifference(multilayer, i, ref_layer_index));
+        particle.applyTranslation(translation);
+        ZLimits limits = LayerZLimits(multilayer, i);
+        m_ff_list.emplace_back(std::unique_ptr<IFormFactor>(
+                                   particle.createSlicedFormFactor(limits)), i);
+        ref_layer_index = i;  // particle now has coordinates relative to layer i
+    }
+//    kvector_t translation(0.0, 0.0, -ZDifference(multilayer, bottom_layer_index, ref_layer_index));
+//    particle.applyTranslation(translation);
+//    m_ff_list.emplace_back(std::unique_ptr<IFormFactor>(particle.createFormFactor()),
+//                           bottom_layer_index);
 }
 
 size_t SlicedFormFactorList::size() const
@@ -89,8 +90,8 @@ size_t LayerIndexTop(const IParticle& particle, const MultiLayer& multilayer,
     std::unique_ptr<IFormFactor> P_ff(particle.createFormFactor());
     std::unique_ptr<IRotation> P_rot(IRotation::createIdentity());
     double position_offset = multilayer.getLayerTopZ(ref_layer_index);
-    double zmin = P_ff->topZ(*P_rot) + position_offset;
-    return multilayer.topZToLayerIndex(zmin);
+    double zmax = P_ff->topZ(*P_rot) + position_offset;
+    return multilayer.topZToLayerIndex(zmax);
 }
 
 double ZDifference(const MultiLayer& multilayer, size_t layer_index, size_t ref_layer_index)

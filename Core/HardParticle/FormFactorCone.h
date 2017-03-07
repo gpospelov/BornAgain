1942256feefc4d304107e426ac25e1ac22cbecb3
Walter Van Herck <w.van.herck@fz-juelich.de>
2017-03-07 14:42:41 +0100

    FormFactorCone::sliceFormFactor implemented (for z-rotations only)

--- Core/HardParticle/FormFactorCone.h
+++ Core/HardParticle/FormFactorCone.h
@@ -43,6 +43,9 @@ public:
     complex_t evaluate_for_q (const cvector_t q) const override final;
 
 protected:
+    IFormFactor* sliceFormFactor(ZLimits limits, const IRotation& rot,
+                                 kvector_t translation) const override final;
+
     void onChange() override final;
 
 private:

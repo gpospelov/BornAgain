5700e1573cb567d53ebafcf07dd9510ed3093cee
Walter Van Herck <w.van.herck@fz-juelich.de>
2017-03-08 14:15:54 +0100

    Remove restriction on angle for pyramid (since this is used in the slicing of the cuboctahedron form factor)

--- Core/HardParticle/FormFactorPyramid.cpp
+++ Core/HardParticle/FormFactorPyramid.cpp
@@ -45,7 +45,7 @@ FormFactorPyramid::FormFactorPyramid(double base_edge, double height, double alp
     setName(BornAgain::FFPyramidType);
     registerParameter(BornAgain::BaseEdge, &m_base_edge).setUnit("nm").setNonnegative();
     registerParameter(BornAgain::Height, &m_height).setUnit("nm").setNonnegative();
-    registerParameter(BornAgain::Alpha, & m_alpha).setUnit("rad").setLimited(0., M_PI_2);
+    registerParameter(BornAgain::Alpha, & m_alpha).setUnit("rad").setLimited(0., M_PI);
     onChange();
 }
 

135c3acf5125ae319c39189964039873c6e078c4
Walter Van Herck <w.van.herck@fz-juelich.de>
2017-03-15 14:18:57 +0100

    Added SSCAHelper class

---
+++ Core/Multilayer/SSCAHelper.cpp
@@ -0,0 +1,56 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Core/Multilayer/SSCAHelper.cpp
+//! @brief     Implements classes SSCAHelper.
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2017
+//! @authors   Scientific Computing Group at MLZ Garching
+//! @authors   J. Burle, J. M. Fisher, M. Ganeva, G. Pospelov, W. Van Herck, J. Wuttke
+//
+// ************************************************************************** //
+
+#include "SSCAHelper.h"
+#include "Exceptions.h"
+#include "FormFactorCoherentSum.h"
+#include "InterferenceFunctionRadialParaCrystal.h"
+
+
+SSCAHelper::SSCAHelper(const SafePointerVector<FormFactorCoherentSum>& ff_wrappers)
+    : m_mean_radius {}
+{
+    for (const auto ffw: ff_wrappers)
+        m_mean_radius += ffw->relativeAbundance() * ffw->radialExtension();
+}
+
+complex_t SSCAHelper::getCharacteristicDistribution(
+        double qp, const IInterferenceFunction* p_iff) const
+{
+    const InterferenceFunctionRadialParaCrystal *p_iff_radial
+        = dynamic_cast<const InterferenceFunctionRadialParaCrystal*>(p_iff);
+    if (p_iff_radial == 0)
+        throw Exceptions::ClassInitializationException("Wrong interference function for SSCA");
+    return p_iff_radial->FTPDF(qp);
+}
+
+complex_t SSCAHelper::getCharacteristicSizeCoupling(double qp, double kappa,
+        const SafePointerVector<FormFactorCoherentSum>& ff_wrappers) const
+{
+    complex_t result {};
+    for (auto ffw : ff_wrappers)
+    {
+        double radial_extension = ffw->radialExtension();
+        result += ffw->relativeAbundance() *
+                  calculatePositionOffsetPhase(qp, kappa, radial_extension);
+    }
+    return result;
+}
+
+complex_t SSCAHelper::calculatePositionOffsetPhase(double qp, double kappa,
+                                                   double radial_extension) const
+{
+    return exp_I(kappa * qp * (radial_extension - m_mean_radius));
+}
---
+++ Core/Multilayer/SSCAHelper.h
@@ -0,0 +1,43 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Core/Multilayer/SSCAHelper.h
+//! @brief     Defines classes SSCAHelper.
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2017
+//! @authors   Scientific Computing Group at MLZ Garching
+//! @authors   J. Burle, J. M. Fisher, M. Ganeva, G. Pospelov, W. Van Herck, J. Wuttke
+//
+// ************************************************************************** //
+
+#ifndef SSCAHELPER_H
+#define SSCAHELPER_H
+
+#include "Complex.h"
+#include "SafePointerVector.h"
+
+class FormFactorCoherentSum;
+class IInterferenceFunction;
+
+//! Helper class for SSCApproximationStrategy, offering some methods, shared between
+//! the scalar and polarized scattering calculations
+//! @ingroup algorithms_internal
+
+class SSCAHelper
+{
+public:
+    SSCAHelper(const SafePointerVector<FormFactorCoherentSum>& ff_wrappers);
+
+    complex_t getCharacteristicDistribution(double qp, const IInterferenceFunction* p_iff) const;
+    complex_t getCharacteristicSizeCoupling(double qp, double kappa,
+            const SafePointerVector<FormFactorCoherentSum>& ff_wrappers) const;
+
+private:
+    complex_t calculatePositionOffsetPhase(double qp, double kappa, double radial_extension) const;
+    double m_mean_radius;
+};
+
+#endif // SSCAHELPER_H

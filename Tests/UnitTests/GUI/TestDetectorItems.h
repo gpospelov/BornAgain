a0de559e3ca70b3f032e6d6b6e44437466844802
Gennady Pospelov <g.pospelov@fz-juelich.de>
2017-03-15 10:39:13 +0100

    Fixed bug in detector resolution in the case of non-default detector units.

--- Tests/UnitTests/GUI/TestDetectorItems.h
+++ Tests/UnitTests/GUI/TestDetectorItems.h
@@ -2,12 +2,19 @@
 #include "RectangularDetectorItem.h"
 #include "InstrumentModel.h"
 #include "ComboProperty.h"
+#include "InstrumentItem.h"
+#include "DetectorItems.h"
+#include "ConvolutionDetectorResolution.h"
+#include "ResolutionFunction2DGaussian.h"
+#include "IDetector2D.h"
+#include "Units.h"
 
 class TestDetectorItems : public QObject {
     Q_OBJECT
 
 private slots:
     void test_detectorAlignment();
+    void test_resolutionFunction();
 };
 
 inline void TestDetectorItems::test_detectorAlignment()
@@ -28,4 +35,27 @@ inline void TestDetectorItems::test_detectorAlignment()
     detector->setItemValue(RectangularDetectorItem::P_ALIGNMENT,
                            QVariant::fromValue<ComboProperty>(alignment));
     QVERIFY(detector->getItem(RectangularDetectorItem::P_NORMAL)->isVisible() == false);
+}
+
+inline void TestDetectorItems::test_resolutionFunction()
+{
+    InstrumentModel model;
+    InstrumentItem* instrument = dynamic_cast<InstrumentItem*>(
+                model.insertNewItem(Constants::InstrumentType));
+
+    DetectorItem *detectorItem = instrument->detectorItem();
+
+    detectorItem->setGroupProperty(DetectorItem::P_RESOLUTION_FUNCTION,
+                                   Constants::ResolutionFunction2DGaussianType);
+
+    auto detector = detectorItem->createDetector();
+
+    auto convol = dynamic_cast<const ConvolutionDetectorResolution*>(detector->detectorResolution());
+
+    auto gaussian = dynamic_cast<const ResolutionFunction2DGaussian*>(convol->getResolutionFunction2D());
+
+
+    QCOMPARE(Units::rad2deg(gaussian->getSigmaX()), 0.02);
+
+
 }

51ce7ba8475c789256489fcc74e85a4c732f4919
Gennady Pospelov <g.pospelov@fz-juelich.de>
2017-03-30 16:28:54 +0200

    FIXME removed (cleanup in GUI mapper unit tests)

--- Tests/UnitTests/GUI/TestMapperForItem.h
+++ Tests/UnitTests/GUI/TestMapperForItem.h
@@ -8,16 +8,14 @@
 
 
 //! Test Widget which logs calling activity of ModelMapper
-class Widget {
+class Widget
+{
 public:
     Widget()
-        : m_onPropertyChangeCount(0)
-        , m_onChildPropertyChangeCount(0)
-        , m_onParentChangeCount(0)
-        , m_onChildrenChangeCount(0)
-        , m_onSiblingsChangeCount(0)
-        , m_onAboutToRemoveChild(0)
-    { }
+        : m_onPropertyChangeCount(0), m_onChildPropertyChangeCount(0), m_onParentChangeCount(0),
+          m_onChildrenChangeCount(0), m_onSiblingsChangeCount(0), m_onAboutToRemoveChild(0)
+    {
+    }
 
     void clear()
     {
@@ -31,85 +29,53 @@ public:
         m_reported_names.clear();
     }
 
-    void subscribe(ModelMapper *mapper, bool with_subscription = false)
+    void subscribe(ModelMapper* mapper, bool with_subscription = false)
     {
         clear();
 
-        void *caller = (with_subscription ? this : 0);
+        void* caller = (with_subscription ? this : 0);
 
-        mapper->setOnPropertyChange(
-                    [this] (QString name)
-        {
-            onPropertyChange(name);
-        }, caller);
+        mapper->setOnPropertyChange([this](QString name) { onPropertyChange(name); }, caller);
 
         mapper->setOnChildPropertyChange(
-                    [this](SessionItem* item, QString name)
-        {
-            onChildPropertyChange(item, name);
-        }, caller);
+            [this](SessionItem* item, QString name) { onChildPropertyChange(item, name); }, caller);
 
-        mapper->setOnParentChange(
-                    [this](SessionItem *parent) {
-            onParentChange(parent);
-        }, caller);
+        mapper->setOnParentChange([this](SessionItem* parent) { onParentChange(parent); }, caller);
 
+        mapper->setOnChildrenChange([this](SessionItem*) { onChildrenChange(); }, caller);
 
-        mapper->setOnChildrenChange(
-                    [this](SessionItem*)
-        {
-            onChildrenChange();
-        }, caller);
+        mapper->setOnSiblingsChange([this]() { onSiblingsChange(); }, caller);
 
-        mapper->setOnSiblingsChange(
-                    [this]()
-        {
-            onSiblingsChange();
-        }, caller);
-
-
-        mapper->setOnAboutToRemoveChild(
-                    [this](SessionItem* item)
-        {
-            onAboutToRemoveChild(item);
-        }, caller);
-
+        mapper->setOnAboutToRemoveChild([this](SessionItem* item) { onAboutToRemoveChild(item); },
+                                        caller);
     }
 
-    void onPropertyChange(const QString &name)
+    void onPropertyChange(const QString& name)
     {
         m_reported_names.append(name);
         m_onPropertyChangeCount++;
     }
 
-    void onChildPropertyChange(SessionItem *item, const QString &name)
+    void onChildPropertyChange(SessionItem* item, const QString& name)
     {
         m_reported_items.append(item);
         m_reported_names.append(name);
         m_onChildPropertyChangeCount++;
     }
 
-    void onParentChange(SessionItem *item)
+    void onParentChange(SessionItem* item)
     {
         m_reported_items.append(item);
         m_onParentChangeCount++;
     }
 
-    void onChildrenChange()
-    {
-        m_onChildrenChangeCount++;
-    }
+    void onChildrenChange() { m_onChildrenChangeCount++; }
 
-    void onSiblingsChange()
-    {
-        m_onSiblingsChangeCount++;
-    }
+    void onSiblingsChange() { m_onSiblingsChangeCount++; }
 
-    void unsubscribe(ModelMapper *mapper) {
-        mapper->unsubscribe(this);
-    }
+    void unsubscribe(ModelMapper* mapper) { mapper->unsubscribe(this); }
 
-    void onAboutToRemoveChild(SessionItem *item)
+    void onAboutToRemoveChild(SessionItem* item)
     {
         m_reported_items.append(item);
         m_onAboutToRemoveChild++;
@@ -121,32 +87,30 @@ public:
     int m_onChildrenChangeCount;
     int m_onSiblingsChangeCount;
     int m_onAboutToRemoveChild;
-    QList<SessionItem *> m_reported_items;
+    QList<SessionItem*> m_reported_items;
     QStringList m_reported_names;
 };
 
-
 //! Test if ModelMapper reports correctly (number of callbacks,
 //! names of properties and reporting items)
-class TestMapperForItem : public QObject {
+class TestMapperForItem : public QObject
+{
     Q_OBJECT
 
 public:
-    TestMapperForItem(QObject *parent = 0)
-        : QObject(parent)
-        , m_mapped_item(0)
-    { }
+    TestMapperForItem(QObject* parent = 0) : QObject(parent), m_mapped_item(0) {}
 
-    void setItem(SessionItem *item, Widget *widget = 0, bool with_subscription = false)
+    void setItem(SessionItem* item, Widget* widget = 0, bool with_subscription = false)
     {
         m_mapped_item = item;
         m_mapper.reset(new ModelMapper);
         m_mapper->setItem(item);
-        if(widget) widget->subscribe(m_mapper.get(), with_subscription);
+        if (widget)
+            widget->subscribe(m_mapper.get(), with_subscription);
     }
 
 private:
-    SessionItem *m_mapped_item;
+    SessionItem* m_mapped_item;
     std::unique_ptr<ModelMapper> m_mapper;
 
 private slots:
@@ -178,8 +142,8 @@ inline void TestMapperForItem::test_onPropertyChange()
 {
     Widget w;
     SampleModel model;
-    SessionItem *multilayer = model.insertNewItem(Constants::MultiLayerType);
-    SessionItem *layer = model.insertNewItem(Constants::LayerType, model.indexOfItem(multilayer));
+    SessionItem* multilayer = model.insertNewItem(Constants::MultiLayerType);
+    SessionItem* layer = model.insertNewItem(Constants::LayerType, model.indexOfItem(multilayer));
 
     // Mapper is looking on child; set property of child
     setItem(layer, &w);
@@ -215,7 +179,7 @@ inline void TestMapperForItem::test_onPropertyChange()
     QCOMPARE(w.m_onParentChangeCount, 0);
     QCOMPARE(w.m_onChildrenChangeCount, 0);
     QCOMPARE(w.m_onSiblingsChangeCount, 0);
-    QVERIFY( (w.m_reported_items.size() == 1) && (w.m_reported_items[0] == layer));
+    QVERIFY((w.m_reported_items.size() == 1) && (w.m_reported_items[0] == layer));
     QVERIFY((w.m_reported_names.size() == 1) && (w.m_reported_names[0] == LayerItem::P_THICKNESS));
 
     // Mapper is looking on parent; set property of parent;
@@ -228,29 +192,26 @@ inline void TestMapperForItem::test_onPropertyChange()
     QCOMPARE(w.m_onChildrenChangeCount, 0);
     QCOMPARE(w.m_onSiblingsChangeCount, 0);
     QVERIFY(w.m_reported_items.isEmpty());
-    QVERIFY((w.m_reported_names.size() == 1) && (w.m_reported_names[0] == MultiLayerItem::P_CROSS_CORR_LENGTH));
+    QVERIFY((w.m_reported_names.size() == 1)
+            && (w.m_reported_names[0] == MultiLayerItem::P_CROSS_CORR_LENGTH));
 }
 
 inline void TestMapperForItem::test_onParentChange()
 {
     Widget w;
     SampleModel model;
-    SessionItem *multilayer = model.insertNewItem(Constants::MultiLayerType);
-    SessionItem *layer = model.insertNewItem(Constants::LayerType, model.indexOfItem(multilayer));
-//    SessionItem *layer = model.insertNewItem(Constants::LayerType);
+    SessionItem* multilayer = model.insertNewItem(Constants::MultiLayerType);
+    SessionItem* layer = model.insertNewItem(Constants::LayerType, model.indexOfItem(multilayer));
 
     // Mapper is looking on child; changing child's parent
     setItem(layer, &w);
     QVERIFY(m_mapped_item == layer);
     multilayer->takeRow(layer->parentRow());
-//    model.moveParameterizedItem(layer, multilayer);
-    // FIXME check onParentChange while moving an item
 
     QCOMPARE(w.m_onPropertyChangeCount, 0);
     QCOMPARE(w.m_onChildPropertyChangeCount, 0);
     QCOMPARE(w.m_onParentChangeCount, 1);
     QCOMPARE(w.m_onChildrenChangeCount, 0);
-    //QVERIFY((w.m_reported_items.size() == 1) && (w.m_reported_items[0] == nullptr));
     QVERIFY(w.m_reported_names.isEmpty());
 }
 
@@ -258,7 +219,7 @@ inline void TestMapperForItem::test_onChildrenChange()
 {
     Widget w;
     SampleModel model;
-    SessionItem *multilayer = model.insertNewItem(Constants::MultiLayerType);
+    SessionItem* multilayer = model.insertNewItem(Constants::MultiLayerType);
 
     // Mapper is looking on parent; adding new child to parent
     setItem(multilayer, &w);
@@ -278,13 +239,13 @@ inline void TestMapperForItem::test_onSiblingsChange()
 {
     Widget w;
     SampleModel model;
-    SessionItem *multilayer = model.insertNewItem(Constants::MultiLayerType);
-    SessionItem *layer = model.insertNewItem(Constants::LayerType, model.indexOfItem(multilayer));
+    SessionItem* multilayer = model.insertNewItem(Constants::MultiLayerType);
+    SessionItem* layer = model.insertNewItem(Constants::LayerType, model.indexOfItem(multilayer));
 
     // Mapper is looking on child; adding another child to parent
     setItem(layer, &w);
     QVERIFY(m_mapped_item == layer);
-    SessionItem *layer2 = model.insertNewItem(Constants::LayerType, model.indexOfItem(multilayer));
+    SessionItem* layer2 = model.insertNewItem(Constants::LayerType, model.indexOfItem(multilayer));
     Q_UNUSED(layer2);
 
     QCOMPARE(w.m_onPropertyChangeCount, 0);
@@ -297,18 +258,14 @@ inline void TestMapperForItem::test_onSiblingsChange()
 
     multilayer->takeItem(1, MultiLayerItem::T_LAYERS);
     QCOMPARE(w.m_onSiblingsChangeCount, 2);
-
-    // FIXME
-//    multilayer->takeRow(layer2->parentRow());
-    //    QCOMPARE(m_onSiblingsChangeCount, 2);
 }
 
 inline void TestMapperForItem::test_Subscription()
 {
     Widget w;
     SampleModel model;
-    SessionItem *multilayer = model.insertNewItem(Constants::MultiLayerType);
-    SessionItem *layer = model.insertNewItem(Constants::LayerType, model.indexOfItem(multilayer));
+    SessionItem* multilayer = model.insertNewItem(Constants::MultiLayerType);
+    SessionItem* layer = model.insertNewItem(Constants::LayerType, model.indexOfItem(multilayer));
 
     // Mapper is looking on child; set property of child
     setItem(layer, &w, true);
@@ -335,8 +292,8 @@ inline void TestMapperForItem::test_TwoWidgetsSubscription()
 {
     Widget w1, w2;
     SampleModel model;
-    SessionItem *multilayer = model.insertNewItem(Constants::MultiLayerType);
-    SessionItem *layer = model.insertNewItem(Constants::LayerType, model.indexOfItem(multilayer));
+    SessionItem* multilayer = model.insertNewItem(Constants::MultiLayerType);
+    SessionItem* layer = model.insertNewItem(Constants::LayerType, model.indexOfItem(multilayer));
 
     // Mapper is looking on child; set property of child
     setItem(layer);
@@ -353,16 +310,15 @@ inline void TestMapperForItem::test_TwoWidgetsSubscription()
     layer->setItemValue(LayerItem::P_THICKNESS, 2.0);
     QCOMPARE(w1.m_onPropertyChangeCount, 1);
     QCOMPARE(w2.m_onPropertyChangeCount, 2);
-
 }
 
 inline void TestMapperForItem::test_AboutToRemoveChild()
 {
     Widget w;
     SampleModel model;
-    SessionItem *container = model.insertNewItem(Constants::ProjectionContainerType);
+    SessionItem* container = model.insertNewItem(Constants::ProjectionContainerType);
 
-    SessionItem *line = model.insertNewItem(Constants::HorizontalLineMaskType, container->index());
+    SessionItem* line = model.insertNewItem(Constants::HorizontalLineMaskType, container->index());
 
     setItem(container, &w);
     QCOMPARE(w.m_onAboutToRemoveChild, 0);
@@ -372,5 +328,4 @@ inline void TestMapperForItem::test_AboutToRemoveChild()
     QCOMPARE(w.m_onAboutToRemoveChild, 1);
     QCOMPARE(w.m_reported_items.size(), 1);
     QCOMPARE(w.m_reported_items.back(), line);
-
 }

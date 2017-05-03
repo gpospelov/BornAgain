af31afe339911d8847317c92b8549ba0598750a0
Gennady Pospelov <g.pospelov@fz-juelich.de>
2017-05-03 14:01:00 +0200

    Possibility to adjust detector size in original py persistence tests.

--- Tests/Functional/PyCore/persistence/example_template.py
+++ Tests/Functional/PyCore/persistence/example_template.py
@@ -15,21 +15,53 @@ sys.path.append(example_dir)
 example = __import__(example_name)
 
 
-def test_simulation():
+def get_default_simulation():
     """
     Returns a GISAXS simulation.
     """
     simulation = ba.GISASSimulation()
-    simulation.setSample(example.get_sample())
     simulation.setDetectorParameters(5, -1*deg, +1*deg, 6, 0*deg, +2*deg)
     simulation.setBeamParameters(1.5*angstrom, 0.15*deg, 0.0*deg)
     return simulation
 
 
-if __name__ == '__main__':
-    simulation = test_simulation()
+def get_minified_simulation():
+    """
+    Returns a GISAXS simulation constructed from example simulation with smaller detector
+    """
+    simulation = example.get_simulation()
+    detector = simulation.getInstrument().getDetector()
+
+    # preserving axes range, making less bins
+    ax = detector.getAxis(0)
+    ay = detector.getAxis(1)
+    xmin, xmax = ax.getMin(), ax.getMax()
+    ymin, ymax = ay.getMin(), ay.getMax()
+
+    simulation.setDetectorParameters(5, xmin, xmax, 6, ymin, ymax)
+    return simulation
+
+
+def get_simulation():
+
+    if "CustomFormFactor" in example_name:
+        return get_minified_simulation()
+    else:
+        return get_default_simulation()
+
+
+def run_simulation():
+    """
+    Runs simulation and returns resulting intensity map.
+    """
+    simulation = get_simulation()
+    simulation.setSample(example.get_sample())
     simulation.runSimulation()
-    result = simulation.getIntensityData()
+    return simulation.getIntensityData()
+
+
+if __name__ == '__main__':
+    result = run_simulation()
 
     filename = os.path.join(output_dir, example_name+".ref.int.gz")
     print("example_template.py -> Writing results in '{0}'".format(filename))

98e466b7deb98b085348cd3817c5d0193349ec1e
Gennady Pospelov <g.pospelov@fz-juelich.de>
2017-05-03 13:17:27 +0200

    ex03 python persistence test switched to the smaller detector

--- Examples/python/simulation/ex03_InterferenceFunctions/CosineRipplesAtRectLattice.py
+++ Examples/python/simulation/ex03_InterferenceFunctions/CosineRipplesAtRectLattice.py
@@ -5,9 +5,6 @@ import numpy
 import bornagain as ba
 from bornagain import deg, angstrom, nm
 
-phi_min, phi_max = -1.5, 1.5
-alpha_min, alpha_max = 0.0, 2.5
-
 
 def get_sample():
     """
@@ -49,8 +46,8 @@ def get_simulation():
     characterizing the input beam and output detector
     """
     simulation = ba.GISASSimulation()
-    simulation.setDetectorParameters(100, phi_min*deg, phi_max*deg,
-                                     100, alpha_min*deg, alpha_max*deg)
+    simulation.setDetectorParameters(100, -1.5*deg, 1.5*deg,
+                                     100, 0.0*deg, 2.5*deg)
     simulation.setBeamParameters(1.6*angstrom, 0.3*deg, 0.0*deg)
     return simulation
 
@@ -59,9 +56,8 @@ def run_simulation():
     """
     Runs simulation and returns intensity map.
     """
-    sample = get_sample()
     simulation = get_simulation()
-    simulation.setSample(sample)
+    simulation.setSample(get_sample())
     simulation.runSimulation()
     return simulation.getIntensityData()
 

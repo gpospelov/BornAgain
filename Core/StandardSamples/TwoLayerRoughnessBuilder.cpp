1c2cde47c92c544a18efea677d5e5b87e294d0b8
Walter Van Herck <w.van.herck@fz-juelich.de>
2017-05-29 12:48:00 +0200

    Added GUITranslationTest for two layers with rough interface (failing with the current bug)

---
+++ Core/StandardSamples/TwoLayerRoughnessBuilder.cpp
@@ -0,0 +1,55 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Core/StandardSamples/TwoLayerRoughnessBuilder.cpp
+//! @brief     Implement class TwoLayerRoughnessBuilder.
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2015
+//! @authors   Scientific Computing Group at MLZ Garching
+//! @authors   C. Durniak, M. Ganeva, G. Pospelov, W. Van Herck, J. Wuttke
+//
+// ************************************************************************** //
+
+#include "TwoLayerRoughnessBuilder.h"
+#include "HomogeneousMaterial.h"
+#include "Layer.h"
+#include "LayerRoughness.h"
+#include "MultiLayer.h"
+#include "RealParameter.h"
+#include "Units.h"
+
+TwoLayerRoughnessBuilder::TwoLayerRoughnessBuilder()
+    : m_sigma(1.0*Units::nanometer)
+    , m_hurst(0.3)
+    , m_lateralCorrLength(5.0*Units::nanometer)
+{
+    init_parameters();
+}
+
+
+void TwoLayerRoughnessBuilder::init_parameters()
+{
+    registerParameter("sigma", &m_sigma).setUnit("nm").setNonnegative();
+    registerParameter("hurst", &m_hurst);
+    registerParameter("lateralCorrLength", &m_lateralCorrLength).setUnit("nm").setNonnegative();
+}
+
+
+MultiLayer* TwoLayerRoughnessBuilder::buildSample() const
+{
+    MultiLayer* multi_layer = new MultiLayer();
+    HomogeneousMaterial air_material("Air", 0., 0.);
+    HomogeneousMaterial substrate_material("Substrate", 15e-6, 0.0);
+
+    Layer air_layer(air_material, 0);
+    Layer substrate_layer(substrate_material, 0);
+
+    LayerRoughness roughness(m_sigma, m_hurst, m_lateralCorrLength);
+
+    multi_layer->addLayer(air_layer);
+    multi_layer->addLayerWithTopRoughness(substrate_layer, roughness);
+    return multi_layer;
+}

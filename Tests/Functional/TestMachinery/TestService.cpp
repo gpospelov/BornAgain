056d83908ee222379cd457885e119e820ad5057e
Gennady Pospelov <g.pospelov@fz-juelich.de>
2017-09-22 13:03:18 +0200

    New TestService to replace IStandardTest machinery

---
+++ Tests/Functional/TestMachinery/TestService.cpp
@@ -0,0 +1,85 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Tests/Functional/TestMachinery/TestService.h
+//! @brief     Defines class TestService.
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2015
+//! @authors   Scientific Computing Group at MLZ Garching
+//! @authors   C. Durniak, M. Ganeva, G. Pospelov, W. Van Herck, J. Wuttke
+//
+// ************************************************************************** //
+
+#include "TestService.h"
+#include "StandardTestCatalogue.h"
+#include "IMultiLayerBuilder.h"
+#include "SampleBuilderFactory.h"
+#include "SimulationFactory.h"
+#include "GISASSimulation.h"
+#include "MultiLayer.h"
+#include "IFunctionalTest.h"
+#include "TestUtils.h"
+#include "BornAgainNamespace.h"
+#include <iostream>
+
+namespace {
+
+//! Returns test full name, composed from the test name and the name of sample builder.
+
+std::string fullTestName(const std::string& test_name, const std::string& builder_name)
+{
+    std::string result = test_name;
+
+    if (builder_name != BornAgain::SampleBuilderType)
+        result += "_" + builder_name;
+
+    return result;
+}
+
+}
+
+
+//! Runs test corresponding to given command line parameters, returns true if success.
+
+bool TestServiceBase::execute(int argc, char** argv)
+{
+    StandardTestInfo info = TestUtils::testInfo(argc, argv);
+    if (info.m_test_name.empty())
+        return false;
+
+    std::unique_ptr<IMultiLayerBuilder> builder(
+                SampleBuilderFactory().createItem(info.m_sample_builder_name) );
+
+    size_t n_subtests = builder->size();
+    int number_of_failed_tests = 0;
+
+    for(size_t sample_index=0; sample_index<builder->size(); ++sample_index) {
+        std::unique_ptr<GISASSimulation> simulation(
+                    SimulationFactory().createItem(info.m_simulation_name));
+
+        std::unique_ptr<MultiLayer> sample(builder->createSample(sample_index));
+        simulation->setSample(*sample);
+
+        std::string test_name = fullTestName(info.m_test_name, builder->getName());
+
+        std::unique_ptr<IFunctionalTest> test(createTest(test_name, info.m_test_description,
+                                           simulation.release(), info.m_threshold));
+
+        if(n_subtests)
+            std::cout << "IStandardTest::execute() -> " << test_name << " " << sample_index+1
+                      << "/" << n_subtests << " (" << builder->getName() << ")\n";
+
+        if(!test->runTest())
+            ++number_of_failed_tests;
+
+    }
+
+    if(number_of_failed_tests)
+        std::cout << "summary: " << number_of_failed_tests << " of "
+                  << n_subtests << " subtests failed\n";
+
+    return number_of_failed_tests==0;
+}
---
+++ Tests/Functional/TestMachinery/TestService.h
@@ -0,0 +1,61 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Tests/Functional/TestMachinery/TestService.h
+//! @brief     Defines class TestService.
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2015
+//! @authors   Scientific Computing Group at MLZ Garching
+//! @authors   C. Durniak, M. Ganeva, G. Pospelov, W. Van Herck, J. Wuttke
+//
+// ************************************************************************** //
+
+#ifndef TESTSERVICE_H
+#define TESTSERVICE_H
+
+#include <string>
+class IFunctionalTest;
+class GISASSimulation;
+
+//! @class TestServiceBase
+//! @ingroup standard_samples
+//! @brief Main class to run standard functional test from standalone executable.
+
+class TestServiceBase
+{
+public:
+    virtual ~TestServiceBase() = default;
+    bool execute(int argc, char** argv);
+
+private:
+    virtual IFunctionalTest* createTest(const std::string& name, const std::string& description,
+                                        GISASSimulation* simulation, double threshold) = 0;
+};
+
+
+//! @class TestService
+//! @ingroup standard_samples
+//! @brief Extends main class to run standard functional test from standalone executable.
+//! Concrete test type is templated.
+
+template<typename T>
+class TestService : public TestServiceBase
+{
+
+private:
+    virtual IFunctionalTest* createTest(const std::string& name, const std::string& description,
+                                        GISASSimulation* simulation, double threshold)
+    {
+        return createStandardTest(name, description, simulation, threshold);
+    }
+
+    T* createStandardTest(const std::string& name, const std::string& description,
+                          GISASSimulation* simulation, double threshold) {
+        return new T(name, description, simulation, threshold);
+    }
+};
+
+#endif // TESTSERVICE_H

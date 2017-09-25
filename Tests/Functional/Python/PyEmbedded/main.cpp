5888b26eae7cecd91b084d6e72f50c42ee2285fb
Gennady Pospelov <g.pospelov@fz-juelich.de>
2017-09-25 16:04:09 +0200

    Placeholder for PyEmbedded functional tests.

---
+++ Tests/Functional/Python/PyEmbedded/main.cpp
@@ -0,0 +1,23 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Tests/Functional/Python/PyEmbedded/main.cpp
+//! @brief     Implements program PyEmbedded to run functional tests
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2015
+//! @authors   Scientific Computing Group at MLZ Garching
+//! @authors   C. Durniak, M. Ganeva, G. Pospelov, W. Van Herck, J. Wuttke
+//
+// ************************************************************************** //
+
+#include <iostream>
+
+//! Runs PyEmbedded functional test.
+int main(int /*argc*/, char** /*argv*/)
+{
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}

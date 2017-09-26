bcbc177141afda6fd7178de166972bc63b44ebdf
Gennady Pospelov <g.pospelov@fz-juelich.de>
2017-09-26 14:13:46 +0200

    IFunctionalTest.cpp introduced and destructor moved there to avoid virtual table duplications.

---
+++ Tests/Functional/TestMachinery/IFunctionalTest.cpp
@@ -0,0 +1,18 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Tests/Functional/TestMachinery/IFunctionalTest.cpp
+//! @brief     Implements IFunctionalTest class.
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2015
+//! @authors   Scientific Computing Group at MLZ Garching
+//! @authors   C. Durniak, M. Ganeva, G. Pospelov, W. Van Herck, J. Wuttke
+//
+// ************************************************************************** //
+
+#include "IFunctionalTest.h"
+
+IFunctionalTest::~IFunctionalTest() = default;
--- Tests/Functional/TestMachinery/IFunctionalTest.h
+++ Tests/Functional/TestMachinery/IFunctionalTest.h
@@ -3,7 +3,7 @@
 //  BornAgain: simulate and fit scattering at grazing incidence
 //
 //! @file      Tests/Functional/TestMachinery/IFunctionalTest.h
-//! @brief     Defines and implements pure virtual base class IFunctionalTest.
+//! @brief     Defines IFunctionalTest class.
 //!
 //! @homepage  http://www.bornagainproject.org
 //! @license   GNU General Public License v3 or higher (see COPYING)
@@ -28,6 +28,7 @@ public:
     IFunctionalTest() {}
     IFunctionalTest(const std::string& name, const std::string& description)
         : INamed(name), m_description(description) {}
+    virtual ~IFunctionalTest();
 
     virtual bool runTest() = 0; //!< Returns true when successful
 

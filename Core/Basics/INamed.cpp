28af740d8dce9c1fd05644ae83140c15bb881e2c
Gennady Pospelov <g.pospelov@fz-juelich.de>
2017-09-26 14:09:38 +0200

    INamed.cpp created and destructor moved there to avoid duplicated vtables.

--- Core/Basics/INamed.cpp
+++ Core/Basics/INamed.cpp
@@ -2,8 +2,8 @@
 //
 //  BornAgain: simulate and fit scattering at grazing incidence
 //
-//! @file      Core/Basics/INamed.h
-//! @brief     Defines INamed class.
+//! @file      Core/Basics/INamed.cpp
+//! @brief     Implements INamed class.
 //!
 //! @homepage  http://www.bornagainproject.org
 //! @license   GNU General Public License v3 or higher (see COPYING)
@@ -13,28 +13,8 @@
 //
 // ************************************************************************** //
 
-#ifndef INAMED_H
-#define INAMED_H
+#include "INamed.h"
 
-#include <string>
-#include "WinDllMacros.h"
+INamed::~INamed() = default;
 
-//! Interface for named objects.
-//! @ingroup tools_internal
 
-class BA_CORE_API_ INamed
-{
-public:
-    INamed() : m_name() {}
-    INamed(const std::string& name) : m_name(name) {}
-    virtual ~INamed();
-    std::string getName() const { return m_name; }
-
-protected:
-    void setName(const std::string& name) { m_name = name; }
-
-private:
-    std::string m_name;
-};
-
-#endif // INAMED_H

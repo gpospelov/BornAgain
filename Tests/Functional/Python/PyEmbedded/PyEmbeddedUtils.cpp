3a925c85f8a64ce12b38e1c8945fc78f22ff3b15
Gennady Pospelov <g.pospelov@fz-juelich.de>
2017-09-27 15:26:03 +0200

    New PyEmbeddedUtils namespace for various converters from embedded Python.

---
+++ Tests/Functional/Python/PyEmbedded/PyEmbeddedUtils.cpp
@@ -0,0 +1,58 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Tests/Functional/Python/PyEmbedded/PyEmbeddedUtils.cpp
+//! @brief     IOmplements various functions from PyEmbeddedUtils namespace
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2015
+//! @authors   Scientific Computing Group at MLZ Garching
+//! @authors   C. Durniak, M. Ganeva, G. Pospelov, W. Van Herck, J. Wuttke
+//
+// ************************************************************************** //
+
+#include "PyEmbeddedUtils.h"
+#include <stdexcept>
+#include <iostream>
+
+std::string PyEmbeddedUtils::toString(PyObject* obj, bool decref)
+{
+    std::string result;
+#if PY_MAJOR_VERSION >= 3
+    PyObject* pyStr = PyUnicode_AsEncodedString(object, "utf-8", "Error ~");
+    result = std::string(PyBytes_AsString(pyStr));
+#else
+    result = std::string(PyString_AsString(obj));
+#endif
+
+    if (decref)
+        Py_DECREF(obj);
+}
+
+std::vector<std::string> PyEmbeddedUtils::toVectorString(PyObject* obj, bool decref)
+{
+    std::vector<std::string> result;
+    if (PyTuple_Check(obj)) {
+        for (Py_ssize_t i = 0; i < PyTuple_Size(obj); i++) {
+            PyObject *value = PyTuple_GetItem(obj, i);
+            result.push_back( toString(value) );
+        }
+
+    } else if (PyList_Check(obj)) {
+        for (Py_ssize_t i = 0; i < PyList_Size(obj); i++) {
+            PyObject *value = PyList_GetItem(obj, i);
+            result.push_back( toString(value) );
+        }
+
+    } else {
+        throw std::runtime_error("PyEmbeddedUtils::toVectorString() -> Error. Unexpected object.");
+    }
+
+    if (decref)
+        Py_DECREF(obj);
+
+    return result;
+}
+

cfe6b738061d5d7562fb15bf6e8731bdfe93d9ad
Dmitry Yurov <d.yurov@fz-juelich.de>
2017-10-20 14:50:45 +0200

    Remove refractiveIndex method from Particle class
    Redmine: #1858

--- Tests/UnitTests/Core/Sample/ParticleTest.h
+++ Tests/UnitTests/Core/Sample/ParticleTest.h
@@ -17,7 +17,6 @@ TEST_F(ParticleTest, InitialState)
     Particle particle;
     HomogeneousMaterial vacuum;
     EXPECT_EQ(vacuum, *particle.material());
-    EXPECT_EQ(complex_t(1,0), particle.refractiveIndex());
     EXPECT_EQ(nullptr, particle.createFormFactor());
     EXPECT_EQ(nullptr, particle.rotation());
     EXPECT_EQ(BornAgain::ParticleType, particle.getName());
@@ -29,7 +28,6 @@ TEST_F(ParticleTest, Clone)
     HomogeneousMaterial vacuum;
     std::unique_ptr<Particle> clone(particle.clone());
     EXPECT_EQ(vacuum, *clone->material());
-    EXPECT_EQ(complex_t(1,0), clone->refractiveIndex());
     EXPECT_EQ(nullptr, clone->createFormFactor());
     EXPECT_EQ(nullptr, clone->rotation());
     EXPECT_EQ(BornAgain::ParticleType, clone->getName());
@@ -43,15 +41,13 @@ TEST_F(ParticleTest, Constructors)
 
     // construction with material
     std::unique_ptr<Particle> p1(new Particle(mat));
-    EXPECT_EQ("Air", p1->material()->getName());
-    EXPECT_EQ(complex_t(1,0), p1->refractiveIndex());
+    EXPECT_EQ(mat, *p1->material());
     EXPECT_EQ(nullptr, p1->createFormFactor());
     EXPECT_EQ( nullptr, p1->rotation());
 
     // construction with formfactor
     std::unique_ptr<Particle> p2(new Particle(mat, sphere));
-    EXPECT_EQ("Air", p2->material()->getName());
-    EXPECT_EQ(complex_t(1,0), p2->refractiveIndex());
+    EXPECT_EQ(mat, *p2->material());
     EXPECT_TRUE(dynamic_cast<FormFactorDecoratorMaterial *>(p2->createFormFactor()));
     EXPECT_EQ(complex_t(1,0), dynamic_cast<FormFactorDecoratorMaterial *>(
                       p2->createFormFactor())->getAmbientRefractiveIndex());
@@ -59,8 +55,7 @@ TEST_F(ParticleTest, Constructors)
 
     // construction with transformation
     std::unique_ptr<Particle> p3(new Particle(mat, sphere, transform));
-    EXPECT_EQ("Air", p3->material()->getName());
-    EXPECT_EQ(complex_t(1,0), p3->refractiveIndex());
+    EXPECT_EQ(mat, *p3->material());
     EXPECT_TRUE(dynamic_cast<FormFactorDecoratorMaterial *>(p3->createFormFactor()));
     EXPECT_EQ(complex_t(1,0), dynamic_cast<FormFactorDecoratorMaterial *>(
                       p3->createFormFactor())->getAmbientRefractiveIndex());
@@ -83,8 +78,7 @@ TEST_F(ParticleTest, setters)
 
     std::unique_ptr<Particle> particle2(particle.clone());
     EXPECT_EQ(BornAgain::ParticleType, particle2->getName());
-    EXPECT_EQ(vacuum.getName(), particle2->material()->getName());
-    EXPECT_EQ(complex_t(1.0), particle2->refractiveIndex());
+    EXPECT_EQ(vacuum, *particle2->material());
     EXPECT_TRUE(nullptr != particle2->rotation());
 }
 

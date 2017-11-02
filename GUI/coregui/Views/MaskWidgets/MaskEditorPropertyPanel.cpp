d1355f1720347c6bc9ec4dd40b7c34ec779286d7
Gennady Pospelov <g.pospelov@fz-juelich.de>
2017-11-02 14:11:08 +0100

    Standard code beautification in MaskEditorPropertyPanel

--- GUI/coregui/Views/MaskWidgets/MaskEditorPropertyPanel.cpp
+++ GUI/coregui/Views/MaskWidgets/MaskEditorPropertyPanel.cpp
@@ -19,29 +19,19 @@
 #include "ComponentEditor.h"
 #include "ContentPane.h"
 #include "IntensityDataItem.h"
-#include "MaskEditorFlags.h"
 #include "SessionModel.h"
-#include <QButtonGroup>
-#include <QGridLayout>
-#include <QItemSelection>
-#include <QLabel>
 #include <QListView>
-#include <QModelIndexList>
-#include <QPushButton>
-#include <QToolButton>
 #include <QVBoxLayout>
-#include <QTreeView>
-
 
 //! Widget to cheat Accordion to resize correctly.
 
 class EnvelopWidget : public QWidget
 {
 public:
-    EnvelopWidget(QWidget *content)
+    EnvelopWidget(QWidget* content)
     {
-        QVBoxLayout *mainLayout = new QVBoxLayout;
-        mainLayout->setContentsMargins(0,0,0,0);
+        QVBoxLayout* mainLayout = new QVBoxLayout;
+        mainLayout->setContentsMargins(0, 0, 0, 0);
         mainLayout->setSpacing(0);
         setSizePolicy(QSizePolicy::Minimum, QSizePolicy::MinimumExpanding);
         mainLayout->addWidget(content);
@@ -51,24 +41,20 @@ public:
     QSize sizeHint() const { return QSize(128, 500); }
 };
 
-MaskEditorPropertyPanel::MaskEditorPropertyPanel(QWidget *parent)
-    : QWidget(parent)
-    , m_listView(new QListView)
-    , m_maskPropertyEditor(new ComponentEditor)
-    , m_plotPropertyEditor(new ComponentEditor)
-    , m_accordion(new AccordionWidget)
-    , m_maskModel(0)
-    , m_intensityDataItem(0)
+MaskEditorPropertyPanel::MaskEditorPropertyPanel(QWidget* parent)
+    : QWidget(parent), m_listView(new QListView), m_maskPropertyEditor(new ComponentEditor),
+      m_plotPropertyEditor(new ComponentEditor), m_accordion(new AccordionWidget), m_maskModel(0),
+      m_intensityDataItem(0)
 {
     setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Expanding);
     setObjectName(QLatin1String("MaskEditorToolPanel"));
 
     m_listView->setContextMenuPolicy(Qt::CustomContextMenu);
-    connect(m_listView, SIGNAL(customContextMenuRequested(const QPoint &)),
-            this, SLOT(onCustomContextMenuRequested(const QPoint &)));
+    connect(m_listView, SIGNAL(customContextMenuRequested(const QPoint&)), this,
+            SLOT(onCustomContextMenuRequested(const QPoint&)));
 
-    QVBoxLayout *mainLayout = new QVBoxLayout;
-    mainLayout->setContentsMargins(0,0,0,0);
+    QVBoxLayout* mainLayout = new QVBoxLayout;
+    mainLayout->setContentsMargins(0, 0, 0, 0);
     mainLayout->setSpacing(0);
 
     setup_PlotProperties(m_accordion);
@@ -83,9 +69,19 @@ MaskEditorPropertyPanel::MaskEditorPropertyPanel(QWidget *parent)
     m_plotPropertyEditor->setHeaderHidden(true);
 }
 
+QSize MaskEditorPropertyPanel::sizeHint() const
+{
+    return QSize(128, 128);
+}
 
-void MaskEditorPropertyPanel::setMaskContext(SessionModel *model, const QModelIndex &maskContainerIndex,
-                    IntensityDataItem *intensityItem)
+QSize MaskEditorPropertyPanel::minimumSizeHint() const
+{
+    return QSize(128, 128);
+}
+
+void MaskEditorPropertyPanel::setMaskContext(SessionModel* model,
+                                             const QModelIndex& maskContainerIndex,
+                                             IntensityDataItem* intensityItem)
 {
     m_maskModel = model;
     m_rootIndex = maskContainerIndex;
@@ -95,15 +91,13 @@ void MaskEditorPropertyPanel::setMaskContext(SessionModel *model, const QModelIn
     m_listView->setRootIndex(m_rootIndex);
     m_listView->setSelectionMode(QAbstractItemView::ExtendedSelection);
 
-    connect(m_listView->selectionModel(),
-            SIGNAL(selectionChanged(QItemSelection, QItemSelection)),
-            this,
-            SLOT(onSelectionChanged(QItemSelection, QItemSelection)));
+    connect(m_listView->selectionModel(), &QItemSelectionModel::selectionChanged,
+            this, &MaskEditorPropertyPanel::onSelectionChanged);
 
     m_plotPropertyEditor->setItem(m_intensityDataItem);
 }
 
-QItemSelectionModel *MaskEditorPropertyPanel::selectionModel()
+QItemSelectionModel* MaskEditorPropertyPanel::selectionModel()
 {
     Q_ASSERT(m_listView);
     return m_listView->selectionModel();
@@ -114,76 +108,74 @@ void MaskEditorPropertyPanel::setPanelHidden(bool value)
 {
     this->setHidden(value);
 
-    if(!m_rootIndex.isValid()) return;
+    if (!m_rootIndex.isValid())
+        return;
 
-    if(value) {
+    if (value) {
         m_maskPropertyEditor->setItem(0);
         m_plotPropertyEditor->setItem(0);
     } else {
         QModelIndexList indexes = selectionModel()->selectedIndexes();
-        if(indexes.size()) {
+        if (indexes.size())
             m_maskPropertyEditor->setItem(m_maskModel->itemForIndex(indexes.front()));
-        }
+
         m_plotPropertyEditor->setItem(m_intensityDataItem);
     }
 }
 
-void MaskEditorPropertyPanel::onSelectionChanged(const QItemSelection &selected, const QItemSelection &deselected)
+void MaskEditorPropertyPanel::onSelectionChanged(const QItemSelection& selected,
+                                                 const QItemSelection&)
 {
-    Q_UNUSED(deselected);
-    if(selected.size()) {
+    if (selected.size())
         m_maskPropertyEditor->setItem(m_maskModel->itemForIndex(selected.indexes().front()));
-    } else {
+    else
         m_maskPropertyEditor->setItem(0);
-    }
-
-
 }
 
-void MaskEditorPropertyPanel::onCustomContextMenuRequested(const QPoint &point)
+void MaskEditorPropertyPanel::onCustomContextMenuRequested(const QPoint& point)
 {
     emit itemContextMenuRequest(m_listView->mapToGlobal(point));
 }
 
-void MaskEditorPropertyPanel::setup_PlotProperties(AccordionWidget *accordion)
+void MaskEditorPropertyPanel::setup_PlotProperties(AccordionWidget* accordion)
 {
-    ContentPane *cp = accordion->getContentPane(accordion->addContentPane("Plot properties"));
+    ContentPane* cp = accordion->getContentPane(accordion->addContentPane("Plot properties"));
     cp->setMaximumHeight(600);
     cp->setHeaderTooltip("Plot properties editor");
     cp->setContainerFrameStyle(QFrame::Plain);
-    QFrame *contentFrame = cp->getContentFrame();
+    QFrame* contentFrame = cp->getContentFrame();
 
-    QVBoxLayout *layout = new QVBoxLayout();
+    QVBoxLayout* layout = new QVBoxLayout();
     layout->setContentsMargins(0, 0, 0, 0);
     m_plotPropertyEditor->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     layout->addWidget(new EnvelopWidget(m_plotPropertyEditor));
     contentFrame->setLayout(layout);
 }
 
-void MaskEditorPropertyPanel::setup_MaskStack(AccordionWidget *accordion)
+void MaskEditorPropertyPanel::setup_MaskStack(AccordionWidget* accordion)
 {
-    ContentPane *cp = accordion->getContentPane(accordion->addContentPane("Mask stack"));
+    ContentPane* cp = accordion->getContentPane(accordion->addContentPane("Mask stack"));
     cp->setMaximumHeight(600);
     cp->setHeaderTooltip("List of created masks representing mask stacking order.");
     cp->setContainerFrameStyle(QFrame::Plain);
-    QFrame *contentFrame = cp->getContentFrame();
+    QFrame* contentFrame = cp->getContentFrame();
 
-    QVBoxLayout *layout = new QVBoxLayout();
+    QVBoxLayout* layout = new QVBoxLayout();
     layout->setContentsMargins(0, 0, 0, 0);
     layout->addWidget(m_listView);
     layout->addWidget(new EnvelopWidget(m_listView));
     contentFrame->setLayout(layout);
 }
 
-void MaskEditorPropertyPanel::setup_MaskProperties(AccordionWidget *accordion)
+void MaskEditorPropertyPanel::setup_MaskProperties(AccordionWidget* accordion)
 {
-    ContentPane *cp = accordion->getContentPane(accordion->addContentPane("Mask properties"));
+    ContentPane* cp = accordion->getContentPane(accordion->addContentPane("Mask properties"));
     cp->setMaximumHeight(600);
     cp->setHeaderTooltip("Property editor for currently selected mask.");
     cp->setContainerFrameStyle(QFrame::Plain);
-    QFrame *contentFrame = cp->getContentFrame();
+    QFrame* contentFrame = cp->getContentFrame();
 
-    QVBoxLayout *layout = new QVBoxLayout();
+    QVBoxLayout* layout = new QVBoxLayout();
     layout->setContentsMargins(0, 0, 0, 0);
     layout->addWidget(new EnvelopWidget(m_maskPropertyEditor));
     contentFrame->setLayout(layout);

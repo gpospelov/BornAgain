3007a6c04a9a2a960e35e10d3e8b93f02f8b7df3
Gennady Pospelov <g.pospelov@fz-juelich.de>
2017-11-08 14:48:59 +0100

    Convenience TestComponentView for testing ComponentProxyModel

---
+++ GUI/coregui/Views/PropertyEditor/TestComponentView.cpp
@@ -0,0 +1,58 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      GUI/coregui/Views/PropertyEditor/TestComponentView.cpp
+//! @brief     Defines class TestComponentView
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2016
+//! @authors   Scientific Computing Group at MLZ Garching
+//! @authors   Céline Durniak, Marina Ganeva, David Li, Gennady Pospelov
+//! @authors   Walter Van Herck, Joachim Wuttke
+//
+// ************************************************************************** //
+
+#include "TestComponentView.h"
+#include "mainwindow.h"
+#include "ComponentProxyModel.h"
+#include "SampleModel.h"
+#include <QTreeView>
+#include <QBoxLayout>
+#include <QPushButton>
+#include <QDebug>
+
+TestComponentView::TestComponentView(MainWindow* mainWindow)
+    : m_mainWindow(mainWindow)
+    , m_proxyModel(new ComponentProxyModel(this))
+    , m_sourceTree(new QTreeView)
+    , m_proxyTree(new QTreeView)
+    , m_updateButton(new QPushButton("Update models"))
+{
+    auto layout = new QHBoxLayout();
+    layout->setMargin(0);
+    layout->setSpacing(0);
+
+    layout->addWidget(m_sourceTree);
+    layout->addWidget(m_proxyTree);
+
+    // vertical layout
+    auto vlayout = new QVBoxLayout;
+    auto button = new QPushButton("Update models");
+    connect(button, &QPushButton::clicked, this, &TestComponentView::onUpdateRequest);
+
+    vlayout->addWidget(button);
+    vlayout->addLayout(layout);
+
+    setLayout(vlayout);
+
+    m_sourceTree->setModel(m_mainWindow->sampleModel());
+}
+
+void TestComponentView::onUpdateRequest()
+{
+    qDebug() << "TestComponentView::onUpdateRequest() ->";
+    m_proxyModel->setSessionModel(m_mainWindow->sampleModel());
+    m_proxyTree->setModel(m_proxyModel);
+}
---
+++ GUI/coregui/Views/PropertyEditor/TestComponentView.h
@@ -0,0 +1,47 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      GUI/coregui/Views/PropertyEditor/TestComponentView.h
+//! @brief     Defines class TestComponentView
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2016
+//! @authors   Scientific Computing Group at MLZ Garching
+//! @authors   Céline Durniak, Marina Ganeva, David Li, Gennady Pospelov
+//! @authors   Walter Van Herck, Joachim Wuttke
+//
+// ************************************************************************** //
+
+#ifndef TESTCOMPONENTVIEW_H
+#define TESTCOMPONENTVIEW_H
+
+#include "WinDllMacros.h"
+#include <QWidget>
+
+class MainWindow;
+class QPushButton;
+class ComponentProxyModel;
+class QTreeView;
+
+//! View to tests QListView working with ComponentProxyModel.
+
+class TestComponentView : public QWidget
+{
+    Q_OBJECT
+public:
+    TestComponentView(MainWindow *mainWindow = nullptr);
+
+private slots:
+    void onUpdateRequest();
+
+private:
+    MainWindow* m_mainWindow;
+    ComponentProxyModel* m_proxyModel;
+    QTreeView* m_sourceTree;
+    QTreeView* m_proxyTree;
+    QPushButton* m_updateButton;
+};
+
+#endif // TESTCOMPONENTVIEW_H

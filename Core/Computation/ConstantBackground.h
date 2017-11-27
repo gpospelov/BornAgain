89295e5eb65a1a1d613f18b544af943429f38356
Walter Van Herck <w.van.herck@fz-juelich.de>
2017-11-27 10:43:57 +0100

    Added class representing a constant background

---
+++ Core/Computation/ConstantBackground.h
@@ -0,0 +1,38 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Core/Computation/ConstantBackground.h
+//! @brief     Defines class ConstantBackground.
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2017
+//! @authors   Scientific Computing Group at MLZ Garching
+//! @authors   C. Durniak, M. Ganeva, G. Pospelov, W. Van Herck, J. Wuttke
+//
+// ************************************************************************** //
+
+#ifndef CONSTANTBACKGROUND_H
+#define CONSTANTBACKGROUND_H
+
+#include "IBackground.h"
+
+//! Class representing a constant background signal
+//!
+//! @ingroup simulation
+
+class BA_CORE_API_ ConstantBackground : public IBackground
+{
+public:
+    ConstantBackground(double background_value);
+    ~ConstantBackground();
+    ConstantBackground* clone() const override final;
+
+    void addBackGround(std::vector<SimulationElement>::iterator start,
+                       std::vector<SimulationElement>::iterator end) const override final;
+private:
+    double m_background_value;
+};
+
+#endif // CONSTANTBACKGROUND_H

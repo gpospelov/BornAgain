fa4494146a3da58064a40fcac517a4e7eb904f5e
anikhalder <halder.anik04@gmail.com>
2017-11-29 16:48:18 +0100

    Add Google Test Header file for Unit Tests

---
+++ Tests/UnitTests/Core/utils/google_test.h
@@ -0,0 +1,16 @@
+// include this header file for each Unit Test
+
+#ifndef GOOGLE_TEST_H
+#define GOOGLE_TEST_H
+
+#ifdef _WIN32
+#pragma warning ( push )
+#pragma warning ( disable: 4275 )
+#include <gtest/gtest.h>
+#pragma warning ( pop )
+#else
+#include <gtest/gtest.h>
+#endif
+
+
+#endif //GOOGLE_TEST_H

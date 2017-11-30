a8c07297aed91e369bb0095708f9c8b7609da74d
Walter Van Herck <w.van.herck@fz-juelich.de>
2017-11-30 17:26:45 +0100

    Added PoissonNoiseBackground

--- Examples/python/simulation/ex05_BeamAndDetector/ConstantBackground.py
+++ Examples/python/simulation/ex05_BeamAndDetector/ConstantBackground.py
@@ -39,8 +39,9 @@ def get_simulation():
     simulation.setDetectorParameters(100, 0.0*deg, 2.0*deg,
                                      100, 0.0*deg, 2.0*deg)
     simulation.setBeamParameters(1.0*angstrom, 0.2*deg, 0.0*deg)
-    simulation.setBeamIntensity(1e6)
-    bg = ba.ConstantBackground(1e3)
+    simulation.setBeamIntensity(1e2)
+    # bg = ba.ConstantBackground(1e3)
+    bg = ba.PoissonNoiseBackground()
     simulation.setBackground(bg)
     return simulation
 

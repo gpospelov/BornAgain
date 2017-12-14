6f888eef97d365a8327abb3cc22a93a0531e847d
Gennady Pospelov <g.pospelov@fz-juelich.de>
2017-12-14 10:06:55 +0100

    Standard code beautification in application options.

--- GUI/main/appoptions.cpp
+++ GUI/main/appoptions.cpp
@@ -21,15 +21,11 @@
 #include <fstream>
 #include <iostream>
 
-
-ApplicationOptions::ApplicationOptions(int argc, char **argv)
-    : m_options_is_consistent(false)
+ApplicationOptions::ApplicationOptions(int argc, char** argv) : m_options_is_consistent(false)
 {
-    m_options.add_options()
-        ("help,h",      "print help message")
-        ("version,v",   "print version number")
-        ("with-debug",  "run application with debug printout")
-        ("no-splash",   "do not show splash screen");
+    m_options.add_options()("help,h", "print help message")("version,v", "print version number")(
+        "with-debug", "run application with debug printout")("no-splash",
+                                                             "do not show splash screen");
 
     parseCommandLine(argc, argv);
 
@@ -38,14 +34,21 @@ ApplicationOptions::ApplicationOptions(int argc, char **argv)
 
 //! access variables
 
-const bpo::variable_value& ApplicationOptions::operator[] (const std::string& s) const
+const bpo::variable_value& ApplicationOptions::operator[](const std::string& s) const
 {
     return m_variables_map[s.c_str()];
 }
 
+bool ApplicationOptions::find(std::string name) const
+{
+    return (m_variables_map.count(name.c_str()) ? true : false);
+}
+
+bool ApplicationOptions::isConsistent() const { return m_options_is_consistent; }
+
 //! parse command line arguments
 
-void ApplicationOptions::parseCommandLine(int argc, char **argv)
+void ApplicationOptions::parseCommandLine(int argc, char** argv)
 {
     m_options_is_consistent = false;
     // parsing command line arguments
@@ -53,35 +56,44 @@ void ApplicationOptions::parseCommandLine(int argc, char **argv)
         // if positional option description is empty, no command line arguments
         // without '--' or '-' will be accepted
         // 'store' populates the variable map
-        bpo::store( bpo::command_line_parser( argc, argv).options( m_options ).positional( m_positional_options ).run(), m_variables_map);
-        //bpo::store(bpo::parse_command_line(argc, argv, m_options), m_variables_map);
+        bpo::store(bpo::command_line_parser(argc, argv)
+                       .options(m_options)
+                       .positional(m_positional_options)
+                       .run(),
+                   m_variables_map);
+        // bpo::store(bpo::parse_command_line(argc, argv, m_options), m_variables_map);
 
         // 'notify' raises any errors encountered
         bpo::notify(m_variables_map);
 
         m_options_is_consistent = true;
-    }
-    catch(std::exception& e) {
+    } catch (std::exception& e) {
         // we get here if there is unrecognized options
         std::cout << "main() ->  " << e.what() << std::endl;
         std::cout << m_options << std::endl;
     }
-
 }
+
+boost::program_options::variables_map& ApplicationOptions::getVariables()
+{
+    return m_variables_map;
+}
+
+boost::program_options::options_description& ApplicationOptions::getOptions() { return m_options; }
 
 void ApplicationOptions::processOptions()
 {
-    if(!m_options_is_consistent) return;
+    if (!m_options_is_consistent)
+        return;
 
     if (m_variables_map.count("help")) {
         printHelpMessage();
         m_options_is_consistent = false;
-    }
-    else if(m_variables_map.count("version")) {
-        std::cout << "BornAgain-"<<GUIHelpers::getBornAgainVersionString().toStdString() << std::endl;
+    } else if (m_variables_map.count("version")) {
+        std::cout << "BornAgain-" << GUIHelpers::getBornAgainVersionString().toStdString()
+                  << std::endl;
         m_options_is_consistent = false;
     }
-
 }
 
 void ApplicationOptions::printHelpMessage() const
@@ -89,5 +101,3 @@ void ApplicationOptions::printHelpMessage() const
     std::cout << "BornAgain Graphical User Interface" << std::endl;
     std::cout << m_options << std::endl;
 }
-
-
--- GUI/main/appoptions.h
+++ GUI/main/appoptions.h
@@ -18,17 +18,15 @@
 #define APPOPTIONS_H
 
 #include "Macros.h"
-#include "WinDllMacros.h"
-GCC_DIAG_OFF(unused-parameter);
+GCC_DIAG_OFF(unused - parameter);
 #include <boost/program_options/options_description.hpp>
 #include <boost/program_options/positional_options.hpp>
 #include <boost/program_options/variables_map.hpp>
-GCC_DIAG_ON(unused-parameter);
+GCC_DIAG_ON(unused - parameter);
 
 #include <string>
 
 namespace bpo = boost::program_options;
-
 
 //! @class ApplicationOptions
 //! @ingroup tools_internal
@@ -37,37 +35,38 @@ namespace bpo = boost::program_options;
 class ApplicationOptions
 {
 public:
-    ApplicationOptions(int argc = 0, char **argv = 0);
+    ApplicationOptions(int argc = 0, char** argv = 0);
 
     //! access to variable with given name defined in variables container
-    const bpo::variable_value& operator[] (const std::string& s) const;
+    const bpo::variable_value& operator[](const std::string& s) const;
 
     //! Returns true if option with given name has been set
-    bool find(std::string name) const { return (m_variables_map.count(name.c_str()) ? true : false); }
+    bool find(std::string name) const;
 
-    //! Returns true if options are consistent (no conflicting options, no --help request, config file is parsed)
-    bool isConsistent() const { return m_options_is_consistent; }
+    //! Returns true if options are consistent (no conflicts, no --help request)
+    bool isConsistent() const;
 
     //! Parses command line arguments
-    void parseCommandLine(int argc, char **argv);
+    void parseCommandLine(int argc, char** argv);
 
     //! Returns reference to the variables container
-    bpo::variables_map& getVariables() { return m_variables_map; }
+    bpo::variables_map& getVariables();
 
     //! Returns reference to the options description
-    bpo::options_description& getOptions() { return m_options; }
+    bpo::options_description& getOptions();
 
     void processOptions();
 
     void printHelpMessage() const;
 
 private:
-    bool m_options_is_consistent;       //! true if options are consistent (no conflicts, no --help request)
-    bpo::options_description m_options; //! options description, to be filled with add() from different program modules
-    bpo::positional_options_description m_positional_options; //! positional options description, to be filled with addPositional() from main module
+    //! true if options are consistent (no conflicts, no --help request)
+    bool m_options_is_consistent;
+    //! options description, to be filled with add() from different program modules
+    bpo::options_description m_options;
+    //! positional options description, to be filled with addPositional() from main module
+    bpo::positional_options_description m_positional_options;
     bpo::variables_map m_variables_map; //! parsed variables
 };
-
-
 
 #endif // APPOPTIONS_H

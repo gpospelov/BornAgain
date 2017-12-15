29015ca163568be6e719a2babf85240abc101f54
Gennady Pospelov <g.pospelov@fz-juelich.de>
2017-12-15 10:57:49 +0100

    Unnecessary error stream redirection removed

--- Tests/UnitTests/utilities/main_testlist.cpp
+++ Tests/UnitTests/utilities/main_testlist.cpp
@@ -1,16 +1,9 @@
 #include "google_test.h"
-
 #include "testlist.h"
-#include "ErrorStreamRedirect.h"
 
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
-
-    // redirect std::cerr stream
-    std::stringstream oss;
-    ErrorStreamRedirect redirecter( oss.rdbuf() );
-    (void)redirecter;
 
     // run all google tests
     return RUN_ALL_TESTS();

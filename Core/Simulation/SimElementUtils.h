8339f078bc221252c5773d9a3a3e0e525c66d35e
Dmitry Yurov <d.yurov@fz-juelich.de>
2018-01-11 12:57:22 +0100

    Created a namespace for simulation element utils
    Redmine: #1917

---
+++ Core/Simulation/SimElementUtils.h
@@ -0,0 +1,22 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Core/Simulation/SimElementUtils.h
+//! @brief     Defines a set of functions to operate on simulation elements.
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2018
+//! @authors   Scientific Computing Group at MLZ (see CITATION, AUTHORS)
+//
+// ************************************************************************** //
+
+#ifndef SIMELEMENTUTILS_H_
+#define SIMELEMENTUTILS_H_
+
+namespace SimElementUtils {
+
+}
+
+#endif /* SIMELEMENTUTILS_H_ */

549260b8f2b0c04f12241ae8ae28c3323a647e99
Walter Van Herck <w.van.herck@fz-juelich.de>
2018-01-23 11:43:33 +0100

    Added dummy files for SimulationResults

---
+++ Core/Instrument/SimulationResults.cpp
@@ -0,0 +1,16 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Core/Instrument/SimulationResults.cpp
+//! @brief     Implements class SimulationResults.
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2018
+//! @authors   Scientific Computing Group at MLZ (see CITATION, AUTHORS)
+//
+// ************************************************************************** //
+
+#include "SimulationResults.h"
+
---
+++ Core/Instrument/SimulationResults.h
@@ -0,0 +1,29 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Core/Instrument/SimulationResults.h
+//! @brief     Defines class SimulationResults.
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2018
+//! @authors   Scientific Computing Group at MLZ (see CITATION, AUTHORS)
+//
+// ************************************************************************** //
+
+#ifndef SIMULATIONRESULTS_H
+#define SIMULATIONRESULTS_H
+
+#include "WinDllMacros.h"
+#include <cstddef>
+
+//! Wrapper around OutputData<double> that also provides unit conversions.
+//! @ingroup fitting_internal
+
+class BA_CORE_API_ SimulationResults
+{
+public:
+};
+
+#endif // SIMULATIONRESULTS_H

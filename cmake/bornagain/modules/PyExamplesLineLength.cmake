68ba08a2b823c4cd0127767256f627ae54038aee
Gennady Pospelov <g.pospelov@fz-juelich.de>
2018-02-01 11:23:16 +0100

    PyExamples functional test not generates output in common directory

--- cmake/bornagain/modules/PyExamplesLineLength.cmake
+++ cmake/bornagain/modules/PyExamplesLineLength.cmake
@@ -1,13 +1,12 @@
-set(WEB_LEN_LIM 85) # maximum line length of code for display in Drupal web pages
-
 # Check whether the line length of all python examples remains below WEB_LEN_LIM
+
+set(WEB_LEN_LIM 85) # maximum line length of code for display
 
 if(NOT MSVC)
 
 file(GLOB PY_EXAMPLES "${PY_EXAMPLES_DIR}/*/ex*/*.py")
 
-add_test(NAME "PyExamplesLineLength"
-    COMMAND ${PYTHON_EXECUTABLE}
+add_test(NAME "PyExamplesLineLength" COMMAND ${PYTHON_EXECUTABLE}
     ${CMAKE_SOURCE_DIR}/dev-tools/analyze/check-line-length.py ${WEB_LEN_LIM}
     ${PY_EXAMPLES})
 

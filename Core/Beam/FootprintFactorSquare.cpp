5a8d498893758824d24e7f1d5e10274a4c72e2a3
Dmitry Yurov <d.yurov@fz-juelich.de>
2018-02-08 17:23:45 +0100

    FootprintFactors now return zero under negative angles
    Redmine: #1961

--- Core/Beam/FootprintFactorSquare.cpp
+++ Core/Beam/FootprintFactorSquare.cpp
@@ -15,9 +15,8 @@ FootprintFactorSquare* FootprintFactorSquare::clone() const
 
 double FootprintFactorSquare::calculate(double alpha) const
 {
-    if (alpha > M_PI || alpha < 0.0)
-        throw std::runtime_error("Error in FootprintFactorSquare::calculate: "
-                                 "incident angle is out of range");
+    if (alpha < 0.0 || alpha > M_PI_2)
+        return 0.0;
     if (widthRatio() == 0.0)
         return 1.0;
     const double arg = std::sin(alpha) / widthRatio();

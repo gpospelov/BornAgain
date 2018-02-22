2a04ff08e359c87dbf23d34cc315563cfd63653f
Gennady Pospelov <g.pospelov@fz-juelich.de>
2018-02-22 10:24:19 +0100

    Python example refactoring to update to new unit convention.

--- Examples/python/simulation/ex07_Miscellaneous/AccessingSimulationResults.py
+++ Examples/python/simulation/ex07_Miscellaneous/AccessingSimulationResults.py
@@ -39,18 +39,11 @@ def get_simulation():
     Returns a GISAXS simulation with beam and detector defined.
     """
     simulation = ba.GISASSimulation()
-    simulation.setDetectorParameters(200, -2.0*deg, 2.0*deg,
-                                     200, 0.0*deg, 2.0*deg)
+    simulation.setDetectorParameters(201, -2.0*deg, 2.0*deg,
+                                     201, 0.0*deg, 2.0*deg)
     simulation.setBeamParameters(1.0*angstrom, 0.2*deg, 0.0*deg)
+    simulation.setBeamIntensity(1e+05)
     return simulation
-
-
-def plot_cropped_map(hist):
-    """
-    Plot cropped version of intensity data
-    """
-    crop = hist.crop(-1.0*deg, 0.5*deg, 1.0*deg, 1.0*deg)
-    ba.plot_histogram(crop)
 
 
 def get_noisy_image(hist):
@@ -67,14 +60,18 @@ def get_noisy_image(hist):
     return result
 
 
-def plot_relative_difference(hist):
+def plot_histogram(hist, zmin=None, zmax=None):
+    ba.plot_histogram(hist, xlabel=r'$\varphi_f ^{\circ}$', ylabel=r'$\alpha_f ^{\circ}$',
+                      zlabel="", zmin=zmin, zmax=zmax)
+
+
+def get_relative_difference(hist):
     """
     Creates noisy histogram made of original histogram,
     then creates and plots a relative difference histogram: (noisy-hist)/hist
     """
     noisy = get_noisy_image(hist)
-    diff = noisy.relativeDifferenceHistogram(hist)
-    ba.plot_histogram(diff, zmin=1e-03, zmax=10)
+    return noisy.relativeDifferenceHistogram(hist)
 
 
 def plot_slices(hist):
@@ -84,71 +81,68 @@ def plot_slices(hist):
     noisy = get_noisy_image(hist)
 
     # projection along Y, slice at fixed x-value
-    proj1 = noisy.projectionY(0.0*deg)
-    plt.semilogy(proj1.getBinCenters()/deg,
+    proj1 = noisy.projectionY(0.0)
+    plt.semilogy(proj1.getBinCenters(),
                  proj1.getBinValues(),
                  label=r'$\phi=0.0^{\circ}$')
 
     # projection along Y, slice at fixed x-value
-    proj2 = noisy.projectionY(0.5*deg)  # slice at fixed value
-    plt.semilogy(proj2.getBinCenters()/deg,
+    proj2 = noisy.projectionY(0.5)  # slice at fixed value
+    plt.semilogy(proj2.getBinCenters(),
                  proj2.getBinValues(),
                  label=r'$\phi=0.5^{\circ}$')
 
     # projection along Y for all X values between [xlow, xup], averaged
-    proj3 = noisy.projectionY(0.4*deg, 0.6*deg)
-    plt.semilogy(proj3.getBinCenters()/deg,
+    proj3 = noisy.projectionY(0.41, 0.59)
+    plt.semilogy(proj3.getBinCenters(),
                  proj3.getArray(ba.IHistogram.AVERAGE),
                  label=r'$<\phi>=0.5^{\circ}$')
 
-    plt.xlim(proj1.getXmin()/deg, proj1.getXmax()/deg)
-    plt.ylim(1.0, proj1.getMaximum()*10.0)
+    plt.xlim(proj1.getXmin(), proj1.getXmax())
+    plt.ylim(proj2.getMinimum(), proj1.getMaximum()*10.0)
     plt.xlabel(r'$\alpha_f ^{\circ}$', fontsize=16)
     plt.legend(loc='upper right')
+    plt.tight_layout()
 
 
-def save_to_file(result):
-    """
-    Saves intensity data into file
-    """
-    result.save("result.int")
-    result.save("result.tif")
-    result.save("result.txt")
-    result.save("result.int.gz")
-    result.save("result.tif.gz")
-    result.save("result.txt.gz")
-    result.save("result.int.bz2")
-    result.save("result.tif.bz2")
-    result.save("result.txt.bz2")
-
-
-def plot(result):
+def plot(hist):
     """
     Runs different plotting functions one by one
     to demonstrate trivial data presentation tasks.
     """
 
-    fig = plt.figure(figsize=(12.80, 10.24))
+    plt.figure(figsize=(12.80, 10.24))
 
     plt.subplot(2, 2, 1)
-    ba.plot_histogram(result)
+    plot_histogram(hist)
     plt.title("Intensity as colormap")
 
     plt.subplot(2, 2, 2)
-    plot_cropped_map(result)
+    crop = hist.crop(-1.0, 0.5, 1.0, 1.0)
+    plot_histogram(crop)
     plt.title("Cropping")
 
     plt.subplot(2, 2, 3)
-    plot_relative_difference(result)
+    reldiff_hist = get_relative_difference(hist)
+    plot_histogram(reldiff_hist, zmin=1e-03, zmax=10)
     plt.title("Relative difference")
 
     plt.subplot(2, 2, 4)
-    plot_slices(result)
+    plot_slices(hist)
     plt.title("Various slicing of 2D into 1D")
 
-    save_to_file(result)
+    # save to file
+    # result.save("result.int")
+    # result.save("result.tif")
+    # result.save("result.txt")
+    # result.save("result.int.gz")
+    # result.save("result.tif.gz")
+    # result.save("result.txt.gz")
+    # result.save("result.int.bz2")
+    # result.save("result.tif.bz2")
+    # result.save("result.txt.bz2")
 
-    plt.subplots_adjust(left=0.07, right=0.97, top=0.9, bottom=0.1, hspace=0.25)
+    plt.tight_layout()
     plt.show()
 
 

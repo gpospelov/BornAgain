a4101e28fee603401bd69aa90ca496910ae5d707
Walter Van Herck <w.van.herck@fz-juelich.de>
2018-02-22 13:36:54 +0100

    Code style

--- cmake/bornagain/modules/CPackWindows.cmake
+++ cmake/bornagain/modules/CPackWindows.cmake
@@ -11,18 +11,17 @@ set(CPACK_PACKAGE_FILE_NAME
 
 set(CPACK_GENERATOR "NSIS")
 
-#set(CPACK_NSIS_MODIFY_PATH ON)
-set( CPACK_NSIS_INSTALL_ROOT "C:")
+set(CPACK_NSIS_INSTALL_ROOT "C:")
 set(CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL ON)
 
-set( CPACK_NSIS_MENU_LINKS "bin\\\\BornAgain.exe" "BornAgain")
+set(CPACK_NSIS_MENU_LINKS "bin\\\\BornAgain.exe" "BornAgain")
 
 
-set( CPACK_PACKAGE_ICON "${CMAKE_CURRENT_SOURCE_DIR}/GUI\\\\coregui\\\\images\\\\BornAgain.ico" )
-set( CPACK_NSIS_MUI_ICON "${CMAKE_CURRENT_SOURCE_DIR}/GUI\\\\coregui\\\\images\\\\BornAgain.ico" )
-set( CPACK_NSIS_MUI_UNIICON "${CMAKE_CURRENT_SOURCE_DIR}/GUI\\\\coregui\\\\images\\\\BornAgain.ico" )
+set(CPACK_PACKAGE_ICON "${CMAKE_CURRENT_SOURCE_DIR}/GUI\\\\coregui\\\\images\\\\BornAgain.ico" )
+set(CPACK_NSIS_MUI_ICON "${CMAKE_CURRENT_SOURCE_DIR}/GUI\\\\coregui\\\\images\\\\BornAgain.ico" )
+set(CPACK_NSIS_MUI_UNIICON "${CMAKE_CURRENT_SOURCE_DIR}/GUI\\\\coregui\\\\images\\\\BornAgain.ico" )
 
-set (CPACK_NSIS_EXTRA_INSTALL_COMMANDS "
+set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS "
   Push \\\"PYTHONPATH\\\"
   Push \\\"P\\\"
   Push \\\"HKCU\\\"
@@ -33,7 +32,7 @@ set (CPACK_NSIS_EXTRA_INSTALL_COMMANDS "
   CreateShortCut \\\"$DESKTOP\\\\BornAgain.lnk\\\" \\\"$INSTDIR\\\\bin\\\\BornAgain.exe\\\"
 ")
 
-set (CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS "
+set(CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS "
   Push \\\"PYTHONPATH\\\"
   Push \\\"R\\\"
   Push \\\"HKCU\\\"

8404cdeecb4b5e2aa4c77c6d8ec2a1cb32b8067b
Gennady Pospelov <g.pospelov@fz-juelich.de>
2018-02-23 13:53:51 +0100

    MacOS vagrant deployment: two installers at once

--- dev-tools/deploy/yosemite-dev/build_bornagain.sh
+++ dev-tools/deploy/yosemite-dev/build_bornagain.sh
@@ -10,8 +10,12 @@ rm -r -f $WORKDIR; mkdir -p $WORKDIR; cd $WORKDIR;
 #git clone --recurse-submodules -b develop https://github.com/scgmlz/BornAgain.git
 git clone --recurse-submodules -b MacBuild https://github.com/gpospelov/BornAgain.git
 
-mkdir BornAgain-build
-cd BornAgain-build
+# --------------------------------------------------------------------
+echo 'Building on Python2'
+# --------------------------------------------------------------------
+
+mkdir BornAgain-build-python2
+cd BornAgain-build-python2
 #export ANACONDA=/Users/vagrant/anaconda2
 #cmake -DCMAKE_C_COMPILER=/usr/local/opt/llvm/bin/clang -DCMAKE_CXX_COMPILER=/usr/local/opt/llvm/bin/clang++  -DPYTHON_LIBRARY=$ANACONDA/lib/libpython2.7.dylib -DPYTHON_EXECUTABLE=$ANACONDA/bin/python2.7 -DBORNAGAIN_APPLE_BUNDLE=ON -DCMAKE_PREFIX_PATH=/usr/local ../BornAgain
 cmake -DCMAKE_C_COMPILER=/usr/local/opt/llvm/bin/clang -DCMAKE_CXX_COMPILER=/usr/local/opt/llvm/bin/clang++ -DBORNAGAIN_USE_PYTHON3=OFF -DBORNAGAIN_APPLE_BUNDLE=ON -DCMAKE_PREFIX_PATH=/usr/local ../BornAgain
@@ -19,6 +23,20 @@ make -j4
 cpack -V
 
 echo 'Copying dmg installer back to build server'
-echo 'scp /Users/vagrant/build/BornAgain-build/*.dmg scg@scgmini:./deployment/results/.'
-scp /Users/vagrant/build/BornAgain-build/*.dmg scg@scgmini:./deployment/results/.
+echo 'scp /Users/vagrant/build/BornAgain-build-python2/*.dmg scg@scgmini:./deployment/results/.'
+scp /Users/vagrant/build/BornAgain-build-python2/*.dmg scg@scgmini:./deployment/results/.
 
+# --------------------------------------------------------------------
+echo 'Building on Python3'
+# --------------------------------------------------------------------
+cd $WORKDIR
+
+mkdir BornAgain-build-python3
+cd BornAgain-build-python3
+cmake -DCMAKE_C_COMPILER=/usr/local/opt/llvm/bin/clang -DCMAKE_CXX_COMPILER=/usr/local/opt/llvm/bin/clang++ -DBORNAGAIN_USE_PYTHON3=ON -DBORNAGAIN_APPLE_BUNDLE=ON -DCMAKE_PREFIX_PATH=/usr/local ../BornAgain
+make -j4
+cpack -V
+
+echo 'Copying dmg installer back to build server'
+echo 'scp /Users/vagrant/build/BornAgain-build-python3/*.dmg scg@scgmini:./deployment/results/.'
+scp /Users/vagrant/build/BornAgain-build-python3/*.dmg scg@scgmini:./deployment/results/.

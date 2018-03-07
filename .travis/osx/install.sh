d52e3d0cbd7d95ab69e63a676fd658eab5659c3c
Walter Van Herck <w.van.herck@fz-juelich.de>
2018-03-07 11:27:55 +0100

    Further removed obsolete dependencies

--- .travis/osx/install.sh
+++ .travis/osx/install.sh
@@ -1,3 +1,2 @@
 brew update
-brew install git fftw gsl python homebrew/python/matplotlib numpy boost-python qt5 eigen libyaml yaml-cpp
-pip install pyyaml
+brew install git fftw gsl python homebrew/python/matplotlib numpy boost-python qt5

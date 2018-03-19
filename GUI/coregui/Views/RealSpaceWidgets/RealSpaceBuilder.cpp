e0a8cbe2f8bc0b84a03fc89ef54bc89290924055
anikhalder <halder.anik04@gmail.com>
2018-03-19 17:47:43 +0100

    Added 2D Basic, Hexagonal, Square interference lattices with lattice angle and rotational angle

--- GUI/coregui/Views/RealSpaceWidgets/RealSpaceBuilder.cpp
+++ GUI/coregui/Views/RealSpaceWidgets/RealSpaceBuilder.cpp
@@ -27,6 +27,7 @@
 #include "VectorItem.h"
 #include "InterferenceFunctionItems.h"
 #include "Lattice2DItems.h"
+#include "Units.h"
 
 #include <QMessageBox>
 
@@ -93,122 +94,96 @@ void RealSpaceBuilder::populateLayout(RealSpaceModel* model, const SessionItem& 
             auto interference2DLatticeItem =
                     interference->getGroupItem(InterferenceFunction2DLatticeItem::P_LATTICE_TYPE);
 
+            /*
             QMessageBox box;
             box.setText(interference->displayName());
             box.setDetailedText(interference2DLatticeItem->modelType());
             box.exec();
+            */
 
             // Check if 2D Lattice interference type is Basic
             if (interference2DLatticeItem->modelType() == Constants::BasicLatticeType)
             {
-
-                double lattice_length1 = interference2DLatticeItem->
+                // lattice length 1
+                double l1 = interference2DLatticeItem->
                         getItemValue(BasicLatticeItem::P_LATTICE_LENGTH1).toDouble();
 
-                double lattice_length2 = interference2DLatticeItem->
+                // lattice length 2
+                double l2 = interference2DLatticeItem->
                         getItemValue(BasicLatticeItem::P_LATTICE_LENGTH2).toDouble();
 
+                // lattice angle
+                double l_angle = interference2DLatticeItem->
+                        getItemValue(BasicLatticeItem::P_LATTICE_ANGLE).toDouble();
+
+                // lattice rotation angle
+                double l_rot_angle = interference2DLatticeItem->
+                        getItemValue(BasicLatticeItem::P_LATTICE_ROTATION_ANGLE).toDouble();
+
+                /*
                 QMessageBox box;
                 box.setText(interference2DLatticeItem->displayName());
-                box.setDetailedText(QString::number(lattice_length1+lattice_length2));
+                box.setDetailedText(QString::number(l1+l2));
                 box.exec();
+                */
 
-                // Retrieving abundances of particles
-
-                float total_abundance = 0;
-
-                QVector<float> abundances;
-
-                //int iter=0; // for QMessageBox
-
-                for (auto particle : layoutItem.getItems(ParticleLayoutItem::T_PARTICLES))
-                {
-                    total_abundance = total_abundance +
-                            particle->getItemValue(ParticleItem::P_ABUNDANCE).toFloat();
-
-                    abundances.append(total_abundance);
-
-                    /*
-                    QMessageBox box;
-                    float r = (rand()/static_cast<float>(RAND_MAX));
-                    box.setText("Random number = "+QString::number(r));
-                    box.setDetailedText(particle->displayName()+" abundance = "+
-                      QString::number(particle->getItemValue(ParticleItem::P_ABUNDANCE).toFloat())+
-                      "\nCumulative abundance = "+QString::number(abundances.at(iter)));
-                    box.exec();
-                    ++iter;
-                    */
-
-                }
-
-                // defining layer_size here again as namespace is unnamed in TransformTo3D
-                double layer_size = 50.0;
-                double layer_thickness = 25.0;
-                int quot1, quot2;
-                float result1 = static_cast<float>(remquo(layer_size,lattice_length1,&quot1));
-                float result2 = static_cast<float>(remquo(layer_size,lattice_length2,&quot2));
-
-                Q_UNUSED(result1);
-                Q_UNUSED(result2);
-
-                for (int i = -quot1; i <= quot1; ++i)
-                {
-                    for (int j = -quot2; j <= quot2; ++j)
-                    {
-                        float rand_num = (rand()/static_cast<float>(RAND_MAX));
-                        int k = 0;
-
-                        for (auto particle : layoutItem.getItems(ParticleLayoutItem::T_PARTICLES))
-                        {
-                            // Retrieving x,y,z coordinates of the current particle
-                            SessionItem* particle_position =
-                                    particle->getItem(ParticleItem::P_POSITION);
-                            float x = particle_position->getItemValue(VectorItem::P_X).toFloat();
-                            float y = particle_position->getItemValue(VectorItem::P_Y).toFloat();
-                            float z = particle_position->getItemValue(VectorItem::P_Z).toFloat();
-
-                            float pos_x = static_cast<float>(lattice_length1*i) + x;
-                            float pos_y = static_cast<float>(lattice_length2*j) + y;
-                            float pos_z = z;
-
-                            // Check if the position is inside the 3D model display coordinates
-                            if(abs(pos_x) <= layer_size && abs(pos_y) <= layer_size
-                                    && abs(pos_z) <= layer_thickness)
-                            {
-                                // Randomly display a particle at the position given its abundance
-                                if (rand_num <= abundances.at(k)/total_abundance)
-                                {
-                                    populateParticle(model, *particle,
-                                                     QVector3D(pos_x,pos_y,pos_z));
-                                    break;
-                                }
-                                else
-                                    ++k;
-                            }
-                        }
-                    }
-                }
+                populateInterference2DBasic(model, layoutItem, l1, l2, l_angle, l_rot_angle);
             }
 
-            /* // To be done!
             // Check if 2D Lattice interference type is Hexagonal
             else if (interference2DLatticeItem->modelType() == Constants::HexagonalLatticeType)
             {
+                // lattice length
+                double l = interference2DLatticeItem->
+                        getItemValue(HexagonalLatticeItem::P_LATTICE_LENGTH).toDouble();
+
+                // lattice angle
+                double l_alpha = 60.0; // 60 degrees for a Hexagonal lattice
+
+                // lattice rotation angle
+                double l_xi = interference2DLatticeItem->
+                        getItemValue(HexagonalLatticeItem::P_LATTICE_ROTATION_ANGLE).toDouble();
+
+                /*
                 QMessageBox box;
-                box.setText(interferenceItem->displayName());
+                box.setText(interference2DLatticeItem->displayName());
+                box.setDetailedText(QString::number(l1+l2));
                 box.exec();
+                */
+
+                // l1 = l2 = l (lattice vectors are of equal length)
+                populateInterference2DBasic(model, layoutItem, l, l, l_alpha, l_xi);
             }
+
+
             // Check if 2D Lattice interference type is Square
             else if (interference2DLatticeItem->modelType() == Constants::SquareLatticeType)
             {
+                // lattice length
+                double l = interference2DLatticeItem->
+                        getItemValue(SquareLatticeItem::P_LATTICE_LENGTH).toDouble();
+
+                // lattice angle
+                double l_alpha = 90.0; // 90 degrees for a Square lattice
+
+                // lattice rotation angle
+                double l_xi = interference2DLatticeItem->
+                        getItemValue(SquareLatticeItem::P_LATTICE_ROTATION_ANGLE).toDouble();
+
+                /*
                 QMessageBox box;
-                box.setText(interferenceItem->displayName());
+                box.setText(interference2DLatticeItem->displayName());
+                box.setDetailedText(QString::number(l1+l2));
                 box.exec();
+                */
+
+                // l1 = l2 = l (lattice vectors are of equal length)
+                populateInterference2DBasic(model, layoutItem, l, l, l_alpha, l_xi);
             }
-            */
+
         }
 
-        /* // To be done!
+        /* // To be done
         // Check if interference type is 1D Lattice
         else if (interference->modelType() == Constants::InterferenceFunction1DLatticeType)
         {
@@ -242,16 +217,12 @@ void RealSpaceBuilder::populateLayout(RealSpaceModel* model, const SessionItem& 
 
             // Retrieving x,y,z coordinates of the current particle
             SessionItem* particle_position = particle->getItem(ParticleItem::P_POSITION);
-            float x = particle_position->getItemValue(VectorItem::P_X).toFloat();
-            float y = particle_position->getItemValue(VectorItem::P_Y).toFloat();
-            float z = particle_position->getItemValue(VectorItem::P_Z).toFloat();
-
-            // Retrieving abundance of the current particle
-            float abundance = particle->getItemValue(ParticleItem::P_ABUNDANCE).toFloat();
+            double x = particle_position->getItemValue(VectorItem::P_X).toDouble();
+            double y = particle_position->getItemValue(VectorItem::P_Y).toDouble();
+            double z = particle_position->getItemValue(VectorItem::P_Z).toDouble();
 
             QMessageBox box;
             box.setText("Values: "+QString::number(x)+" "+QString::number(y)+" "+QString::number(z));
-            box.setDetailedText("Abundance: "+QString::number(abundance));
             box.exec();
 
             populateParticle(model, *particle, QVector3D(x,y,z));
@@ -261,6 +232,102 @@ void RealSpaceBuilder::populateLayout(RealSpaceModel* model, const SessionItem& 
     // Used to be
     //for (auto particle : layoutItem.getItems(ParticleLayoutItem::T_PARTICLES))
     //    populateParticle(model, *particle, origin);
+}
+
+void RealSpaceBuilder::populateInterference2DBasic(RealSpaceModel* model,
+                                                   const SessionItem& layoutItem, double l1,
+                                                   double l2, double l_alpha, double l_xi)
+{
+    // Retrieving abundances of particles
+
+    double total_abundance = 0;
+
+    QVector<double> abundances;
+
+    for (auto particle : layoutItem.getItems(ParticleLayoutItem::T_PARTICLES))
+    {
+        total_abundance = total_abundance +
+                particle->getItemValue(ParticleItem::P_ABUNDANCE).toDouble();
+
+        abundances.append(total_abundance);
+
+    }
+
+    // defining layer_size here again as namespace is unnamed in TransformTo3D
+    double layer_size = 50.0;
+    double layer_thickness = 25.0;
+
+    // The following seqment with quot is to calculate the limits of the integer multiple of the
+    // lattice vectors required in order to populate correctly the 3D model's interference lattice
+
+    //int quot1, quot2;
+    //float result1 = static_cast<float>(remquo((layer_size*2),
+    //                                   (l1*cos(Units::deg2rad(l_xi))),&quot1));
+    //float result2 = static_cast<float>(remquo((layer_size*2),
+    //                            (l2*sin(Units::deg2rad(l_alpha+l_xi))),&quot2));
+
+    int quot1 = static_cast<int>(layer_size*2)/
+            static_cast<int>(l1*cos(Units::deg2rad(l_xi)));
+
+
+    int quot2 = static_cast<int>(layer_size*2)/
+            static_cast<int>(l2*sin(Units::deg2rad(l_alpha+l_xi)));
+
+    int quot = quot1 >= quot2 ? quot1: quot2;
+
+    /*
+    QMessageBox mbox;
+    mbox.setText((QString::number(quot1)+" "+QString::number(quot2)));
+    mbox.setDetailedText((QString::number(l1*cos(Units::deg2rad(l_xi)))+
+                  " "+QString::number((layer_size*2)/(l2*sin(Units::deg2rad(l_alpha+l_xi))))));
+
+    mbox.exec();
+    */
+
+    for (int i = -quot; i <= quot; ++i)
+    {
+        for (int j = -quot; j <= quot; ++j)
+        {
+            double rand_num = (rand()/static_cast<double>(RAND_MAX));
+
+            int k = 0;
+
+            for (auto particle : layoutItem.getItems(ParticleLayoutItem::T_PARTICLES))
+            {
+                // Retrieving local x,y,z offset from the lattice point for the current particle
+                SessionItem* particle_position =
+                        particle->getItem(ParticleItem::P_POSITION);
+                double x = particle_position->getItemValue(VectorItem::P_X).toDouble();
+                double y = particle_position->getItemValue(VectorItem::P_Y).toDouble();
+                double z = particle_position->getItemValue(VectorItem::P_Z).toDouble();
+
+                double pos_x = i*l1*cos(Units::deg2rad(l_xi))+
+                        j*l2*cos(Units::deg2rad(l_alpha + l_xi)) + x;
+
+                double pos_y = i*l1*sin(Units::deg2rad(l_xi))+
+                        j*l2*sin(Units::deg2rad(l_alpha + l_xi)) + y;
+
+                double pos_z = z;
+
+                // Check if the position lies within the boundaries of the 3D model
+                if(abs(pos_x) <= layer_size && abs(pos_y) <= layer_size
+                        && abs(pos_z) <= layer_thickness)
+                {
+                    // Randomly display a particle at the position given its abundance
+                    if (rand_num <= abundances.at(k)/total_abundance)
+                    {
+                        populateParticle(model, *particle,
+                                         QVector3D(static_cast<float>(pos_x),
+                                                   static_cast<float>(pos_y),
+                                                   static_cast<float>(pos_z)));
+                        break;
+                    }
+                    else
+                        ++k;
+                }
+            }
+        }
+    }
 }
 
 void RealSpaceBuilder::populateParticle(RealSpaceModel* model, const SessionItem& particleItem,

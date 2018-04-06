6705df6f2b9769ff60ea4d39a88be2c1ff6802c2
Dmitry Yurov <d.yurov@fz-juelich.de>
2018-04-06 11:13:25 +0200

    Removed SpecularData from SpecularSimulationElement
    Redmine: #2028

--- Tests/UnitTests/Core/SimulationElement/SpecularSimulationElementTest.h
+++ Tests/UnitTests/Core/SimulationElement/SpecularSimulationElementTest.h
@@ -13,7 +13,6 @@ protected:
                          const SpecularSimulationElement& rhs);
 
     PolarizationHandler test_polarization;
-    SpecularData test_specular_data;
     Eigen::Matrix2cd identity;
 };
 
@@ -24,14 +23,6 @@ SpecularSimulationElementTest::SpecularSimulationElementTest()
     polarization << 0, 1,
                     1, 0;
     test_polarization.setPolarization(polarization);
-
-    const size_t size = 10;
-    std::vector<ScalarRTCoefficients> test_data(size);
-    for (size_t i = 0; i < size; ++i) {
-        test_data[i].kz = 1.0;
-        test_data[i].lambda = 1.0;
-    }
-    test_specular_data = SpecularData(test_data);
 }
 
 SpecularSimulationElementTest::~SpecularSimulationElementTest() = default;
@@ -49,8 +40,6 @@ void SpecularSimulationElementTest::compareElements(const SpecularSimulationElem
     EXPECT_EQ(lhs.polarizationHandler().getPolarization(),
               rhs.polarizationHandler().getPolarization());
     EXPECT_EQ(rhs.polarizationHandler().getPolarization(), test_polarization.getPolarization());
-    EXPECT_EQ(lhs.specularData()[0].getScalarKz(), rhs.specularData()[0].getScalarKz());
-    EXPECT_EQ(rhs.specularData()[0].getScalarKz(), test_specular_data[0].getScalarKz());
     EXPECT_EQ(lhs.getWavelength(), rhs.getWavelength());
     EXPECT_EQ(lhs.getAlphaI(), rhs.getAlphaI());
     EXPECT_EQ(lhs.getIntensity(), rhs.getIntensity());
@@ -72,9 +61,6 @@ TEST_F(SpecularSimulationElementTest, InitialState)
     EXPECT_EQ(k_i, element.getKi());
     EXPECT_TRUE(element.isCalculated());
 
-    const SpecularData& spec_data = element.specularData();
-    EXPECT_FALSE(spec_data.isInited());
-
     EXPECT_EQ(identity, element.polarizationHandler().getPolarization());
     EXPECT_EQ(identity, element.polarizationHandler().getAnalyzerOperator());
 }
@@ -83,7 +69,6 @@ TEST_F(SpecularSimulationElementTest, CopyMoveAssign)
 {
     auto element = createDefaultElement();
 
-    element.setSpecular(test_specular_data);
     element.setPolarizationHandler(test_polarization);
     element.setCalculationFlag(false);
 

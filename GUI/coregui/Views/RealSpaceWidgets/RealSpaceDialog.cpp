42fed554b247d628fce0ace9f6d6c105581eec2f
anikhalder <halder.anik04@gmail.com>
2018-04-06 11:46:09 +0200

    Implement read and write settings of RealSpaceDialog on creation and closure

--- GUI/coregui/Views/RealSpaceWidgets/RealSpaceDialog.cpp
+++ GUI/coregui/Views/RealSpaceWidgets/RealSpaceDialog.cpp
@@ -1,14 +1,25 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      GUI/coregui/Views/RealSpaceWidgets/RealSpaceDialog.cpp
+//! @brief     Implements class RealSpaceDialog
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2018
+//! @authors   Scientific Computing Group at MLZ (see CITATION, AUTHORS)
+//
+// ************************************************************************** //
 
 #include "RealSpaceDialog.h"
 #include "RealSpaceWidget.h"
-
 #include "StyleUtils.h"
 #include <QAction>
 #include <QPushButton>
 #include <QSettings>
 #include <QVBoxLayout>
-
-#include <QApplication>
+#include "mainwindow_constants.h"
 
 namespace
 {
@@ -27,6 +38,9 @@ RealSpaceDialog::RealSpaceDialog(SampleModel *sampleModel,
     resize(default_dialog_size);
     setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 
+    // read the user's last settings (size and position) of the dialog box
+    readSettings();
+
     QVBoxLayout *layout = new QVBoxLayout;
     layout->setMargin(0);
     layout->setSpacing(0);
@@ -41,4 +55,33 @@ RealSpaceDialog::RealSpaceDialog(SampleModel *sampleModel,
     setLayout(layout);
     setAttribute(Qt::WA_DeleteOnClose, true);
     StyleUtils::setResizable(this);
+}
+
+void RealSpaceDialog::closeEvent(QCloseEvent *)
+{
+    // on close event, remember the settings (size and position) of the dialog box
+    writeSettings();
+}
+
+void RealSpaceDialog::readSettings()
+{
+    QSettings settings;
+    if (settings.childGroups().contains(Constants::S_REALSPACEDIALOG)) {
+        settings.beginGroup(Constants::S_REALSPACEDIALOG);
+        resize(settings.value(Constants::S_WINDOWSIZE, default_dialog_size).toSize());
+        if (settings.contains(Constants::S_WINDOWPOSITION)) {
+            move(settings.value(Constants::S_WINDOWPOSITION).toPoint());
+        }
+    } else {
+        resize(default_dialog_size);
+    }
+}
+
+void RealSpaceDialog::writeSettings()
+{
+    QSettings settings;
+    settings.beginGroup(Constants::S_REALSPACEDIALOG);
+    settings.setValue(Constants::S_WINDOWSIZE, this->size());
+    settings.setValue(Constants::S_WINDOWPOSITION, this->pos());
+    settings.endGroup();
 }
--- GUI/coregui/Views/RealSpaceWidgets/RealSpaceDialog.h
+++ GUI/coregui/Views/RealSpaceWidgets/RealSpaceDialog.h
@@ -1,3 +1,17 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      GUI/coregui/Views/RealSpaceWidgets/RealSpaceDialog.h
+//! @brief     Implements class RealSpaceDialog
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2018
+//! @authors   Scientific Computing Group at MLZ (see CITATION, AUTHORS)
+//
+// ************************************************************************** //
+
 #ifndef REALSPACEDIALOG_H
 #define REALSPACEDIALOG_H
 
@@ -7,14 +21,12 @@
 #include <SampleModel.h>
 #include <QTreeView>
 
-#include <QCloseEvent>
-
 class MaterialModel;
 class MaterialEditor;
 class ExternalProperty;
 class QBoxLayout;
 
-//! Dialog to hold RealSpaceViewer.
+//! Dialog to hold RealSpaceWidget.
 
 class BA_CORE_API_ RealSpaceDialog : public QDialog
 {
@@ -25,7 +37,14 @@ public:
                           QItemSelectionModel* selectionModel = nullptr,
                           QWidget *parent = nullptr);
 
+public slots:
+    void closeEvent(QCloseEvent*);
+
 private:
+    void init_material_editor();
+    void readSettings();
+    void writeSettings();
+
     SampleModel *m_sampleModel;
     QItemSelectionModel *m_selectionModel;
 };

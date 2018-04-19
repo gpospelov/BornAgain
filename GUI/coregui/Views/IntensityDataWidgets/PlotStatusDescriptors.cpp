9969c48d769c069087896537b33b1f9a0864f5ee
Dmitry Yurov <d.yurov@fz-juelich.de>
2018-04-19 16:44:14 +0200

    Removed obsolete code
    Redmine: #2036

--- GUI/coregui/Views/IntensityDataWidgets/PlotStatusDescriptors.cpp
+++ GUI/coregui/Views/IntensityDataWidgets/PlotStatusDescriptors.cpp
@@ -13,61 +13,8 @@
 // ************************************************************************** //
 
 #include "PlotStatusDescriptors.h"
+#include "PythonFormatting.h"
 #include "GUIHelpers.h"
-
-IPlotDescriptor::IPlotDescriptor()
-    : m_in_axes_range(false), m_x(0.0), m_y(0.0)
-{
-}
-
-IPlotDescriptor::~IPlotDescriptor() = default;
-
-SpecularPlotDescriptor::SpecularPlotDescriptor()
-    : IPlotDescriptor(), m_nx(0)
-{
-}
-
-SpecularPlotDescriptor::~SpecularPlotDescriptor() = default;
-
-QString SpecularPlotDescriptor::statusString() const
-{
-    QString result = QString(" [x: %1, y: %2]    [binx: %3]")
-                         .arg(QString::number(x(), 'f', 4))
-                         .arg(valueToString())
-                         .arg(m_nx, 2);
-
-    return result;
-}
-
-QString SpecularPlotDescriptor::valueToString() const
-{
-    return QString::fromStdString(PythonFormatting::printScientificDouble(y()));
-}
-
-ColorMapDescriptor::ColorMapDescriptor()
-    : IPlotDescriptor(), m_nx(0), m_ny(0), m_value(0), m_logz(false)
-{
-}
-
-ColorMapDescriptor::~ColorMapDescriptor() = default;
-
-QString ColorMapDescriptor::statusString() const
-{
-    QString result = QString(" [x: %1, y: %2]    [binx: %3, biny:%4]    [value: %5]")
-                         .arg(QString::number(x(), 'f', 4))
-                         .arg(QString::number(y(), 'f', 4), 2)
-                         .arg(m_nx, 2)
-                         .arg(m_ny, 2)
-                         .arg(valueToString());
-
-    return result;
-}
-
-QString ColorMapDescriptor::valueToString() const
-{
-    return m_logz ? QString::fromStdString(PythonFormatting::printScientificDouble(m_value))
-                  : QString::number(m_value, 'f', 2);
-}
 
 PlotEventInfo::PlotEventInfo()
     : PlotEventInfo(INFO_TYPE::Undefined)
--- GUI/coregui/Views/IntensityDataWidgets/PlotStatusDescriptors.h
+++ GUI/coregui/Views/IntensityDataWidgets/PlotStatusDescriptors.h
@@ -16,66 +16,7 @@
 #define PLOTSTATUSDESCRIPTORS_H
 
 #include "WinDllMacros.h"
-#include "PythonFormatting.h"
 #include <QString>
-
-//! Base for plot descriptors
-
-class BA_CORE_API_ IPlotDescriptor
-{
-public:
-    IPlotDescriptor();
-    virtual ~IPlotDescriptor();
-
-    virtual QString statusString() const = 0;
-
-    bool& inAxesRange() { return m_in_axes_range; }
-    bool inAxesRange() const { return m_in_axes_range; }
-    double& x() {return m_x;}
-    double x() const {return m_x;}
-    double& y() {return m_y;}
-    double y() const {return m_y;}
-
-private:
-    bool m_in_axes_range;
-    double m_x, m_y;
-};
-
-//! Contains parameters of 1D intensity data bin under current mouse position.
-
-class BA_CORE_API_ SpecularPlotDescriptor : public IPlotDescriptor
-{
-public:
-    SpecularPlotDescriptor();
-    ~SpecularPlotDescriptor() override;
-
-    QString statusString() const override;
-    int& nx() {return m_nx;}
-    int nx() const {return m_nx;}
-
-private:
-    QString valueToString() const;
-
-    int m_nx;
-};
-
-//! Contains parameters of 2D intensity data bin under current mouse position.
-
-class BA_CORE_API_ ColorMapDescriptor : public IPlotDescriptor
-{
-public:
-    ColorMapDescriptor();
-    ~ColorMapDescriptor() override;
-
-    QString statusString() const override;
-
-    int m_nx, m_ny;
-    double m_value;
-    bool m_logz;
-
-private:
-    QString valueToString() const;
-};
 
 class ColorMap;
 class SpecularPlot;

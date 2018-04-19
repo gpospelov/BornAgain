4504de3ac3ab3915ec411c92ca7aa54e1babd0fc
Dmitry Yurov <d.yurov@fz-juelich.de>
2018-04-19 14:45:18 +0200

    PlotEventInfo implementation
    Redmine: #2036

--- GUI/coregui/Views/IntensityDataWidgets/PlotStatusDescriptors.cpp
+++ GUI/coregui/Views/IntensityDataWidgets/PlotStatusDescriptors.cpp
@@ -13,6 +13,7 @@
 // ************************************************************************** //
 
 #include "PlotStatusDescriptors.h"
+#include "GUIHelpers.h"
 
 IPlotDescriptor::IPlotDescriptor()
     : m_in_axes_range(false), m_x(0.0), m_y(0.0)
@@ -66,4 +67,56 @@ QString ColorMapDescriptor::valueToString() const
 {
     return m_logz ? QString::fromStdString(PythonFormatting::printScientificDouble(m_value))
                   : QString::number(m_value, 'f', 2);
+}
+
+PlotEventInfo::PlotEventInfo()
+    : PlotEventInfo(INFO_TYPE::Undefined)
+{}
+
+PlotEventInfo::PlotEventInfo(const ColorMap*)
+    : PlotEventInfo(INFO_TYPE::Plot2D)
+{}
+
+PlotEventInfo::PlotEventInfo(const SpecularPlot*)
+    : PlotEventInfo(INFO_TYPE::Plot1D)
+{}
+
+QString PlotEventInfo::statusString() const
+{
+    checkPlotType();
+    QString result;
+    result = m_info_type == INFO_TYPE::Plot1D
+                 ? QString(" [x: %1, y: %2]    [binx: %3]")
+                       .arg(QString::number(x(), 'f', 4))
+                       .arg(valueToString())
+                       .arg(m_nx, 2)
+                 : QString(" [x: %1, y: %2]    [binx: %3, biny:%4]    [value: %5]")
+                       .arg(QString::number(x(), 'f', 4))
+                       .arg(QString::number(y(), 'f', 4), 2)
+                       .arg(m_nx, 2)
+                       .arg(m_ny, 2)
+                       .arg(valueToString());
+
+    return result;
+}
+
+PlotEventInfo::PlotEventInfo(INFO_TYPE type)
+    : m_in_axes_range(false), m_log_valued_axis(false)
+    , m_x(0.0), m_y(0.0), m_value(0.0)
+    , m_nx(0), m_ny(0)
+    , m_info_type(type)
+{}
+
+QString PlotEventInfo::valueToString() const
+{
+    checkPlotType();
+    return m_info_type == INFO_TYPE::Plot1D || m_log_valued_axis
+               ? QString::fromStdString(PythonFormatting::printScientificDouble(m_value))
+               : QString::number(m_value, 'f', 2);
+}
+
+void PlotEventInfo::checkPlotType() const
+{
+    if (m_info_type == INFO_TYPE::Undefined)
+        throw GUIHelpers::Error("Error in PlotEventInfo::checkPlotType: plot type is undefined.");
 }
--- GUI/coregui/Views/IntensityDataWidgets/PlotStatusDescriptors.h
+++ GUI/coregui/Views/IntensityDataWidgets/PlotStatusDescriptors.h
@@ -77,4 +77,52 @@ private:
     QString valueToString() const;
 };
 
+class ColorMap;
+class SpecularPlot;
+
+//! Contains parameters of mouse position in 1D or 2D plot.
+
+class BA_CORE_API_ PlotEventInfo
+{
+public:
+    PlotEventInfo();
+    PlotEventInfo(const ColorMap*);
+    PlotEventInfo(const SpecularPlot*);
+
+    QString statusString() const;
+
+    // setters, getters
+    void setInAxesRange(bool flag) { m_in_axes_range = flag; }
+    bool inAxesRange() const { return m_in_axes_range; }
+
+    void setLogValueAxis(bool flag) {m_log_valued_axis = flag;}
+    bool isLogValueAxis() const {return m_log_valued_axis;}
+
+    void setX(double x) {m_x = x;}
+    double x() const {return m_x;}
+    void setY(double y) {m_y = y;}
+    double y() const {return m_y;}
+
+    void setValue(double value) {m_value = value;}
+    double value() const {return m_value;}
+
+    void setNx(int nx) {m_nx = nx;}
+    int nx() const {return m_nx;}
+    void setNy(int ny) {m_ny = ny;}
+    int ny() const {return m_ny;}
+
+
+private:
+    enum class INFO_TYPE {Undefined, Plot1D, Plot2D};
+
+    PlotEventInfo(INFO_TYPE type);
+    QString valueToString() const;
+    void checkPlotType() const;
+
+    bool m_in_axes_range, m_log_valued_axis;
+    double m_x, m_y, m_value;
+    int m_nx, m_ny;
+    INFO_TYPE m_info_type;
+};
+
 #endif // PLOTSTATUSDESCRIPTORS_H

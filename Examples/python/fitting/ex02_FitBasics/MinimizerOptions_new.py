0d6f78bdcbea0d5d78a6a0c03eda336917a444c3
Gennady Pospelov <g.pospelov@fz-juelich.de>
2018-07-23 15:30:43 +0200

    Fitting example modified for FitObjective.

--- Examples/python/fitting/ex02_FitBasics/MinimizerOptions_new.py
+++ Examples/python/fitting/ex02_FitBasics/MinimizerOptions_new.py
@@ -5,11 +5,15 @@ import bornagain as ba
 from bornagain import deg, angstrom, nm
 
 
-def get_sample(cylinder_height=5.0*nm, cylinder_radius=5.0*nm,
-               prism_length=5.0*nm, prism_height=5.0*nm):
+def get_sample(params):
     """
     Returns a sample with uncorrelated cylinders and prisms on a substrate.
     """
+    cylinder_height = params["cylinder_height"]
+    cylinder_radius = params["cylinder_radius"]
+    prism_height = params["prism_height"]
+    prism_base_edge = params["prism_base_edge"]
+
     # defining materials
     m_air = ba.HomogeneousMaterial("Air", 0.0, 0.0)
     m_substrate = ba.HomogeneousMaterial("Substrate", 6e-6, 2e-8)
@@ -18,7 +22,7 @@ def get_sample(cylinder_height=5.0*nm, cylinder_radius=5.0*nm,
     # collection of particles
     cylinder_ff = ba.FormFactorCylinder(cylinder_radius, cylinder_height)
     cylinder = ba.Particle(m_particle, cylinder_ff)
-    prism_ff = ba.FormFactorPrism3(prism_length, prism_height)
+    prism_ff = ba.FormFactorPrism3(prism_base_edge, prism_height)
     prism = ba.Particle(m_particle, prism_ff)
     particle_layout = ba.ParticleLayout()
     particle_layout.addParticle(cylinder, 0.5)
@@ -36,7 +40,7 @@ def get_sample(cylinder_height=5.0*nm, cylinder_radius=5.0*nm,
     return multi_layer
 
 
-def get_simulation():
+def get_simulation(params):
     """
     Returns a GISAXS simulation with beam and detector defined
     """
@@ -45,12 +49,13 @@ def get_simulation():
                                      100, 0.0*deg, 2.0*deg)
     simulation.setBeamParameters(1.0*angstrom, 0.2*deg, 0.0*deg)
     simulation.setBeamIntensity(1e+08)
+    simulation.setSample(get_sample(params))
     return simulation
 
 
 def run_fitting():
     """
-    run fitting
+    main function to run fitting
     """
 
     # prints info about available minimizers
@@ -59,56 +64,54 @@ def run_fitting():
     # prints detailed info about available minimizers and their options
     print(ba.MinimizerFactory().catalogueDetailsToString())
 
-    sample = get_sample()
-    simulation = get_simulation()
-    simulation.setSample(sample)
-
     real_data = ba.IntensityDataIOFactory.readIntensityData(
-        'refdata_fitcylinderprisms.int.gz')
+        'refdata_fitcylinderprisms.int.gz').array()
 
-    fit_suite = ba.FitSuite()
-    fit_suite.addSimulationAndRealData(simulation, real_data)
-    fit_suite.initPrint(10)
+    fit_objective = ba.FitObjective()
+    fit_objective.addSimulationAndData(get_simulation, real_data, 1.0)
+    fit_objective.initPrint(10)
 
-    # setting fitting parameters with starting values
-    fit_suite.addFitParameter("*Cylinder/Height", 4.*nm).setLowerLimited(0.01)
-    fit_suite.addFitParameter("*Cylinder/Radius", 6.*nm).setLowerLimited(0.01)
-    fit_suite.addFitParameter("*Prism3/Height", 4.*nm).setLowerLimited(0.01)
-    fit_suite.addFitParameter("*Prism3/BaseEdge", 12.*nm).setLowerLimited(0.01)
+    params = ba.Parameters()
+    params.add("cylinder_height", 4.*nm, min=0.01)
+    params.add("cylinder_radius", 6.*nm, min=0.01)
+    params.add("prism_height", 4.*nm, min=0.01)
+    params.add("prism_base_edge", 12.*nm, min=0.01)
+
+    minimizer = ba.Minimizer()
 
     # Uncomment one of the line below to adjust minimizer settings
 
-    # setting Minuit2 minimizer with Migrad algorithm, limiting number of iterations
-    # Minimization will try to respect MaxFunctionCalls value
-    # fit_suite.setMinimizer("Minuit2", "Migrad", "MaxFunctionCalls=100")
+    """
+    Setting Minuit2 minimizer with Migrad algorithm, limiting number of iterations.
+    Minimization will try to respect MaxFunctionCalls value.
+    """
+    # minimizer.setMinimizer("Minuit2", "Migrad", "MaxFunctionCalls=50")
 
-    # Setting two options at once.
-    # Strategy=2 promises more accurate fit.
-    # fit_suite.setMinimizer("Minuit2", "Simplex", "MaxFunctionCalls=100;Strategy=2")
+    """
+    Setting two options at once.
+    Strategy=2 promises more accurate fit.    
+    """
+    # minimizer.setMinimizer("Minuit2", "Migrad", "MaxFunctionCalls=500;Strategy=2")
 
-    # setting Minuit2 minimizer with Fumili algorithm
-    # fit_suite.setMinimizer("Minuit2", "Fumili")
+    """
+    Setting Minuit2 minimizer with Fumili algorithm.
+    """
+    # minimizer.setMinimizer("Minuit2", "Fumili")
 
-    # Setting Levenberg-Marquardt algorithm
-    # fit_suite.setMinimizer("GSLLMA")
+    """
+    Setting Levenberg-Marquardt algorithm.
+    """
+    # minimizer.setMinimizer("GSLLMA")
 
-    # Setting Genetic algorithm.It requires all parameters
-    # to be limited, so we recreate parameters with min and max defined
-    # fit_suite.fitParameters().clear()
-    # fit_suite.addFitParameter("*Cylinder/Height", 4.*nm).setLimited(3.0, 8.0)
-    # fit_suite.addFitParameter("*Cylinder/Radius", 6.*nm).setLimited(3.0, 8.0)
-    # fit_suite.addFitParameter("*Prism3/Height", 4.*nm).setLimited(3.0, 8.0)
-    # fit_suite.addFitParameter("*Prism3/BaseEdge", 4.*nm).setLimited(3.0, 8.0)
-    # fit_suite.setMinimizer("Genetic", "Default",
-    # "MaxIterations=2;PopSize=200;RandomSeed=1")
+    result = minimizer.minimize(fit_objective.evaluate_residuals, params)
 
-    # running fit with default minimizer
-    fit_suite.runFit()
+    fit_objective.finalize(result)
 
     print("Fitting completed.")
-    print("chi2:", fit_suite.getChi2())
-    for par in fit_suite.fitParameters():
-        print(par.name(), par.value(), par.error())
+    print("chi2:", result.minValue())
+    for fitPar in result.parameters():
+        print(fitPar.name(), fitPar.value, fitPar.error)
+
 
 if __name__ == '__main__':
     run_fitting()

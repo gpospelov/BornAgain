07694109b46e430ad3fb923ff14df807e2c2f918
anikhalder <halder.anik04@gmail.com>
2018-07-24 13:05:10 +0200

    Move Particle3DContainer class to new file

--- GUI/coregui/Views/RealSpaceWidgets/RealSpaceBuilderUtils.cpp
+++ GUI/coregui/Views/RealSpaceWidgets/RealSpaceBuilderUtils.cpp
@@ -37,11 +37,11 @@
 #include "ParticleCompositionItem.h"
 #include "IParticle.h"
 #include "IFormFactorDecorator.h"
-#include "Particle.h"
 #include "ParticleCoreShellItem.h"
 #include "TransformTo3D.h"
 #include "ParticleDistribution.h"
 #include "InterferenceFunctions.h"
+#include "Particle3DContainer.h"
 
 namespace
 {
@@ -53,89 +53,6 @@ namespace
 
         return ff;
     }
-}
-
-// copy constructor
-Particle3DContainer::Particle3DContainer(const Particle3DContainer &other) :
-    m_cumulativeAbundance(other.m_cumulativeAbundance),
-    m_particleType(other.m_particleType)
-{
-    m_container.resize(other.m_container.size());
-    for (size_t i = 0; i < m_container.size(); ++i)
-        m_container[i] = new RealSpace::Particles::Particle(*other.m_container[i]);
-}
-
-// copy assignment
-Particle3DContainer &Particle3DContainer::operator=(const Particle3DContainer &rhs)
-{
-    if (this != &rhs)
-    {
-        clearContainer();
-        m_container.resize(rhs.containerSize());
-        for (size_t i = 0; i < m_container.size(); ++i)
-            m_container[i] = new RealSpace::Particles::Particle(*rhs.m_container[i]);
-
-        m_cumulativeAbundance = rhs.m_cumulativeAbundance;
-        m_particleType = rhs.m_particleType;
-    }
-    return *this;
-}
-
-Particle3DContainer::~Particle3DContainer() noexcept
-{
-    clearContainer();
-}
-
-// move constructor
-Particle3DContainer::Particle3DContainer(Particle3DContainer &&other) noexcept
-    : m_container(std::move(other.m_container))
-    , m_cumulativeAbundance(std::move(other.m_cumulativeAbundance))
-    , m_particleType(std::move(other.m_particleType))
-{
-}
-
-// move assignment
-Particle3DContainer &Particle3DContainer::operator=(Particle3DContainer &&rhs) noexcept
-{
-    if (this != &rhs)
-    {
-        clearContainer();
-        m_container = std::move(rhs.m_container);
-        m_cumulativeAbundance = std::move(rhs.m_cumulativeAbundance);
-        m_particleType = std::move(rhs.m_particleType);
-    }
-    return *this;
-}
-
-
-void Particle3DContainer::clearContainer()
-{
-    for (auto it = m_container.begin(); it != m_container.end(); ++it)
-        delete(*it);
-
-    m_container.clear();
-}
-
-void Particle3DContainer::addParticle(RealSpace::Particles::Particle *particle3D)
-{
-    m_container.emplace_back(particle3D);
-}
-
-void Particle3DContainer::setCumulativeAbundance(double cumulativeAbundance)
-{
-    m_cumulativeAbundance = cumulativeAbundance;
-}
-
-void Particle3DContainer::setParticleType(QString particleType)
-{
-    m_particleType = particleType;
-}
-
-std::unique_ptr<RealSpace::Particles::Particle> Particle3DContainer::createParticle(
-        const size_t &index) const
-{
-    auto particle = new RealSpace::Particles::Particle(*m_container.at(index));
-    return std::unique_ptr<RealSpace::Particles::Particle>(particle);
 }
 
 // compute cumulative abundances of particles
--- GUI/coregui/Views/RealSpaceWidgets/RealSpaceBuilderUtils.h
+++ GUI/coregui/Views/RealSpaceWidgets/RealSpaceBuilderUtils.h
@@ -16,7 +16,6 @@
 #define REALSPACEBUILDERUTILS_H
 
 #include "WinDllMacros.h"
-#include <QVector>
 #include <QVector3D>
 #include <memory>
 
@@ -30,37 +29,8 @@ class ParticleComposition;
 class ParticleCoreShell;
 class ParticleDistribution;
 class IInterferenceFunction;
+class Particle3DContainer;
 namespace RealSpace{ namespace Particles { class Particle; } }
-
-class BA_CORE_API_ Particle3DContainer
-{
-public:
-    Particle3DContainer() {m_cumulativeAbundance = 0;}
-    Particle3DContainer(const Particle3DContainer& other); // copy constructor
-    Particle3DContainer& operator=(const Particle3DContainer& rhs); // copy assignment
-    ~Particle3DContainer() noexcept;
-
-    Particle3DContainer(Particle3DContainer &&other) noexcept; // move constructor
-    Particle3DContainer& operator=(Particle3DContainer&& other) noexcept; // move assignment
-
-    void clearContainer();
-
-    size_t containerSize() const { return m_container.size(); }
-    double cumulativeAbundance() const { return m_cumulativeAbundance; }
-    const QString particleType() const { return m_particleType; }
-
-    void addParticle(RealSpace::Particles::Particle* particle3D);
-    void setCumulativeAbundance(double cumulativeAbundance);
-    void setParticleType(QString particleType);
-
-    std::unique_ptr<RealSpace::Particles::Particle> createParticle(const size_t &index) const;
-
-private:
-    std::vector<RealSpace::Particles::Particle*> m_container;
-    // Contains the constituents of the 3D Particle (e.g. core and shell of a ParticleCoreShell)
-    double m_cumulativeAbundance;
-    QString m_particleType; // type of 3D Particle e.g. ParticleType, ParticleCompositionType etc.
-};
 
 namespace RealSpaceBuilderUtils
 {

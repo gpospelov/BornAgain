44d9d436e7de8727390cf186a641bf462a75c957
Dmitry Yurov <d.yurov@fz-juelich.de>
2018-07-26 13:02:35 +0200

    New PlotterSpecularV2 for FitObjective
    Redmine: #2081

--- Wrap/python/plot_utils.py
+++ Wrap/python/plot_utils.py
@@ -217,7 +217,7 @@ class Plotter:
     def reset(self):
         self._fig.clf()
 
-    def plot(self, fit_suite):
+    def plot(self):
         self._fig.tight_layout()
         plt.pause(0.03)
 
@@ -265,7 +265,7 @@ class PlotterGISAS(Plotter):
         for index, fitPar in enumerate(fit_suite.fitParameters()):
             plt.text(0.01, 0.55 - index * 0.1, '{:30.30s}: {:6.3f}'.format(fitPar.name(), fitPar.value()))
 
-        Plotter.plot(self, fit_suite)
+        Plotter.plot(self)
 
 
 class PlotterGISASV2(Plotter):
@@ -317,7 +317,7 @@ class PlotterGISASV2(Plotter):
             plt.text(0.01, 0.55 - index * 0.1, '{:30.30s}: {:6.3f}'.format(key, params[key]))
             index = index + 1
 
-        Plotter.plot(self, fit_objective)
+        Plotter.plot(self)
 
 
 class PlotterSpecular(Plotter):
@@ -366,14 +366,12 @@ class PlotterSpecular(Plotter):
         trunc_length = 9  # max string field width in the table
         n_digits = 1  # number of decimal digits to print
         n_iterations = fit_suite.numberOfIterations()  # number of iterations
-        minimizer = fit_suite.minimizerName()
         rel_dif = fit_suite.relativeDifference().array().max()  # maximum relative difference
         fitted_parameters = fit_suite.fitParameters()
 
         # creating table content
         labels = ("Parameter", "Value")
-        table_data = [["Minimizer", '{:s}'.format(self.trunc_str(minimizer, trunc_length))],
-                      ["Iteration", '${:d}$'.format(n_iterations)],
+        table_data = [["Iteration", '${:d}$'.format(n_iterations)],
                       ["$d_{r, max}$", '${:s}$'.format(self.as_si(rel_dif, n_digits))]]
         for fitPar in fitted_parameters:
             table_data.append(['{:s}'.format(self.trunc_str(fitPar.name(), trunc_length)),
@@ -385,12 +383,6 @@ class PlotterSpecular(Plotter):
         axs.axis('off')
         table = plt.table(cellText=table_data, colLabels=labels, cellLoc='center',
                           loc='bottom left', bbox=[0.0, 0.0, 1.0, 1.0])
-
-        # # setting alignment to center in 0th column
-        # for key, cell in table.get_celld().items():
-        #     col = key[1]
-        #     if col == 0:
-        #         cell._loc = 'center'  # accessing private member is the only option to change alignment for one cell
 
     def plot_graph(self, fit_suite):
         # retrieving data from fit suite
@@ -421,7 +413,108 @@ class PlotterSpecular(Plotter):
         self.plot_graph(fit_suite)
         self.plot_table(fit_suite)
 
-        Plotter.plot(self, fit_suite)
+        Plotter.plot(self)
+
+
+class PlotterSpecularV2(Plotter):
+    """
+    Draws fit progress. Intended specifically for observing
+    specular data fit.
+    """
+
+    def __init__(self):
+        Plotter.__init__(self)
+        self.gs = gridspec.GridSpec(1, 2, width_ratios=[2.5, 1], wspace=0)
+
+    def __call__(self, fit_objective):
+        self.plot(fit_objective)
+
+    @staticmethod
+    def as_si(val, ndp):
+        """
+        Fancy print of scientific-formatted values
+        :param val: numeric value
+        :param ndp: number of decimal digits to print
+        :return: a string corresponding to the _val_
+        """
+        s = '{x:0.{ndp:d}e}'.format(x=val, ndp=ndp)
+        m, e = s.split('e')
+        return r'{m:s}\times 10^{{{e:d}}}'.format(m=m, e=int(e))
+
+    @staticmethod
+    def trunc_str(token, length):
+        """
+        Truncates token if it is longer than length.
+
+        Example:
+            trunc_str("123456789", 8) returns "123456.."
+
+            trunc_str("123456789", 9) returns "123456789"
+
+        :param token: input string
+        :param length: max non-truncated length
+        :return:
+        """
+        return (token[:length - 2] + '..') if len(token) > length else token
+
+    def plot_table(self, fit_objective):
+
+        iteration_info = fit_objective.iterationInfo()
+
+        trunc_length = 9  # max string field width in the table
+        n_digits = 1  # number of decimal digits to print
+
+        n_iterations = iteration_info.iterationCount()  # current number of iterations passed
+        rel_dif = fit_objective.relativeDifference().array().max()  # maximum relative difference
+        fitted_parameters = iteration_info.parameterMap()
+
+        # creating table content
+        labels = ("Parameter", "Value")
+        table_data = [["Iteration", '${:d}$'.format(n_iterations)],
+                      ["$d_{r, max}$", '${:s}$'.format(self.as_si(rel_dif, n_digits))]]
+        for key, value in fitted_parameters.iteritems():
+            table_data.append(['{:s}'.format(self.trunc_str(key, trunc_length)),
+                               '${:s}$'.format(self.as_si(value, n_digits))])
+
+        # creating table
+        axs = plt.subplot(self.gs[1])
+        axs.axis('tight')
+        axs.axis('off')
+        table = plt.table(cellText=table_data, colLabels=labels, cellLoc='center',
+                          loc='bottom left', bbox=[0.0, 0.0, 1.0, 1.0])
+
+    def plot_graph(self, fit_objective):
+        # retrieving data from fit suite
+        real_data = fit_objective.experimentalData().data()
+        sim_data = fit_objective.simulationResult().data()
+
+        # data values
+        sim_values = sim_data.getArray()
+        real_values = real_data.getArray()
+
+        # default font properties dictionary to use
+        font = {'family': 'serif',
+                'weight': 'normal',
+                'size': label_fontsize}
+
+        plt.subplot(self.gs[0])
+        plt.semilogy(sim_data.getAxis(0).getBinCenters(), sim_values, 'b',
+                     real_data.getAxis(0).getBinCenters(), real_values, 'k--')
+        plt.ylim((0.5 * np.min(real_values), 5 * np.max(real_values)))
+
+        xlabel = get_axes_labels(fit_objective.experimentalData(), ba.AxesUnits.DEFAULT)[0]
+        plt.legend(['BornAgain', 'Reference'], loc='upper right', prop=font)
+        plt.xlabel(xlabel, fontdict=font)
+        plt.ylabel("Intensity", fontdict=font)
+        plt.title("Specular data fitting", fontdict=font)
+
+    def plot(self, fit_objective):
+        Plotter.reset(self)
+
+        self.plot_graph(fit_objective)
+        self.plot_table(fit_objective)
+
+        Plotter.plot(self)
 
 
 class DefaultFitObserver(IFitObserver):

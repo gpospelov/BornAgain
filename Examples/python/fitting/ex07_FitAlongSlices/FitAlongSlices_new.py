db10430bfe1b3b9abe192fdda5eef57dade5faed
Gennady Pospelov <g.pospelov@fz-juelich.de>
2018-07-27 15:04:35 +0200

    Example modified to new FitObjective

--- Examples/python/fitting/ex07_FitAlongSlices/FitAlongSlices_new.py
+++ Examples/python/fitting/ex07_FitAlongSlices/FitAlongSlices_new.py
@@ -3,7 +3,6 @@ Fitting example: fit along slices
 """
 
 import numpy as np
-import matplotlib
 from matplotlib import pyplot as plt
 import bornagain as ba
 from bornagain import deg, angstrom, nm
@@ -12,10 +11,13 @@ phi_slice_value = 0.0  # position of vertical slice in deg
 alpha_slice_value = 0.2  # position of horizontal slice in deg
 
 
-def get_sample(radius=5.0*nm, height=10.0*nm):
+def get_sample(params):
     """
     Returns a sample with uncorrelated cylinders on a substrate.
     """
+    radius = params["radius"]
+    height = params["height"]
+
     m_air = ba.HomogeneousMaterial("Air", 0.0, 0.0)
     m_substrate = ba.HomogeneousMaterial("Substrate", 6e-6, 2e-8)
     m_particle = ba.HomogeneousMaterial("Particle", 6e-4, 2e-8)
@@ -36,7 +38,7 @@ def get_sample(radius=5.0*nm, height=10.0*nm):
     return multi_layer
 
 
-def get_simulation():
+def get_simulation(params, add_masks=True):
     """
     Create and return GISAXS simulation with beam and detector defined
     """
@@ -45,6 +47,14 @@ def get_simulation():
                                      100, 0.0*deg, 2.0*deg)
     simulation.setBeamParameters(1.0*angstrom, 0.2*deg, 0.0*deg)
     simulation.setBeamIntensity(1e+08)
+    simulation.setSample(get_sample(params))
+    if add_masks:
+        # At this point we mask all the detector and then unmask two areas
+        # corresponding to the vertical and horizontal lines. This will make
+        # simulation/fitting to be performed along slices only.
+        simulation.maskAll()
+        simulation.addMask(ba.HorizontalLine(alpha_slice_value*deg), False)
+        simulation.addMask(ba.VerticalLine(phi_slice_value*deg), False)
     return simulation
 
 
@@ -52,9 +62,9 @@ def create_real_data():
     """
     Generating "real" data by adding noise to the simulated data.
     """
-    sample = get_sample(5.0*nm, 10.0*nm)
-    simulation = get_simulation()
-    simulation.setSample(sample)
+    params = {'radius': 5.0*nm, 'height': 10.0*nm}
+
+    simulation = get_simulation(params, add_masks=False)
     simulation.runSimulation()
 
     # retrieving simulated data in the form of numpy array
@@ -67,14 +77,13 @@ def create_real_data():
     return noisy
 
 
-class DrawObserver(ba.IFitObserver):
+class PlotObserver():
     """
     Draws fit progress every nth iteration. Here we plot slices along real
     and simulated images to see fit progress.
     """
 
-    def __init__(self, draw_every_nth=10):
-        ba.IFitObserver.__init__(self, draw_every_nth)
+    def __init__(self):
         self.fig = plt.figure(figsize=(10.25, 7.69))
         self.fig.canvas.draw()
         plt.ion()
@@ -103,27 +112,31 @@ class DrawObserver(ba.IFitObserver):
         plt.legend(loc='upper right')
         plt.title(title)
 
-    def display_fit_parameters(self, fit_suite, nplot):
+    def display_fit_parameters(self, fit_objective, nplot):
         plt.subplot(2, 2, nplot)
         plt.title('Parameters')
         plt.axis('off')
-        plt.text(0.01, 0.85, "Iteration  " + '{:d}     {:s}'.
-                 format(fit_suite.numberOfIterations(),
-                        fit_suite.minimizer().minimizerName()))
-        plt.text(0.01, 0.75, "Chi2       " + '{:8.4f}'.format(fit_suite.getChi2()))
-        for index, fitPar in enumerate(fit_suite.fitParameters()):
-            plt.text(0.01, 0.55 - index*0.1,
-                     '{:30.30s}: {:6.3f}'.format(fitPar.name(), fitPar.value()))
+
+        iteration_info = fit_objective.iterationInfo()
+
+        plt.text(0.01, 0.85, "Iterations  " + '{:d}'.
+                 format(iteration_info.iterationCount()))
+        plt.text(0.01, 0.75, "Chi2       " + '{:8.4f}'.format(iteration_info.chi2()))
+        index = 0
+        params = iteration_info.parameterMap()
+        for key in params:
+            plt.text(0.01, 0.55 - index * 0.1, '{:30.30s}: {:6.3f}'.format(key, params[key]))
+            index = index + 1
 
         plt.tight_layout()
         plt.draw()
         plt.pause(0.01)
 
-    def update(self, fit_suite):
+    def update(self, fit_objective):
         self.fig.clf()
 
-        real_data = fit_suite.experimentalData().histogram2d()
-        simul_data = fit_suite.simulationResult().histogram2d()
+        real_data = fit_objective.experimentalData().histogram2d()
+        simul_data = fit_objective.simulationResult().histogram2d()
 
         # plot real data
         self.plot_real_data(real_data, nplot=1)
@@ -146,10 +159,7 @@ class DrawObserver(ba.IFitObserver):
         self.plot_slices(slices, title, nplot=3)
 
         # display fit parameters
-        self.display_fit_parameters(fit_suite, nplot=4)
-
-        if fit_suite.isLastIteration():
-            plt.ioff()
+        self.display_fit_parameters(fit_objective, nplot=4)
 
 
 def run_fitting():
@@ -159,35 +169,21 @@ def run_fitting():
 
     real_data = create_real_data()
 
-    sample = get_sample()
-    simulation = get_simulation()
-    simulation.setSample(sample)
+    fit_objective = ba.FitObjective()
+    fit_objective.addSimulationAndData(get_simulation, real_data, 1.0)
+    fit_objective.initPrint(10)
 
-    # At this point we mask all the detector and then unmask two areas
-    # corresponding to the vertical and horizontal lines. This will make
-    # simulation/fitting to be performed along slices only.
-    simulation.maskAll()
-    simulation.addMask(ba.HorizontalLine(alpha_slice_value*deg), False)
-    simulation.addMask(ba.VerticalLine(phi_slice_value*deg), False)
+    # creating custom observer which will draw fit progress
+    plotter = PlotObserver()
+    fit_objective.initPlot(10, plotter.update)
 
-    fit_suite = ba.FitSuite()
-    fit_suite.addSimulationAndRealData(simulation, real_data)
-    fit_suite.initPrint(5)
+    params = ba.Parameters()
+    params.add("radius", 6.*nm, min=4.0, max=8.0)
+    params.add("height", 9.*nm, min=8.0, max=12.0)
 
-    draw_observer = DrawObserver(draw_every_nth=5)
-    fit_suite.attachObserver(draw_observer)
-
-    # setting fitting parameters with starting values
-    fit_suite.addFitParameter("*/Cylinder/Radius", 6.*nm).setLimited(4., 8.)
-    fit_suite.addFitParameter("*/Cylinder/Height", 9.*nm).setLimited(8., 12.)
-
-    # running fit
-    fit_suite.runFit()
-
-    print("Fitting completed.")
-    print("chi2:", fit_suite.getChi2())
-    for fitPar in fit_suite.fitParameters():
-        print(fitPar.name(), fitPar.value(), fitPar.error())
+    minimizer = ba.Minimizer()
+    result = minimizer.minimize(fit_objective.evaluate, params)
+    fit_objective.finalize(result)
 
 
 if __name__ == '__main__':

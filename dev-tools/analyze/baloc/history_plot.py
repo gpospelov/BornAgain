1ca2693843925864f4f44d37c12c7ebf12650be1
Gennady Pospelov <g.pospelov@fz-juelich.de>
2018-09-11 11:42:39 +0200

    Cleanup in lines-of-code plotting

--- dev-tools/analyze/baloc/history_plot.py
+++ dev-tools/analyze/baloc/history_plot.py
@@ -4,33 +4,39 @@ from datetime import datetime
 from .file_types import FileTypes, filetype
 
 
-def history_plot(filename):
-    selected_hist = [FileTypes.CORE, FileTypes.FTEST, FileTypes.UTEST, FileTypes.GUI, FileTypes.PYAPI]
+selected_file_type = [FileTypes.CORE, FileTypes.FTEST, FileTypes.UTEST, FileTypes.GUI, FileTypes.PYAPI]
 
+
+def read_history(filename):
     xvals = []  # Time
-    yvals = {}  # id of file type .vs. LOC .vs. time
-    for x in selected_hist:
-        yvals[x] = []
+    ydata = {}  # id of file type .vs. LOC .vs. time
+    for x in selected_file_type:
+        ydata[x] = []
 
     print("Reading file {0}".format(filename))
     with open(filename, 'r') as the_file:
         for line in the_file:
-            line = line.strip()
             parts = line.strip().split()
-            date = datetime.strptime(parts[0]+ " " + parts[1], '%Y-%m-%d %H:%M:%S')
+            date = datetime.strptime(parts[0] + " " + parts[1], '%Y-%m-%d %H:%M:%S')
             xvals.append(date)
-            for x in selected_hist:
-                yvals[x].append(int(parts[2+x]))
+            for x in selected_file_type:
+                ydata[x].append(int(parts[2+x]))
 
-    ypoints = []
+    yvals = []
     descr = []
-    for key in yvals:
+    for key in ydata:
         descr.append(FileTypes.descr[key])
-        ypoints.append(yvals[key])
+        yvals.append(ydata[key])
 
     # printing summary of LOC
-    for x in range(0, len(ypoints)):
-        print("{:18} : {:10}".format(descr[x], ypoints[x][-1]))
+    for x in range(0, len(yvals)):
+        print("{:18} : {:10}".format(descr[x], yvals[x][-1]))
+
+    return xvals, yvals, descr
+
+
+def history_plot(filename):
+    xvals, yvals, descr = read_history(filename)
 
     # figure size
     my_dpi = 96
@@ -38,9 +44,9 @@ def history_plot(filename):
     plt.style.use('seaborn-bright')
 
     # making stackplot
-    plt.stackplot(xvals, ypoints)
+    plt.stackplot(xvals, yvals)
     pal = ["#3399ff", "#ffcc00", "#ff0000", "#0033ff", "#999999"]
-    plt.stackplot(xvals, ypoints, labels=descr, colors=pal)
+    plt.stackplot(xvals, yvals, labels=descr, colors=pal)
 
     # styling axes and grid
     plt.grid(color='gray', linestyle='dashed')

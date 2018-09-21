192361b22e4068f84ec26a34018f7e9cb255d3b5
anikhalder <halder.anik04@gmail.com>
2018-09-21 16:03:48 +0200

    Edit tool tip for view buttons

--- GUI/coregui/Views/RealSpaceWidgets/RealSpaceToolBar.cpp
+++ GUI/coregui/Views/RealSpaceWidgets/RealSpaceToolBar.cpp
@@ -39,7 +39,7 @@ RealSpaceToolBar::RealSpaceToolBar(QWidget* parent)
     m_defaultViewButton->setText("Default View");
     //m_defaultViewButton->setIcon(QIcon(":/SampleDesigner/images/toolbar_recycle.png"));
     m_defaultViewButton->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
-    m_defaultViewButton->setToolTip("Default view of the sample");
+    m_defaultViewButton->setToolTip("Reset view and zoom level to default");
     connect(m_defaultViewButton, &QToolButton::clicked, this, &RealSpaceToolBar::defaultViewAction);
     addWidget(m_defaultViewButton);
 
@@ -49,7 +49,7 @@ RealSpaceToolBar::RealSpaceToolBar(QWidget* parent)
     m_sideViewButton->setText("Side View");
     //m_sideViewButton->setIcon(QIcon(":/SampleDesigner/images/toolbar_recycle.png"));
     m_sideViewButton->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
-    m_sideViewButton->setToolTip("View the sample from the side");
+    m_sideViewButton->setToolTip("View sample from the side at current zoom level");
     connect(m_sideViewButton, &QToolButton::clicked, this, &RealSpaceToolBar::sideViewAction);
     addWidget(m_sideViewButton);
 
@@ -59,7 +59,7 @@ RealSpaceToolBar::RealSpaceToolBar(QWidget* parent)
     m_topViewButton->setText("Top View");
     //m_topViewButton->setIcon(QIcon(":/SampleDesigner/images/toolbar_recycle.png"));
     m_topViewButton->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
-    m_topViewButton->setToolTip("View the sample from the top");
+    m_topViewButton->setToolTip("View sample from the top at current zoom level");
     connect(m_topViewButton, &QToolButton::clicked, this, &RealSpaceToolBar::topViewAction);
     addWidget(m_topViewButton);
 

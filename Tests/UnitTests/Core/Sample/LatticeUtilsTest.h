94d43fd833178efc685378c7fe271547b84978a3
Walter Van Herck <w.van.herck@fz-juelich.de>
2018-09-27 16:55:02 +0200

    Added unit test for HCP lattice

--- Tests/UnitTests/Core/Sample/LatticeUtilsTest.h
+++ Tests/UnitTests/Core/Sample/LatticeUtilsTest.h
@@ -44,3 +44,22 @@ TEST_F(LatticeUtilsTest, diagonalAlignedFCCTest)
     EXPECT_EQ(a1.x(), a2.x());
     EXPECT_EQ(a1.y(), -a2.y());
 }
+
+// tests the creation of an HCP lattice
+TEST_F(LatticeUtilsTest, trivialAlignedHCPTest)
+{
+    MillerIndexOrientation trivial_aligned(MillerIndexOrientation::QZ, { 0, 0, 1},
+                                        MillerIndexOrientation::QX, { 2, -1, 0});
+    auto lattice = LatticeUtils::CreateHCPLattice(2.0, 4.0, trivial_aligned);
+    auto a1 = lattice.getBasisVectorA();
+    auto a2 = lattice.getBasisVectorB();
+    auto a3 = lattice.getBasisVectorC();
+
+    kvector_t v1 { 2.0, 0.0, 0.0 };
+    kvector_t v2 { -1.0, std::sqrt(3.0), 0.0 };
+    kvector_t v3 { 1.0, 1.0/std::sqrt(3.0), 2.0 };
+
+    EXPECT_EQ(a1, v1);
+    EXPECT_EQ(a2, v2);
+    EXPECT_EQ(a3, v3);
+}

f20db12d12b867d3b285648c316745b1c3838109
anikhalder <halder.anik04@gmail.com>
2018-10-15 15:12:40 +0200

    Add sampler class for drawing xy pair from 2D Gauss distribution

--- Core/Aggregate/IDistribution2DSampler.h
+++ Core/Aggregate/IDistribution2DSampler.h
@@ -38,4 +38,16 @@ private:
     double m_omega_x, m_omega_y;
 };
 
+class BA_CORE_API_ Distribution2DGaussSampler : public IDistribution2DSampler
+{
+public:
+    Distribution2DGaussSampler(double omega_x, double omega_y) :
+        m_omega_x(omega_x), m_omega_y(omega_y) {}
+    std::pair<double, double> randomSample() const final;
+
+private:
+    double m_omega_x, m_omega_y;
+};
+
+
 #endif // IDISTRIBUTION2DSAMPLER_H

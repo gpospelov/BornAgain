e7749dbc8625c2dbebc069ddbae982b2570000b5
anikhalder <halder.anik04@gmail.com>
2018-10-15 12:50:50 +0200

    Add IDistribution2DSampler class

---
+++ Core/Aggregate/IDistribution2DSampler.cpp
@@ -0,0 +1,20 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Core/Aggregate/FTDistributions1D.h
+//! @brief     Defines interface class IFTDistribution1D, and children thereof
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2018
+//! @authors   Scientific Computing Group at MLZ (see CITATION, AUTHORS)
+//
+// ************************************************************************** //
+
+#include "IDistribution2DSampler.h"
+
+IDistribution2DSampler::IDistribution2DSampler()
+{
+
+}
---
+++ Core/Aggregate/IDistribution2DSampler.h
@@ -0,0 +1,25 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Core/Aggregate/FTDistributions1D.h
+//! @brief     Defines interface class IFTDistribution1D, and children thereof
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2018
+//! @authors   Scientific Computing Group at MLZ (see CITATION, AUTHORS)
+//
+// ************************************************************************** //
+
+#ifndef IDISTRIBUTION2DSAMPLER_H
+#define IDISTRIBUTION2DSAMPLER_H
+
+
+class IDistribution2DSampler
+{
+public:
+    IDistribution2DSampler();
+};
+
+#endif // IDISTRIBUTION2DSAMPLER_H

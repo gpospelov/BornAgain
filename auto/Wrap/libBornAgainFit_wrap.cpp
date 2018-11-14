ece601517c4c7c1a6842630c67bf71895b76f5fe
Walter Van Herck <w.van.herck@fz-juelich.de>
2018-11-14 13:01:04 +0100

    Regenerate Python API

--- auto/Wrap/libBornAgainFit_wrap.cpp
+++ auto/Wrap/libBornAgainFit_wrap.cpp
@@ -5800,7 +5800,7 @@ SWIG_AsVal_std_complex_Sl_double_Sg_  (PyObject *o, std::complex<double>* val)
 
 
 SWIGINTERNINLINE PyObject*
-SWIG_From_std_complex_Sl_double_Sg_  (/*@SWIG:/home/pospelov/software/local/share/swig/3.0.12/typemaps/swigmacros.swg,104,%ifcplusplus@*/
+SWIG_From_std_complex_Sl_double_Sg_  (/*@SWIG:/usr/local/share/swig/3.0.12/typemaps/swigmacros.swg,104,%ifcplusplus@*/
 
 const std::complex<double>&
 

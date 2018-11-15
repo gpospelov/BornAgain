4ffa683b6a5fad35ee1a0d78a9ea772208bafb01
Gennady Pospelov <g.pospelov@fz-juelich.de>
2018-11-15 18:04:19 +0100

    Move CMake policies in common file

---
+++ cmake/bornagain/modules/BornAgainPolicy.cmake
@@ -0,0 +1,25 @@
+if(APPLE)
+if(POLICY CMP0025)
+    cmake_policy(SET CMP0025 NEW)
+endif()
+endif()
+
+if(POLICY CMP0020)
+    cmake_policy(SET CMP0020 NEW)
+endif()
+
+if(POLICY CMP0028)
+    cmake_policy(SET CMP0028 NEW)
+endif()
+
+if(POLICY CMP0042)
+    cmake_policy(SET CMP0042 NEW)
+endif()
+
+if(POLICY CMP0043)
+    cmake_policy(SET CMP0043 NEW)
+endif()
+
+if(POLICY CMP0071)
+  cmake_policy(SET CMP0071 NEW)
+endif()

926638d51ffb398fd13ab3008e5416b417673b19
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-01-29 10:10:25 +0100

    Win dll fix

--- Core/Tools/SpectrumUtils.h
+++ Core/Tools/SpectrumUtils.h
@@ -15,21 +15,22 @@
 #ifndef SPECTRUMUTILS_H
 #define SPECTRUMUTILS_H
 
-#include "WinDllMacros.h"
 #include "Histogram2D.h"
-#include <vector>
+#include "WinDllMacros.h"
 #include <string>
+#include <vector>
 
 class Histogram2D;
 
 //! Collection of utils for 1D and 2D spectrum processing (background, peaks, ets).
 
-namespace SpectrumUtils {
+namespace SpectrumUtils
+{
 
-std::vector<std::pair<double, double>> FindPeaks(const Histogram2D& hist,
-                                                 double sigma=2,
-                                                 const std::string& option={},
-                                                 double threshold=0.05);
+BA_CORE_API_ std::vector<std::pair<double, double>> FindPeaks(const Histogram2D& hist,
+                                                              double sigma = 2,
+                                                              const std::string& option = {},
+                                                              double threshold = 0.05);
 }
 
 #endif // SPECTRUMUTILS_H

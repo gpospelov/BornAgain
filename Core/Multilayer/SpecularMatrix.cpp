066f60101dc8dcc18701adf0c6c3144138afb118
Dmitry Yurov <d.yurov@fz-juelich.de>
2019-02-01 14:35:15 +0100

    Re-implementation of r- and t-coefficients computation

--- Core/Multilayer/SpecularMatrix.cpp
+++ Core/Multilayer/SpecularMatrix.cpp
@@ -19,24 +19,26 @@
 #include "MathFunctions.h"
 #include "MultiLayer.h"
 #include "MathConstants.h"
+#include <Eigen/Dense>
 #include <stdexcept>
+#include <valarray>
 
 namespace {
-    const complex_t imag_unit = complex_t(0.0, 1.0);
+Eigen::Matrix2cd transitionMatrix(complex_t kzi, complex_t kzi1, double sigma, double thickness);
+void computeTR(std::vector<ScalarRTCoefficients>& coeff, const MultiLayer& sample);
+bool zeroTransmission(const Eigen::Matrix2cd& m);
+void setZeroBelow(std::vector<ScalarRTCoefficients>& coeff, size_t current_layer);
+
+const complex_t imag_unit = complex_t(0.0, 1.0);
+const double pi2_15 = std::pow(M_PI_2, 1.5);
 }
 
-void setZeroBelow(std::vector<ScalarRTCoefficients>& coeff, size_t current_layer);
 bool calculateUpFromLayer(
     std::vector<ScalarRTCoefficients>& coeff, const MultiLayer& sample,
     const double kmag, size_t layer_index);
 size_t bisectRTcomputation(
     std::vector<ScalarRTCoefficients>& coeff, const MultiLayer& sample,
     const double kmag, const size_t lgood, const size_t lbad, const size_t l);
-
-//! Computes refraction angles and transmission/reflection coefficients
-//! for given coherent wave propagation in a multilayer.
-//! Roughness is modelled by tanh profile [see e.g. Phys. Rev. B, vol. 47 (8), p. 4385 (1993)].
-//! k : length: wavenumber in vacuum, direction: defined in layer 0.
 
 void SpecularMatrix::execute(const MultiLayer& sample, const kvector_t k,
                              std::vector<ScalarRTCoefficients>& coeff)
@@ -101,14 +103,39 @@ void SpecularMatrix::execute(const MultiLayer& sample, const kvector_t k,
     }
 }
 
-//! Sets coeff to zero for all layers below current_layer.
-
-void setZeroBelow(std::vector<ScalarRTCoefficients>& coeff, size_t current_layer)
+void SpecularMatrix::execute_(const MultiLayer& sample, kvector_t k,
+                              std::vector<ScalarRTCoefficients>& coeff)
 {
-    size_t N = coeff.size();
-    for (size_t i=current_layer+1; i<N; ++i) {
-        coeff[i].t_r.setZero();
+    size_t N = sample.numberOfLayers();
+    coeff.clear();
+    coeff.resize(N);
+
+    double n_ref = sample.layer(0)->material()->refractiveIndex(2 * M_PI / k.mag()).real();
+
+    // Calculate refraction angle, expressed as lambda or k_z, for each layer.
+    double sign_kz_out = k.z() > 0.0 ? -1.0 : 1.0;
+    for (size_t i = 0; i < N; ++i) {
+        complex_t rad = sample.layer(i)->scalarReducedPotential(k, n_ref);
+        // use small absorptive component for layers with i>0 if radicand becomes very small:
+        if (i > 0 && std::norm(rad) < 1e-40)
+            rad = imag_unit * 1e-40;
+        coeff[i].lambda = sqrt(rad);
+        coeff[i].kz = sign_kz_out * k.mag() * coeff[i].lambda;
     }
+
+    coeff[0].t_r(0) = 1.0;
+    coeff[0].t_r(1) = 0.0;
+
+    if (N == 1) // If only one layer present, there's nothing left to calculate
+        return;
+    else if (coeff[0].kz == 0.0) { // If kz in layer 0 is zero, R0 = -T0 and all others equal to 0
+        coeff[0].t_r(1) = -1.0;
+        setZeroBelow(coeff, 0);
+        return;
+    }
+
+    // Calculate transmission/refraction coefficients t_r for each layer, from top to bottom.
+    computeTR(coeff, sample);
 }
 
 //! Computes RT coefficients coeff, starting from layer number layer_index.
@@ -174,4 +201,71 @@ size_t bisectRTcomputation(
         }
         return bisectRTcomputation(coeff, sample, kmag, lgood, l, (lgood+l)/2);
     }
+}
+
+namespace  {
+Eigen::Matrix2cd transitionMatrix(complex_t kzi, complex_t kzi1, double sigma, double thickness)
+{
+    complex_t roughness = 1;
+    if (sigma > 0.0) {
+        const double sigeff = pi2_15 * sigma;
+        roughness =
+            std::sqrt(MathFunctions::tanhc(sigeff * kzi1) / MathFunctions::tanhc(sigeff * kzi));
+    }
+    const complex_t phase_shift = exp_I(kzi * thickness);
+    const complex_t kz_r_by_rough = kzi / kzi1 / roughness;
+
+    const complex_t a00 = 0.5 * (roughness + kz_r_by_rough);
+    const complex_t a01 = 0.5 * (roughness - kz_r_by_rough);
+    // a10 = a01, a11 = a00
+
+    Eigen::Matrix2cd result;
+    result << a00 * phase_shift, a01 / phase_shift, a01 * phase_shift, a00 / phase_shift;
+    return result;
+}
+
+void computeTR(std::vector<ScalarRTCoefficients>& coeff, const MultiLayer& sample)
+{
+    const size_t coeff_size = coeff.size();
+    std::valarray<Eigen::Vector2cd> m_tr_l({0, 0}, coeff_size);
+    std::valarray<Eigen::Vector2cd> m_tr_r({0, 0}, coeff_size);
+    Eigen::Matrix2cd acc = Eigen::Matrix2cd::Identity();
+    m_tr_l[0] = {1, 0};
+    m_tr_r[0] = {0, 1};
+
+    complex_t kz_previous = coeff[0].getScalarKz();
+    size_t i = 1;
+    for (; i < coeff_size; ++i) {
+        const size_t im1 = i - 1;
+        double sigma = 0.0;
+        if (const LayerRoughness* roughness = sample.layerInterface(im1)->getRoughness())
+            sigma = roughness->getSigma();
+        const complex_t kz = coeff[i].getScalarKz();
+        acc = transitionMatrix(kz_previous, kz, sigma, sample.layer(im1)->thickness()) * acc;
+        if (zeroTransmission(acc))
+            break;
+
+        kz_previous = kz;
+        m_tr_l[i] = acc.col(0);
+        m_tr_r[i] = acc.col(1);
+    }
+    const complex_t r_0 = -m_tr_l[i - 1](1) / m_tr_r[i - 1](1);
+
+    for (size_t i = 0, size = coeff.size(); i < size; ++i)
+        coeff[i].t_r = m_tr_l[i] + r_0 * m_tr_r[i];
+}
+
+bool zeroTransmission(const Eigen::Matrix2cd& m)
+{
+    const double norm = std::norm(m(1, 1));
+    return std::norm(m.determinant()) < norm * norm * 1e-40;
+}
+
+void setZeroBelow(std::vector<ScalarRTCoefficients>& coeff, size_t current_layer)
+{
+    size_t N = coeff.size();
+    for (size_t i = current_layer + 1; i < N; ++i) {
+        coeff[i].t_r.setZero();
+    }
+}
 }
--- Core/Multilayer/SpecularMatrix.h
+++ Core/Multilayer/SpecularMatrix.h
@@ -28,8 +28,15 @@ class BA_CORE_API_ SpecularMatrix
 public:
     //! Computes refraction angles and transmission/reflection coefficients
     //! for given coherent wave propagation in a multilayer.
+    //! Roughness is modelled by tanh profile [see e.g. Phys. Rev. B, vol. 47 (8), p. 4385 (1993)].
     static void execute(const class MultiLayer& sample, const kvector_t k,
                         std::vector<ScalarRTCoefficients>& coeff);
+
+    //! Computes refraction angles and transmission/reflection coefficients
+    //! for given coherent wave propagation in a multilayer.
+    //! Roughness is modelled by tanh profile [see e.g. Phys. Rev. B, vol. 47 (8), p. 4385 (1993)].
+    static void execute_(const class MultiLayer& sample, kvector_t k,
+                         std::vector<ScalarRTCoefficients>& coeff);
 };
 
 #endif // SPECULARMATRIX_H

bbec4b429bb88ec312e1651741134978a2ac8d29
Dmitry Yurov <d.yurov@fz-juelich.de>
2019-02-17 18:57:47 +0100

    Remove erroneous attribute
    To suppress warning on win

--- Core/Instrument/SpecularDetector1D.h
+++ Core/Instrument/SpecularDetector1D.h
@@ -36,7 +36,7 @@ public:
 
 #ifndef SWIG
     //! Create a vector of DetectorElement objects according to the detector
-    [[noreturn]] std::vector<DetectorElement> createDetectorElements(const Beam& beam) override;
+    std::vector<DetectorElement> createDetectorElements(const Beam& beam) override;
 #endif // SWIG
 
     //! Returns region of interest if exists.

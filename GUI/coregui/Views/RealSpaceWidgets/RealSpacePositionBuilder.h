04c9a89e2f1a247e53c5d99ee926a57276654c40
Walter Van Herck <w.van.herck@fz-juelich.de>
2019-02-19 11:19:36 +0100

    Add files for class that generates positions from the knowledge of the interference function

---
+++ GUI/coregui/Views/RealSpaceWidgets/RealSpacePositionBuilder.h
@@ -0,0 +1,21 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      GUI/coregui/Views/RealSpaceWidgets/RealSpacePositionBuilder.h
+//! @brief     Defines class RealSpacePositionBuilder
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2018
+//! @authors   Scientific Computing Group at MLZ (see CITATION, AUTHORS)
+//
+// ************************************************************************** //
+
+#ifndef REALSPACEPOSITIONBUILDER_H
+#define REALSPACEPOSITIONBUILDER_H
+
+#include "WinDllMacros.h"
+
+
+#endif // REALSPACEPOSITIONBUILDER_H

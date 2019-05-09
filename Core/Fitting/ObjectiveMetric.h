4ed09c9f633381b0cf7938950943ed3550d8b52c
Dmitry Yurov <d.yurov@fz-juelich.de>
2019-05-09 16:58:56 +0200

    Implement ObjectiveMetric class hierarchy

---
+++ Core/Fitting/ObjectiveMetric.h
@@ -0,0 +1,179 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Core/Fitting/ObjectiveMetric.h
+//! @brief     Defines ObjectiveMetric classes.
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2018
+//! @authors   Scientific Computing Group at MLZ (see CITATION, AUTHORS)
+//
+// ************************************************************************** //
+
+#ifndef OBJECTIVEMETRIC_H
+#define OBJECTIVEMETRIC_H
+
+#include "ICloneable.h"
+#include <functional>
+#include <vector>
+
+class SimDataPair;
+
+//! Base class for metric implementations
+class BA_CORE_API_ ObjectiveMetric : public ICloneable
+{
+public:
+    //! L1 normalization function.
+    static const std::function<double(double)> l1_norm;
+    //! L2 normalization function.
+    static const std::function<double(double)> l2_norm;
+
+    ObjectiveMetric(std::function<double(double)> norm);
+
+    ObjectiveMetric* clone() const override = 0;
+
+    //! Computes metric value from SimDataPair object. Calls computeFromArrays internally.
+    //! @param data_pair: SimDataPair object. Can optionally contain data uncertainties
+    //! @param use_weights: boolean, defines if data uncertainties should be taken into account
+    virtual double compute(const SimDataPair& data_pair, bool use_weights) const;
+
+    //! Computes metric value from data arrays. Negative values in exp_data
+    //! are ignored as well as non-positive weight_factors and uncertainties.
+    //! All arrays involved in the computation must be of the same size.
+    //! @param sim_data: array with simulated intensities.
+    //! @param exp_data: array with intensity values obtained from an experiment.
+    //! @param uncertainties: array with experimental data uncertainties.
+    //! @param weight_factors: user-defined weighting factors. Used linearly, no matter which norm
+    //! is chosen.
+    virtual double computeFromArrays(std::vector<double> sim_data, std::vector<double> exp_data,
+                                     std::vector<double> uncertainties,
+                                     std::vector<double> weight_factors) const = 0;
+
+    //! Computes metric value from data arrays. Negative values in exp_data
+    //! are ignored as well as non-positive weight_factors.
+    //! All arrays involved in the computation must be of the same size.
+    //! @param sim_data: array with simulated intensities.
+    //! @param exp_data: array with intensity values obtained from an experiment.
+    //! @param weight_factors: user-defined weighting factors. Used linearly, no matter which norm
+    //! is chosen.
+    virtual double computeFromArrays(std::vector<double> sim_data, std::vector<double> exp_data,
+                                     std::vector<double> weight_factors) const = 0;
+
+    void setNorm(std::function<double(double)> norm);
+
+    //! Returns a copy of the normalization function used.
+    auto norm() const { return m_norm; }
+
+private:
+    std::function<double(double)> m_norm; //! normalization function.
+};
+
+//! Implementation of the standard \f$ \chi^2 \f$ metric. With default L2 norm corresponds to the
+//! formula
+//! \f[\chi^2 = \sum \frac{(I - D)^2}{\delta_D^2}\f]
+class BA_CORE_API_ Chi2Metric : public ObjectiveMetric
+{
+public:
+    Chi2Metric();
+    Chi2Metric* clone() const override;
+
+    //! Computes metric value from data arrays. Negative values in exp_data
+    //! are ignored as well as non-positive weight_factors and uncertainties.
+    //! All arrays involved in the computation must be of the same size.
+    //! @param sim_data: array with simulated intensities.
+    //! @param exp_data: array with intensity values obtained from an experiment.
+    //! @param uncertainties: array with experimental data uncertainties.
+    //! @param weight_factors: user-defined weighting factors. Used linearly, no matter which norm
+    //! is chosen.
+    double computeFromArrays(std::vector<double> sim_data, std::vector<double> exp_data,
+                             std::vector<double> uncertainties,
+                             std::vector<double> weight_factors) const override;
+
+    //! Computes metric value from data arrays. Negative values in exp_data
+    //! are ignored as well as non-positive weight_factors.
+    //! All arrays involved in the computation must be of the same size.
+    //! @param sim_data: array with simulated intensities.
+    //! @param exp_data: array with intensity values obtained from an experiment.
+    //! @param weight_factors: user-defined weighting factors. Used linearly, no matter which norm
+    //! is chosen.
+    double computeFromArrays(std::vector<double> sim_data, std::vector<double> exp_data,
+                             std::vector<double> weight_factors) const override;
+};
+
+//! Implementation of the standard \f$ \chi^2 \f$ metric with intensity \f$I\f$
+//! and experimental data \f$D\f$
+//! being replaced by \f$ \log_{10} I \f$ and \f$\log_{10} D\f$ accordingly.
+//! With default L2 norm corresponds to the formula
+//! \f[\chi^2 = \sum \frac{(\log_{10} I - log_{10} D)^2 D^2 \ln^2{10}}{\delta_D^2}\f]
+class BA_CORE_API_ LogMetric : public ObjectiveMetric
+{
+public:
+    LogMetric();
+    LogMetric* clone() const override;
+
+    //! Computes metric value from data arrays. Negative values in exp_data
+    //! are ignored as well as non-positive weight_factors and uncertainties.
+    //! All arrays involved in the computation must be of the same size.
+    //! @param sim_data: array with simulated intensities.
+    //! @param exp_data: array with intensity values obtained from an experiment.
+    //! @param uncertainties: array with experimental data uncertainties.
+    //! @param weight_factors: user-defined weighting factors. Used linearly, no matter which norm
+    //! is chosen.
+    double computeFromArrays(std::vector<double> sim_data, std::vector<double> exp_data,
+                             std::vector<double> uncertainties,
+                             std::vector<double> weight_factors) const override;
+
+    //! Computes metric value from data arrays. Negative values in exp_data
+    //! are ignored as well as non-positive weight_factors.
+    //! All arrays involved in the computation must be of the same size.
+    //! @param sim_data: array with simulated intensities.
+    //! @param exp_data: array with intensity values obtained from an experiment.
+    //! @param weight_factors: user-defined weighting factors. Used linearly, no matter which norm
+    //! is chosen.
+    double computeFromArrays(std::vector<double> sim_data, std::vector<double> exp_data,
+                             std::vector<double> weight_factors) const override;
+};
+
+//! Implementation of relative difference metric.
+//! With default L2 norm and weighting off corresponds to the formula
+//! \f[Result = \sum \frac{(I - D)^2}{I^2}\f]
+//! where \f$I\f$ is the simulated intensity. If weighting is on,
+//! coincides with the metric provided by Chi2Metric class.
+class BA_CORE_API_ RelativeDifferenceMetric : public Chi2Metric
+{
+public:
+    RelativeDifferenceMetric();
+    RelativeDifferenceMetric* clone() const override;
+
+    using Chi2Metric::computeFromArrays;
+
+    //! Computes metric value from data arrays. Negative values in exp_data
+    //! are ignored as well as non-positive weight_factors.
+    //! All arrays involved in the computation must be of the same size.
+    //! @param sim_data: array with simulated intensities.
+    //! @param exp_data: array with intensity values obtained from an experiment.
+    //! @param weight_factors: user-defined weighting factors. Used linearly, no matter which norm
+    //! is chosen.
+    double computeFromArrays(std::vector<double> sim_data, std::vector<double> exp_data,
+                             std::vector<double> weight_factors) const override;
+};
+
+//! Implementation of relative difference metric.
+//! With default L2 norm and weighting off corresponds to the formula
+//! \f[Result = \sum (I \cdot Q^4 - D \cdot Q^4)^2\f]
+//! where \f$Q\f$ is the scattering vector magnitude. If weighting is on,
+//! coincides with the metric provided by Chi2Metric class.
+class BA_CORE_API_ RQ4Metric : public Chi2Metric
+{
+public:
+    RQ4Metric();
+    RQ4Metric* clone() const override;
+    //! Computes metric value from SimDataPair object. Calls computeFromArrays internally.
+    //! @param data_pair: SimDataPair object. Can optionally contain data uncertainties
+    //! @param use_weights: boolean, defines if data uncertainties should be taken into account
+    double compute(const SimDataPair& data_pair, bool use_weights) const override;
+};
+
+#endif // OBJECTIVEMETRIC_H
---
+++ Tests/UnitTests/Core/Fitting/ObjectiveMetricTest.cpp
@@ -0,0 +1,263 @@
+#include "google_test.h"
+#include "ObjectiveMetric.h"
+#include <cmath>
+
+class ObjectiveMetricTest : public ::testing::Test
+{
+public:
+    ~ObjectiveMetricTest() override;
+};
+
+ObjectiveMetricTest::~ObjectiveMetricTest() = default;
+
+TEST_F(ObjectiveMetricTest, Chi2WellFormed)
+{
+    std::vector<double> sim_data {1.0, 2.0, 3.0, 4.0};
+    std::vector<double> exp_data {2.0, 1.0, 4.0, 3.0};
+    std::vector<double> uncertainties {0.1, 0.1, 0.5, 0.5};
+    std::vector<double> weight_factors {1.0, 1.0, 1.0, 2.0};
+
+    Chi2Metric metric;
+
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, sim_data, uncertainties, weight_factors),
+                     0.0);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, sim_data, weight_factors), 0.0);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, uncertainties, weight_factors),
+                     212.0);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, weight_factors), 5.0);
+
+    std::vector<double> exp_data_1 = exp_data;
+    exp_data_1[0] = -1.0;
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data_1, uncertainties, weight_factors),
+                     112.0);
+
+    std::vector<double> uncertainties_1 = uncertainties;
+    uncertainties_1[3] = -1.0;
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, uncertainties_1, weight_factors),
+                     204.0);
+    uncertainties_1[3] = 0.0;
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, uncertainties_1, weight_factors),
+                     204.0);
+
+    metric.setNorm(ObjectiveMetric::l1_norm);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, uncertainties, weight_factors),
+                     26.0);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, weight_factors), 5.0);
+
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays({}, {}, {}, {}), 0.0);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays({}, {}, {}), 0.0);
+}
+
+TEST_F(ObjectiveMetricTest, Chi2IllFormed)
+{
+    std::vector<double> sim_data {1.0, 2.0, 3.0, 4.0};
+    std::vector<double> exp_data {2.0, 1.0, 4.0, 3.0};
+    std::vector<double> uncertainties {0.1, 0.1, 0.5, 0.5};
+    std::vector<double> weight_factors {1.0, 1.0, 1.0, 2.0};
+
+    Chi2Metric metric;
+
+    EXPECT_THROW(metric.computeFromArrays({}, {}, {}, {1.0}), std::runtime_error);
+    EXPECT_THROW(metric.computeFromArrays({}, {}, {1.0}), std::runtime_error);
+    EXPECT_THROW(metric.computeFromArrays(sim_data, exp_data, {}, weight_factors),
+                 std::runtime_error);
+    EXPECT_THROW(metric.computeFromArrays({}, exp_data, weight_factors), std::runtime_error);
+
+    std::vector<double> sim_data_1 = sim_data;
+    sim_data_1[0] = -1.0;
+    EXPECT_THROW(metric.computeFromArrays(sim_data_1, exp_data, uncertainties, weight_factors),
+                 std::runtime_error);
+    EXPECT_THROW(metric.computeFromArrays(sim_data_1, exp_data, weight_factors),
+                 std::runtime_error);
+
+    std::vector<double> weight_factors_1(weight_factors.size(), 0.0);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, uncertainties, weight_factors_1),
+                     0.0);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, weight_factors_1), 0.0);
+
+    std::vector<double> uncertainties_1 = uncertainties;
+    uncertainties_1[0] = std::numeric_limits<double>::min();
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, uncertainties_1, weight_factors),
+                     std::numeric_limits<double>::max());
+}
+
+TEST_F(ObjectiveMetricTest, LogWellFormed)
+{
+    std::vector<double> sim_data {1.0, 10.0, 1.e+2, 1.e+4};
+    std::vector<double> exp_data {10.0, 1.0, 1.e+3, 1.e+5};
+    std::vector<double> uncertainties {0.1, 0.1, 0.5, 0.5};
+    std::vector<double> weight_factors {1.0, 1.0, 1.0, 2.0};
+
+    LogMetric metric;
+
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, sim_data, uncertainties, weight_factors),
+                     0.0);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, sim_data, weight_factors), 0.0);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, uncertainties, weight_factors),
+                     8.00040101e+10 * std::log(10.0) * std::log(10.0));
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, weight_factors), 5.0);
+
+    std::vector<double> exp_data_1 = exp_data;
+    exp_data_1[0] = -1.0;
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data_1, uncertainties, weight_factors),
+                     8.00040001e+10 * std::log(10.0) * std::log(10.0));
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data_1, weight_factors), 4.0);
+
+    std::vector<double> uncertainties_1 = uncertainties;
+    uncertainties_1[3] = -1.0;
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, uncertainties_1, weight_factors),
+                     4.0101e+6 * std::log(10.0) * std::log(10.0));
+    uncertainties_1[3] = 0.0;
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, uncertainties_1, weight_factors),
+                     4.0101e+6 * std::log(10.0) * std::log(10.0));
+
+    metric.setNorm(ObjectiveMetric::l1_norm);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, uncertainties, weight_factors),
+                     4.0211e+5 * std::log(10.0));
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, weight_factors), 5.0);
+
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays({}, {}, {}, {}), 0.0);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays({}, {}, {}), 0.0);
+}
+
+TEST_F(ObjectiveMetricTest, LogIllFormed)
+{
+    std::vector<double> sim_data {1.0, 10.0, 1.e+2, 1.e+4};
+    std::vector<double> exp_data {10.0, 1.0, 1.e+3, 1.e+5};
+    std::vector<double> uncertainties {0.1, 0.1, 0.5, 0.5};
+    std::vector<double> weight_factors {1.0, 1.0, 1.0, 2.0};
+
+    LogMetric metric;
+
+    EXPECT_THROW(metric.computeFromArrays({}, {}, {}, {1.0}), std::runtime_error);
+    EXPECT_THROW(metric.computeFromArrays({}, {}, {1.0}), std::runtime_error);
+
+    std::vector<double> sim_data_1 = sim_data;
+    sim_data_1[0] = -1.0;
+    EXPECT_THROW(metric.computeFromArrays(sim_data_1, exp_data, uncertainties, weight_factors),
+                 std::runtime_error);
+    EXPECT_THROW(metric.computeFromArrays(sim_data_1, exp_data, weight_factors),
+                 std::runtime_error);
+
+    std::vector<double> weight_factors_1(weight_factors.size(), 0.0);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, uncertainties, weight_factors_1),
+                     0.0);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, weight_factors_1), 0.0);
+
+    std::vector<double> uncertainties_1 = uncertainties;
+    uncertainties_1[0] = std::numeric_limits<double>::min();
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, uncertainties_1, weight_factors),
+                     std::numeric_limits<double>::max());
+}
+
+TEST_F(ObjectiveMetricTest, RelativeDifferenceWellFormed)
+{
+    std::vector<double> sim_data {1.0, 2.0, 4.0, 4.0};
+    std::vector<double> exp_data {2.0, 1.0, 2.0, 2.0};
+    std::vector<double> uncertainties {1.0, 1.0, 2.0, 1.0};
+    std::vector<double> weight_factors {1.0, 1.0, 2.0, 1.0};
+
+    RelativeDifferenceMetric metric;
+
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, sim_data, weight_factors), 0.0);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, weight_factors), 2.0);
+
+    std::vector<double> exp_data_1 = exp_data;
+    exp_data_1[0] = -1.0;
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data_1, weight_factors), 1.0);
+
+    std::vector<double> sim_data_1 = sim_data;
+    sim_data_1[0] = 0.0;
+    exp_data_1[0] = 0.0;
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data_1, exp_data_1, weight_factors), 1.0);
+
+    metric.setNorm(ObjectiveMetric::l1_norm);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, weight_factors), 3.0);
+
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, uncertainties, weight_factors),
+                     6.0);
+
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays({}, {}, {}, {}), 0.0);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays({}, {}, {}), 0.0);
+}
+
+TEST_F(ObjectiveMetricTest, RelativeDifferenceIllFormed)
+{
+    std::vector<double> sim_data {1.0, 2.0, 4.0, 4.0};
+    std::vector<double> exp_data {2.0, 1.0, 3.0, 2.0};
+    std::vector<double> uncertainties {1.0, 1.0, 2.0, 1.0};
+    std::vector<double> weight_factors {1.0, 1.0, 2.0, 1.0};
+
+    RelativeDifferenceMetric metric;
+
+    EXPECT_THROW(metric.computeFromArrays(sim_data, {}, uncertainties, weight_factors),
+                 std::runtime_error);
+    EXPECT_THROW(metric.computeFromArrays(sim_data, {}, weight_factors), std::runtime_error);
+
+    std::vector<double> sim_data_1 = sim_data;
+    sim_data_1[0] = -1.0;
+    EXPECT_THROW(metric.computeFromArrays(sim_data_1, exp_data, uncertainties, weight_factors),
+                 std::runtime_error);
+    EXPECT_THROW(metric.computeFromArrays(sim_data_1, exp_data, weight_factors),
+                 std::runtime_error);
+
+    std::vector<double> weight_factors_1(weight_factors.size(), 0.0);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, uncertainties, weight_factors_1),
+                     0.0);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, weight_factors_1), 0.0);
+}
+
+TEST_F(ObjectiveMetricTest, RQ4WellFormed)
+{
+    std::vector<double> sim_data {1.0, 2.0, 4.0, 4.0};
+    std::vector<double> exp_data {2.0, 1.0, 2.0, 2.0};
+    std::vector<double> uncertainties {1.0, 1.0, 2.0, 1.0};
+    std::vector<double> weight_factors {1.0, 1.0, 2.0, 1.0};
+
+    RQ4Metric metric;
+
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, sim_data, weight_factors), 0.0);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, weight_factors), 14.0);
+
+    std::vector<double> exp_data_1 = exp_data;
+    exp_data_1[0] = -1.0;
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data_1, weight_factors), 13.0);
+
+    std::vector<double> sim_data_1 = sim_data;
+    sim_data_1[0] = 0.0;
+    exp_data_1[0] = 0.0;
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data_1, exp_data_1, weight_factors), 13.0);
+
+    metric.setNorm(ObjectiveMetric::l1_norm);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, weight_factors), 8.0);
+
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, uncertainties, weight_factors),
+                     6.0);
+
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays({}, {}, {}, {}), 0.0);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays({}, {}, {}), 0.0);
+}
+
+TEST_F(ObjectiveMetricTest, RQ4IllFormed)
+{
+    std::vector<double> sim_data {1.0, 2.0, 4.0, 4.0};
+    std::vector<double> exp_data {2.0, 1.0, 3.0, 2.0};
+    std::vector<double> uncertainties {1.0, 1.0, 2.0, 1.0};
+    std::vector<double> weight_factors {1.0, 1.0, 2.0, 1.0};
+
+    RQ4Metric metric;
+
+    EXPECT_THROW(metric.computeFromArrays(sim_data, exp_data, {}, weight_factors),
+                 std::runtime_error);
+    EXPECT_THROW(metric.computeFromArrays({}, exp_data, weight_factors), std::runtime_error);
+
+    std::vector<double> sim_data_1 = sim_data;
+    sim_data_1[0] = -1.0;
+    EXPECT_THROW(metric.computeFromArrays(sim_data_1, exp_data, uncertainties, weight_factors),
+                 std::runtime_error);
+
+    std::vector<double> weight_factors_1(weight_factors.size(), 0.0);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, uncertainties, weight_factors_1),
+                     0.0);
+    EXPECT_DOUBLE_EQ(metric.computeFromArrays(sim_data, exp_data, weight_factors_1), 0.0);
+}

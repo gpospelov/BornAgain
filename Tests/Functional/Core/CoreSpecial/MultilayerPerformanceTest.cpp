f230e57d051ec2190529e7d5b8881e840cdd9e1d
Dmitry Yurov <d.yurov@fz-juelich.de>
2019-09-18 12:02:50 +0200

    Create a performance test for the sample with a large number of layers

---
+++ Tests/Functional/Core/CoreSpecial/MultilayerPerformanceTest.cpp
@@ -0,0 +1,60 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Tests/Functional/Core/CoreSpecial/MultilayerPerformanceTest.cpp
+//! @brief     Implements class MultilayerPerformanceTest
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2018
+//! @authors   Scientific Computing Group at MLZ (see CITATION, AUTHORS)
+//
+// ************************************************************************** //
+
+#include "MultilayerPerformanceTest.h"
+#include "MultiLayer.h"
+#include "PlainMultiLayerBySLDBuilder.h"
+#include "SpecularSimulation.h"
+#include "StandardSimulations.h"
+#include <chrono>
+
+using Results = std::vector<std::pair<int, long>>;
+
+namespace
+{
+const std::vector<int> n_layers = {10, 100, 200, 500, 1000};
+
+const auto now = std::chrono::high_resolution_clock::now;
+const auto duration = [](auto time_interval) {
+    return std::chrono::duration_cast<std::chrono::milliseconds>(time_interval).count();
+};
+
+void report(const Results& results)
+{
+    for (auto& pair : results)
+        std::cout << "n_layers = " << pair.first << ",\t time = " << pair.second << " ms\n";
+}
+} // namespace
+
+MultilayerPerformanceTest::~MultilayerPerformanceTest() = default;
+
+bool MultilayerPerformanceTest::runTest()
+{
+    Results results;
+    results.reserve(n_layers.size());
+
+    for (int layer_mult: n_layers) {
+        const auto start_time = now();
+        std::unique_ptr<MultiLayer> sample(PlainMultiLayerBySLDBuilder(layer_mult).buildSample());
+        std::unique_ptr<SpecularSimulation> simulation(StandardSimulations::BasicSpecular());
+        simulation->setSample(*sample);
+
+        simulation->runSimulation();
+
+        results.push_back({layer_mult, duration(now() - start_time)});
+    }
+
+    report(results);
+    return true;
+}

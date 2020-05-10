8bd50b2a258563073978219e94fc8f505e8f1958
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2020-05-10 20:58:37 +0200

    FFCatalog: plot images with 'inferno' color map, as in paper

--- Doc/FFCatalog/fig/ff2/bornplot.py
+++ Doc/FFCatalog/fig/ff2/bornplot.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from matplotlib import rc
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 rc('text', usetex=True)
+rc('image', cmap='inferno')
 
 import bornagain as ba
 from bornagain import nanometer, degree, angstrom, deg2rad

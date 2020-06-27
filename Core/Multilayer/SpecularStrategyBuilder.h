b99531dc9e8619ed9ae5b18135a4c4202854270a
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2020-06-27 18:46:08 +0200

    Core/Doxygen: correct file header comment blocks

--- Core/Multilayer/SpecularStrategyBuilder.h
+++ Core/Multilayer/SpecularStrategyBuilder.h
@@ -2,7 +2,7 @@
 //
 //  BornAgain: simulate and fit scattering at grazing incidence
 //
-//! @file      Core/Multilayer/SpecularStrategyBuilder.cpp
+//! @file      Core/Multilayer/SpecularStrategyBuilder.h
 //! @brief     Defines class SpecularStrategyBuilder.
 //!
 //! @homepage  http://www.bornagainproject.org
--- Core/Simulation/DepthProbeSimulation.cpp
+++ Core/Simulation/DepthProbeSimulation.cpp
@@ -3,7 +3,7 @@
 //  BornAgain: simulate and fit scattering at grazing incidence
 //
 //! @file      Core/Simulation/DepthProbeSimulation.cpp
-//! @brief     Implements class OffSpecSimulation.
+//! @brief     Implements class DepthProbeSimulation
 //!
 //! @homepage  http://www.bornagainproject.org
 //! @license   GNU General Public License v3 or higher (see COPYING)

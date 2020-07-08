a3ea8b42eefabd5ecde3c9821f3870bd6d85f546
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2020-07-08 13:30:08 +0200

    CMake: rm unused compiler version detection

--- cmake/modules/CheckCompiler.cmake
+++ cmake/modules/CheckCompiler.cmake
@@ -2,43 +2,6 @@
 #  CheckCompiler.cmake
 #---------------------------------------------------------------------------------------------------
 
-#----Test if clang setup works----------------------------------------------------------------------
-if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-    execute_process(COMMAND ${CMAKE_C_COMPILER} -v
-        OUTPUT_VARIABLE _clang_version_info
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
-    string(REGEX REPLACE "^.*[ ]([0-9]+)\\.[0-9].*$" "\\1" CLANG_MAJOR "${_clang_version_info}")
-    string(REGEX REPLACE "^.*[ ][0-9]+\\.([0-9]).*$" "\\1" CLANG_MINOR "${_clang_version_info}")
-else()
-    set(CLANG_MAJOR 0)
-    set(CLANG_MINOR 0)
-endif()
-
-#---Obtain the major and minor version of the GNU compiler------------------------------------------
-if (CMAKE_COMPILER_IS_GNUCXX)
-    execute_process(COMMAND ${CMAKE_C_COMPILER} -dumpversion
-        OUTPUT_VARIABLE _gcc_version_info
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
-    string(REGEX REPLACE "^([0-9]+).*$"                   "\\1" GCC_MAJOR ${_gcc_version_info})
-    string(REGEX REPLACE "^[0-9]+\\.([0-9]+).*$"          "\\1" GCC_MINOR ${_gcc_version_info})
-    string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+).*$" "\\1" GCC_PATCH ${_gcc_version_info})
-
-    if(GCC_PATCH MATCHES "\\.+")
-        set(GCC_PATCH "")
-    endif()
-    if(GCC_MINOR MATCHES "\\.+")
-        set(GCC_MINOR "")
-    endif()
-    if(GCC_MAJOR MATCHES "\\.+")
-        set(GCC_MAJOR "")
-    endif()
-    message(STATUS "Found GCC. Major version ${GCC_MAJOR}, minor version ${GCC_MINOR}")
-    set(COMPILER_VERSION gcc${GCC_MAJOR}${GCC_MINOR}${GCC_PATCH})
-else()
-    set(GCC_MAJOR 0)
-    set(GCC_MINOR 0)
-endif()
-
 #---Set a default build type for single-configuration CMake generators if no build type is set------
 if(NOT CMAKE_BUILD_TYPE)
     #  set(CMAKE_BUILD_TYPE RelWithDebInfo CACHE STRING "" FORCE)
@@ -58,14 +21,6 @@ elseif(WIN32)
     include(SetUpWindows)
 endif()
 
-#---Setup details depending on the compiler type----------------------------------------------------
-if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang" OR
-   "${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-    set(CMAKE_CXX_STANDARD 14)
-else()
-    message(FATAL_ERROR "No known c++ compiler found")
-endif()
-
 #---Print the final compiler flags------------------------------------------------------------------
 message(STATUS "BornAgain Platform: ${BORNAGAIN_PLATFORM}")
 message(STATUS "BornAgain Architecture: ${BORNAGAIN_ARCHITECTURE}")
@@ -83,4 +38,3 @@ endif()
 message(STATUS "Compiler Flags: ${all_cxx_flags}")
 message(STATUS "Shared linker Flags: ${CMAKE_SHARED_LINKER_FLAGS}")
 message(STATUS "Exe linker Flags: ${CMAKE_EXE_LINKER_FLAGS}")
-

2259d927a6e8444dd28e3c80016f7fdc04ab00e2
Joachim Wuttke (l) <j.wuttke@fz-juelich.de>
2020-07-11 12:00:53 +0200

    Unit now w/o INamed

--- Core/Parametrization/Unit.h
+++ Core/Parametrization/Unit.h
@@ -15,16 +15,18 @@
 #ifndef BORNAGAIN_CORE_PARAMETRIZATION_UNIT_H
 #define BORNAGAIN_CORE_PARAMETRIZATION_UNIT_H
 
-#include "Core/Basics/INamed.h"
 #include <string>
 
 //! A physical unit.
 
-class Unit : public INamed
+class Unit
 {
 public:
-    explicit Unit(const std::string& name = "") { setUnit(name); }
-    void setUnit(const std::string& name);
+    explicit Unit(const std::string& name = "") : m_name(name) {}
+    void setUnit(const std::string& name) { m_name = name; }
+    const std::string& getName() const { return m_name; }
+private:
+    std::string m_name;
 };
 
 #endif // BORNAGAIN_CORE_PARAMETRIZATION_UNIT_H

2ae3f2e36b90a7843e45633ae627e2c34600b84b
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2020-07-19 08:24:07 +0200

    BROKEN: automatized step towards gtest support
    Tests/Functional/Core/Fitting/FitObjectiveTestCases.cpp:
        t = re.sub(r'(\w+?)::\w+?\(\)\s+: \w*?Test(\("[^"]*?"(,\s+"[^"]*?"){2,3}\))\s*?{\s*?}',
                   r'TEST_F(CoreFitting, \1)\n{\n    EXPECT_TRUE(run\2);\n}',
                   t)

--- Tests/Functional/Core/Fitting/FitObjectiveTestCases.cpp
+++ Tests/Functional/Core/Fitting/FitObjectiveTestCases.cpp
@@ -14,61 +14,73 @@
 
 #include "Tests/Functional/Core/Fitting/FitObjectiveTestCases.h"
 
-MigradCylindersInBA::MigradCylindersInBA()
-    : FitObjectiveTest("Minuit2", "Migrad", "CylindersInBAPlan")
+TEST_F(CoreFitting, MigradCylindersInBA)
 {
+    EXPECT_TRUE(run("Minuit2", "Migrad", "CylindersInBAPlan"));
 }
 
-MigradResidualCylindersInBA::MigradResidualCylindersInBA()
-    : FitObjectiveTest("Minuit2", "Migrad", "CylindersInBAResidualPlan")
+TEST_F(CoreFitting, MigradResidualCylindersInBA)
 {
+    EXPECT_TRUE(run("Minuit2", "Migrad", "CylindersInBAResidualPlan"));
 }
 
-BfgsCylindersInBA::BfgsCylindersInBA()
-    : FitObjectiveTest("GSLMultiMin", "BFGS2", "CylindersInBAEasyPlan")
+TEST_F(CoreFitting, BfgsCylindersInBA)
 {
+    EXPECT_TRUE(run("GSLMultiMin", "BFGS2", "CylindersInBAEasyPlan"));
 }
 
-SteepestDescentCylindersInBA::SteepestDescentCylindersInBA()
-    : FitObjectiveTest("GSLMultiMin", "SteepestDescent", "CylindersInBAEasyPlan")
+TEST_F(CoreFitting, SteepestDescentCylindersInBA)
 {
+    EXPECT_TRUE(run("GSLMultiMin", "SteepestDescent", "CylindersInBAEasyPlan"));
 }
 
-FumuliCylindersInBA::FumuliCylindersInBA()
-    : FitObjectiveTest("Minuit2", "Fumili", "CylindersInBAResidualPlan")
+TEST_F(CoreFitting, FumuliCylindersInBA)
 {
+    EXPECT_TRUE(run("Minuit2", "Fumili", "CylindersInBAResidualPlan"));
 }
 
-LevenbergMarquardtCylindersInBA::LevenbergMarquardtCylindersInBA()
-    : FitObjectiveTest("GSLLMA", "", "CylindersInBAResidualPlan")
+TEST_F(CoreFitting, LevenbergMarquardtCylindersInBA)
 {
+    EXPECT_TRUE(run("GSLLMA", "", "CylindersInBAResidualPlan"));
 }
 
-SimAnCylindersInBA::SimAnCylindersInBA()
-    : FitObjectiveTest("GSLSimAn", "", "CylindersInBAEasyPlan",
-                       "IterationsAtTemp=5;MaxIterations=10;t_min=1.0")
+TEST_F(CoreFitting, SimAnCylindersInBA)
 {
+    EXPECT_TRUE(run("GSLSimAn", "", "CylindersInBAEasyPlan",
+                       "IterationsAtTemp=5;MaxIterations=10;t_min=1.0"));
 }
 
-GeneticCylindersInBA::GeneticCylindersInBA()
-    : FitObjectiveTest("Genetic", "", "CylindersInBAEasyPlan", "MaxIterations=1;RandomSeed=1")
+TEST_F(CoreFitting, GeneticCylindersInBA)
 {
+    EXPECT_TRUE(run("Genetic", "", "CylindersInBAEasyPlan", "MaxIterations=1;RandomSeed=1"));
 }
 
-RectDetectorFit::RectDetectorFit()
-    : FitObjectiveTest("Minuit2", "Migrad", "RectDetPlan", "Strategy=2")
+TEST_F(CoreFitting, RectDetectorFit)
 {
+    EXPECT_TRUE(run("Minuit2", "Migrad", "RectDetPlan", "Strategy=2"));
 }
 
-AdjustMinimizerFit::AdjustMinimizerFit() : FitObjectiveTest("Genetic", "", "AdjustMinimizerPlan") {}
-
-SpecularFitTest::SpecularFitTest() : FitObjectiveTest("Minuit2", "Migrad", "SpecularPlan") {}
-
-SpecularFitTestQ::SpecularFitTestQ() : FitObjectiveTest("Minuit2", "Migrad", "SpecularPlanQ") {}
-
-MultipleSpecFittingTest::MultipleSpecFittingTest()
-    : FitObjectiveTest("Minuit2", "Migrad", "MultipleSpecPlan")
+TEST_F(CoreFitting, AdjustMinimizerFit)
 {
+    EXPECT_TRUE(run("Genetic", "", "AdjustMinimizerPlan"));
 }
 
-OffSpecFitTest::OffSpecFitTest() : FitObjectiveTest("Minuit2", "Migrad", "OffSpecFitPlan") {}
+TEST_F(CoreFitting, SpecularFitTest)
+{
+    EXPECT_TRUE(run("Minuit2", "Migrad", "SpecularPlan"));
+}
+
+TEST_F(CoreFitting, SpecularFitTestQ)
+{
+    EXPECT_TRUE(run("Minuit2", "Migrad", "SpecularPlanQ"));
+}
+
+TEST_F(CoreFitting, MultipleSpecFittingTest)
+{
+    EXPECT_TRUE(run("Minuit2", "Migrad", "MultipleSpecPlan"));
+}
+
+TEST_F(CoreFitting, OffSpecFitTest)
+{
+    EXPECT_TRUE(run("Minuit2", "Migrad", "OffSpecFitPlan"));
+}

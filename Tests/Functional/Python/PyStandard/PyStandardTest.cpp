60b678af3e0702afd03443e5805960631e405bca
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2020-08-02 08:08:56 +0200

    PyStandardTest: merge in one fct

--- Tests/Functional/Python/PyStandard/PyStandardTest.cpp
+++ Tests/Functional/Python/PyStandard/PyStandardTest.cpp
@@ -27,27 +27,6 @@
 namespace
 {
 
-//! Runs a python command, prints messages, returns true unless the system call failed.
-void runPython(const std::string& py_command)
-{
-#ifndef _WIN32
-    std::string sys_command = std::string("PYTHONPATH=") + BABuild::buildLibDir() + " "
-                              + std::string("NOPLOT=TRUE") + " " + BABuild::pythonExecutable()
-                              + " -B " + py_command;
-#else
-    std::string sys_command = std::string("set PYTHONPATH=") + BABuild::buildLibDir() + " & "
-                              + std::string("set NOPLOT=TRUE") + " & \""
-                              + BABuild::pythonExecutable() + "\" -B " + py_command;
-#endif
-    std::cout << sys_command << std::endl /*sic*/; // flush output before calling std::system
-    int ret = std::system(sys_command.c_str());
-    if (ret != 0) {
-        std::stringstream msg;
-        msg << "Command returned non-zero value " << ret;
-        throw std::runtime_error(msg.str());
-    }
-}
-
 } // namespace
 
 //! Runs simulation via a Python script and directly, and returns true if the results agree.
@@ -55,23 +34,40 @@ bool PyStandardTest::runTest()
 {
     // Set output data filename, and remove old output files
     assert(m_name != "");
-    std::string output_name = FileSystemUtils::jointPath(BATesting::PyStandardOutputDir(), m_name);
-    std::string output_path = output_name + ".ref.int.gz";
+    const std::string output_name =
+        FileSystemUtils::jointPath(BATesting::PyStandardOutputDir(), m_name);
+    const std::string output_path = output_name + ".ref.int.gz";
     std::remove(output_path.c_str());
-    std::cout << "Removed old output " << output_path << "\n";
+    std::cout << "- removed old output " << output_path << "\n";
 
     // Generate Python script
-    std::string pyscript_filename =
+    const std::string pyscript_filename =
         FileSystemUtils::jointPath(BATesting::PyStandardOutputDir(), m_name + ".py");
     std::ofstream pythonFile(pyscript_filename);
     pythonFile << ExportToPython::generatePyExportTest(*m_reference_simulation);
     pythonFile.close();
 
     // Run Python script
-    runPython(pyscript_filename + " " + output_path);
+    const std::string py_command = pyscript_filename + " " + output_path;
+#ifndef _WIN32
+    const std::string sys_command = std::string("PYTHONPATH=") + BABuild::buildLibDir() + " "
+                                    + std::string("NOPLOT=TRUE") + " " + BABuild::pythonExecutable()
+                                    + " -B " + py_command;
+#else
+    const std::string sys_command = std::string("set PYTHONPATH=") + BABuild::buildLibDir() + " & "
+                                    + std::string("set NOPLOT=TRUE") + " & \""
+                                    + BABuild::pythonExecutable() + "\" -B " + py_command;
+#endif
+    std::cout << "- system call: " << sys_command << std::endl; // note: endl = \n + flush
+    int ret = std::system(sys_command.c_str());
+    if (ret != 0) {
+        std::stringstream msg;
+        msg << "System call returned non-zero value " << ret;
+        throw std::runtime_error(msg.str());
+    }
 
     // Run direct simulation
-    std::cout << "Running simulation and comparing with result from Py script\n";
+    std::cout << "- run reference simulation\n";
     m_reference_simulation->runSimulation();
     const std::unique_ptr<OutputData<double>> reference_data =
         m_reference_simulation->result().data();

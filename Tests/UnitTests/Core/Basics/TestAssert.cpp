48829c6dd1aaabfe2fea96c5f8a5911077a58183
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2020-08-05 22:49:40 +0200

    no clue how to test failed assertion under Windows

--- Tests/UnitTests/Core/Basics/TestAssert.cpp
+++ Tests/UnitTests/Core/Basics/TestAssert.cpp
@@ -8,9 +8,11 @@ class TestAssert : public ::testing::Test
 TEST_F(TestAssert, Assert)
 {
     EXPECT_NO_THROW(ASSERT(1));
+#ifndef _WIN32 // no clue about argument 2 of EXPECT_EXIT under Windows
 #ifdef QT_MESSAGELOGCONTEXT
     EXPECT_EXIT(ASSERT(0), ::testing::KilledBySignal(6), "Assertion failed");
 #else
     EXPECT_EXIT(ASSERT(0), ::testing::KilledBySignal(6), "Assertion .* failed");
+#endif
 #endif
 }

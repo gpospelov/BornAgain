4c479e8969fb1cdce3c33948b569c843c9f22e0c
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2020-08-06 14:42:20 +0200

    replace Qt flag by HAVE_QT flag of our own

--- Core/Basics/Assert.h
+++ Core/Basics/Assert.h
@@ -18,18 +18,18 @@
 // ASSERT must be declared as a macro, not a function, in order for the error
 // message to correctly report the source line where the assertion failed.
 
-#ifdef QT_MESSAGELOGCONTEXT
+#ifdef HAVE_QT
 
 #include <QtGlobal>
 #define ASSERT(condition)                                                                          \
     if (!(condition))                                                                              \
     qFatal("Assertion %s failed in %s, line %d", (#condition), __FILE__,  __LINE__)
 
-#else // QT_MESSAGELOGCONTEXT undefined
+#else // HAVE_QT undefined
 
 #include <cassert>
 #define ASSERT(condition) assert(condition)
 
-#endif // QT_MESSAGELOGCONTEXT
+#endif // HAVE_QT
 
 #endif // BORNAGAIN_CORE_BASICS_ASSERT_H

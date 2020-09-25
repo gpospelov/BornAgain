bc748fb8b196d85d0112a350ded2af7971255722
Joachim Wuttke (l) <j.wuttke@fz-juelich.de>
2020-09-25 14:11:32 +0200

    Core StandardSamples: rm includes

--- Core/StandardSamples/SlicedCompositionBuilder.cpp
+++ Core/StandardSamples/SlicedCompositionBuilder.cpp
@@ -15,14 +15,12 @@
 #include "Core/StandardSamples/SlicedCompositionBuilder.h"
 #include "Core/Aggregate/ParticleLayout.h"
 #include "Core/Basics/Units.h"
-#include "Core/HardParticle/FormFactorFullSphere.h"
 #include "Core/HardParticle/FormFactorTruncatedSphere.h"
 #include "Core/Material/MaterialFactoryFuncs.h"
 #include "Core/Multilayer/Layer.h"
 #include "Core/Multilayer/MultiLayer.h"
 #include "Core/Particle/Particle.h"
 #include "Core/Particle/ParticleComposition.h"
-#include "Core/Vector/Transform3D.h"
 
 MultiLayer* SlicedCompositionBuilder::buildSample() const
 {

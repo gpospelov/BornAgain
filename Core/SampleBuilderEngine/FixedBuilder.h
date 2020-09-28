cebe84b6af151f891ffb8fa7669749c6bc39509c
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2020-09-28 14:22:49 +0200

    Core add SampleBuilderEngine/FixedBuilder

---
+++ Core/SampleBuilderEngine/FixedBuilder.h
@@ -0,0 +1,35 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      Core/SampleBuilderEngine/FixedBuilder.h
+//! @brief     Defines class FixedBuilder.
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2020
+//! @authors   Scientific Computing Group at MLZ (see CITATION, AUTHORS)
+//
+// ************************************************************************** //
+
+#ifndef BORNAGAIN_CORE_SAMPLEBUILDERENGINE_FIXEDBUILDER_H
+#define BORNAGAIN_CORE_SAMPLEBUILDERENGINE_FIXEDBUILDER_H
+
+#include "Core/SampleBuilderEngine/ISampleBuilder.h"
+#include <memory>
+
+class MultiLayer;
+
+//! A trivial sample builder class that builds a fixed sample.
+
+class FixedBuilder : public ISampleBuilder
+{
+public:
+    FixedBuilder() = delete;
+    FixedBuilder(const MultiLayer&);
+    MultiLayer* buildSample() const;
+private:
+    std::unique_ptr<const MultiLayer> m_sample;
+};
+
+#endif // BORNAGAIN_CORE_SAMPLEBUILDERENGINE_FIXEDBUILDER_H

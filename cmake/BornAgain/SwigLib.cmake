f89e71964ec15ea90d83684a9aab55dd2f902ebb
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2020-10-05 23:17:08 +0200

    CMake: move code to SwigLib.cmake

--- cmake/BornAgain/SwigLib.cmake
+++ cmake/BornAgain/SwigLib.cmake
@@ -1,8 +1,49 @@
-function(SwigLib name lib)
+function(SwigLib name lib TMP_DIR)
 
     if(NOT BORNAGAIN_PYTHON)
-        return()
+        message(FATAL_ERROR "Function SwigLib called though BORNAGAIN_PYTHON=false")
     endif()
+
+    if(CONFIGURE_BINDINGS)
+
+        file(MAKE_DIRECTORY ${TMP_DIR})
+
+        GeneratePythonDocs(${AUTO_DIR}/doxygen${name}.i ${WRAP_DIR}/swig)
+
+        set(swig_dependencies
+            ${WRAP_DIR}/swig/lib${lib}.i
+            ${WRAP_DIR}/swig/directors.i
+            ${WRAP_DIR}/swig/extend${name}.i
+            ${WRAP_DIR}/swig/ignores.i
+            ${WRAP_DIR}/swig/shared_pointers.i
+            ${WRAP_DIR}/swig/warnings.i
+            )
+        foreach(FNAM ${swig_dependencies})
+            if(NOT EXISTS ${FNAM})
+                message(FATAL_ERROR "Could NOT find SWIG input ${FNAM}")
+            endif()
+        endforeach()
+        list(APPEND swig_dependencies ${AUTO_DIR}/doxygen${name}.i)
+
+        set(SWIG_FLAGS "-c++;-python;-o;${AUTO_DIR}/lib${lib}_wrap.cpp;-outdir;${TMP_DIR}"
+                               ";-I${CMAKE_SOURCE_DIR};-I${CMAKE_BINARY_DIR}/inc")
+
+        add_custom_command(
+            OUTPUT ${AUTO_DIR}/lib${lib}.py
+            COMMAND ${Python3_EXECUTABLE} ${WRAP_DIR}/swig/tweaks.py
+                   ${TMP_DIR}/lib${lib}.py
+                   ${AUTO_DIR}/lib${lib}.py
+            DEPENDS ${TMP_DIR}/lib${lib}.py
+            )
+        add_custom_command(
+            OUTPUT ${TMP_DIR}/lib${lib}.py
+                   ${AUTO_DIR}/lib${lib}_wrap.h
+                   ${AUTO_DIR}/lib${lib}_wrap.cpp
+            COMMAND ${SWIG_EXECUTABLE} ${SWIG_FLAGS} ${WRAP_DIR}/swig/lib${lib}.i
+            DEPENDS ${swig_dependencies} ${include_files}
+            )
+
+    endif(CONFIGURE_BINDINGS)
 
 
 endfunction()

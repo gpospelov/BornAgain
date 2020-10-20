ed31a7644b7916d213d0635eeaed84a308526fd5
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2020-10-20 15:48:01 +0200

    CMake Win: try w/o libcmtd

--- cmake/BornAgain/Windows.cmake
+++ cmake/BornAgain/Windows.cmake
@@ -12,4 +12,4 @@ add_definitions(-DEIGEN_DONT_ALIGN_STATICALLY=1)
 message(STATUS "Disabling static align for Eigen")
 
 string(APPEND CMAKE_CXX_FLAGS " /MP /D_USE_MATH_DEFINES /wd4005 /wd4244 -I${CMAKE_SOURCE_DIR}/Wrap -FIw32pragma.h")
-string(APPEND CMAKE_SHARED_LINKER_FLAGS " /NODEFAULTLIB:libcmtd.lib")
+# string(APPEND CMAKE_SHARED_LINKER_FLAGS " /NODEFAULTLIB:libcmtd.lib")

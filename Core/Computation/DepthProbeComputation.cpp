2a14fcc7040e0f0f387d410837d9760e611dd3a7
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2020-10-29 14:44:54 +0100

    corr doxy file comment

--- Core/Computation/DepthProbeComputation.cpp
+++ Core/Computation/DepthProbeComputation.cpp
@@ -3,7 +3,7 @@
 //  BornAgain: simulate and fit scattering at grazing incidence
 //
 //! @file      Core/Computation/DepthProbeComputation.cpp
-//! @brief     Implements class SpecularComputation.
+//! @brief     Implements class DepthProbeComputation.
 //!
 //! @homepage  http://www.bornagainproject.org
 //! @license   GNU General Public License v3 or higher (see COPYING)

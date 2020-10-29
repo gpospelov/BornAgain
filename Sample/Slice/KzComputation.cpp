5954d7bba7621c4916ba7fa842f8a3a7e290bb30
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2020-10-29 23:01:06 +0100

    uniform variable name 'result'

--- Sample/Slice/KzComputation.cpp
+++ Sample/Slice/KzComputation.cpp
@@ -45,24 +45,24 @@ std::vector<complex_t> KzComputation::computeReducedKz(const std::vector<Slice>&
     const double n_ref = slices[0].material().refractiveIndex(2 * M_PI / k.mag()).real();
     const double k_base = k.mag() * (k.z() > 0.0 ? -1 : 1);
 
-    std::vector<complex_t> kz(N);
+    std::vector<complex_t> result(N);
     // Calculate refraction angle, expressed as k_z, for each layer.
     complex_t rad = slices[0].scalarReducedPotential(k, n_ref);
-    kz[0] = k_base * std::sqrt(rad);
+    result[0] = k_base * std::sqrt(rad);
     for (size_t i = 1; i < N; ++i) {
         rad = checkForUnderflow(slices[i].scalarReducedPotential(k, n_ref));
-        kz[i] = k_base * std::sqrt(rad);
+        result[i] = k_base * std::sqrt(rad);
     }
-    return kz;
+    return result;
 }
 
 std::vector<complex_t> KzComputation::computeKzFromSLDs(const std::vector<Slice>& slices, double kz)
 {
     const size_t N = slices.size();
     const double k_sign = kz > 0.0 ? -1 : 1;
-    std::vector<complex_t> result(N);
-
     complex_t kz2_base = kz * kz + normalizedSLD(slices[0].material());
+
+    std::vector<complex_t> result(N);
     result[0] = -kz;
     // Calculate refraction angle, expressed as k_z, for each layer.
     for (size_t i = 1; i < N; ++i) {
@@ -76,18 +76,18 @@ std::vector<complex_t> KzComputation::computeKzFromRefIndices(const std::vector<
                                                               kvector_t k)
 {
     const size_t N = slices.size();
-    const double kz_val = k.z();
-    const double k_sign = kz_val > 0.0 ? -1 : 1;
+    const double kz = k.z();
+    const double k_sign = kz > 0.0 ? -1 : 1;
     const double k2 = k.mag2();
-    const double kz2 = kz_val * kz_val;
+    const double kz2 = kz * kz;
     const double wl = 2 * M_PI / std::sqrt(k2);
     const complex_t n2_ref = slices[0].material().refractiveIndex2(wl);
 
-    std::vector<complex_t> kz(N);
-    kz[0] = -kz_val;
+    std::vector<complex_t> result(N);
+    result[0] = -kz;
     for (size_t i = 1; i < N; ++i) {
         const complex_t n2_norm = slices[i].material().refractiveIndex2(wl) - n2_ref;
-        kz[i] = k_sign * std::sqrt(checkForUnderflow(k2 * n2_norm + kz2));
+        result[i] = k_sign * std::sqrt(checkForUnderflow(k2 * n2_norm + kz2));
     }
-    return kz;
+    return result;
 }

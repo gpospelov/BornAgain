1b2e3f5927d3449df3c0ac6c70a311dad0ca6291
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2020-11-19 20:54:51 +0100

    ctd

--- cmake/BornAgain/LineLength.cmake
+++ cmake/BornAgain/LineLength.cmake
@@ -9,18 +9,18 @@ if(NOT MSVC)
     foreach(dir ${AllComponents})
         file(GLOB_RECURSE src1 ${dir}/*.cpp)
         file(GLOB_RECURSE src2 ${dir}/*.h)
-        add_test(NAME "LineLength.Cpp.${dir}"
+        add_test(NAME LineLength.Cpp.${dir}
             COMMAND ${LINECOUNT} ${SRC_LEN_LIM} ${src1} ${src2})
     endforeach()
 
     foreach(dir ${AllComponents})
         file(GLOB_RECURSE src1 ${dir}/*CMakeLists.txt)
-        add_test(NAME "LineLength.CMake.${dir}"
+        add_test(NAME LineLength.CMake.${dir}
             COMMAND ${LINECOUNT} ${SRC_LEN_LIM} ${src1})
     endforeach()
 
     file(GLOB_RECURSE sources ${PY_EXAMPLES_DIR}/*.py)
-    add_test(NAME "LineLength.PyExamples"
+    add_test(NAME LineLength.PyExamples
         COMMAND ${LINECOUNT} ${WEB_LEN_LIM} ${sources})
 
 endif()

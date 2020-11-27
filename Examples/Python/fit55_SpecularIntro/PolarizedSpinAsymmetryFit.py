07fa5b17dd96eaaaa919f808851917483b5b062f
Randolf Beerwerth <r.beerwerth@fz-juelich.de>
2020-11-27 14:03:42 +0100

    yapf examples

--- Examples/Python/fit55_SpecularIntro/PolarizedSpinAsymmetryFit.py
+++ Examples/Python/fit55_SpecularIntro/PolarizedSpinAsymmetryFit.py
@@ -31,14 +31,10 @@ qmax = 1.96
 scan_size = 1500
 
 # The SLD of the substrate is kept constant
-sldMao  = (5.377e-06, 0)
+sldMao = (5.377e-06, 0)
 
 # constant to convert between magnetization and magnetic SLD
 RhoMconst = 2.910429812376859e-12
-
-
-
-
 
 ####################################################################
 #                  Create Sample and Simulation                    #
@@ -49,33 +45,32 @@ def get_sample(params):
     """
     construct the sample with the given parameters
     """
-    magnetizationMagnitude = params["rhoM_Mafo"] * 1e-6 / RhoMconst
-    angle                  = 0
-    magnetizationVector    = ba.kvector_t(
-                  magnetizationMagnitude * numpy.sin(angle * deg), 
-                  magnetizationMagnitude * numpy.cos(angle * deg), 
-                  0)
-    
-    mat_vacuum    = ba.MaterialBySLD("Vacuum", 0.0, 0.0)
-    mat_layer     = ba.MaterialBySLD("(Mg,Al,Fe)3O4", 
-                params["rho_Mafo"] * 1e-6, 0, magnetizationVector)
+    magnetizationMagnitude = params["rhoM_Mafo"]*1e-6/RhoMconst
+    angle = 0
+    magnetizationVector = ba.kvector_t(magnetizationMagnitude*numpy.sin(angle*deg),
+                                       magnetizationMagnitude*numpy.cos(angle*deg),
+                                       0)
+
+    mat_vacuum = ba.MaterialBySLD("Vacuum", 0.0, 0.0)
+    mat_layer = ba.MaterialBySLD("(Mg,Al,Fe)3O4", params["rho_Mafo"]*1e-6, 0,
+                                 magnetizationVector)
     mat_substrate = ba.MaterialBySLD("MgAl2O4", *sldMao)
 
     ambient_layer = ba.Layer(mat_vacuum)
-    layer = ba.Layer(mat_layer, params["t_Mafo"] * angstrom)
+    layer = ba.Layer(mat_layer, params["t_Mafo"]*angstrom)
     substrate_layer = ba.Layer(mat_substrate)
 
     r_Mafo = ba.LayerRoughness()
-    r_Mafo.setSigma(params["r_Mafo"] * angstrom)
-    
+    r_Mafo.setSigma(params["r_Mafo"]*angstrom)
+
     r_substrate = ba.LayerRoughness()
-    r_substrate.setSigma(params["r_Mao"] * angstrom)
-    
+    r_substrate.setSigma(params["r_Mao"]*angstrom)
+
     multi_layer = ba.MultiLayer()
     multi_layer.addLayer(ambient_layer)
     multi_layer.addLayerWithTopRoughness(layer, r_Mafo)
     multi_layer.addLayerWithTopRoughness(substrate_layer, r_substrate)
-        
+
     return multi_layer
 
 
@@ -88,8 +83,8 @@ def get_simulation(q_axis, parameters, polarization, analyzer):
     simulation = ba.SpecularSimulation()
     q_axis = q_axis + parameters["q_offset"]
     scan = ba.QSpecScan(q_axis)
-    
-    dq = parameters["q_res"] * q_axis
+
+    dq = parameters["q_res"]*q_axis
     n_sig = 4.0
     n_samples = 25
 
@@ -98,7 +93,7 @@ def get_simulation(q_axis, parameters, polarization, analyzer):
 
     simulation.setBeamPolarization(polarization)
     simulation.setAnalyzerProperties(analyzer, 1.0, 0.5)
-    
+
     simulation.setScan(scan)
     return simulation
 
@@ -110,10 +105,9 @@ def run_simulation(q_axis, fitParams, *, polarization, analyzer):
     Vectors for polarization and analyzer need to be provided
     """
     parameters = dict(fitParams, **fixedParams)
-    
+
     sample = get_sample(parameters)
-    simulation = get_simulation(q_axis, parameters, 
-                                polarization, analyzer)
+    simulation = get_simulation(q_axis, parameters, polarization, analyzer)
 
     simulation.setSample(sample)
     simulation.runSimulation()
@@ -125,9 +119,9 @@ def qr(result):
     Returns two arrays that hold the q-values as well as the
     reflectivity from a given simulation result
     """
-    q = numpy.array( result.result().axis(ba.Axes.QSPACE) )
-    r = numpy.array( result.result().array(ba.Axes.QSPACE) )
-    
+    q = numpy.array(result.result().axis(ba.Axes.QSPACE))
+    r = numpy.array(result.result().array(ba.Axes.QSPACE))
+
     return q, r
 
 
@@ -136,27 +130,31 @@ def qr(result):
 ####################################################################
 
 
-
 def plot(qs, rs, exps, labels, filename):
     """
     Plot the simulated result together with the experimental data
     """
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    
+
     for q, r, exp, l in zip(qs, rs, exps, labels):
-    
-      ax.errorbar( exp[0], exp[1], xerr=exp[3], yerr=exp[2], 
-                   fmt='.', markersize=0.75, linewidth=0.5)
-      
-      ax.plot(q, r, label=l)
-    
+
+        ax.errorbar(exp[0],
+                    exp[1],
+                    xerr=exp[3],
+                    yerr=exp[2],
+                    fmt='.',
+                    markersize=0.75,
+                    linewidth=0.5)
+
+        ax.plot(q, r, label=l)
+
     ax.set_yscale('log')
     plt.legend()
-    
+
     plt.xlabel("Q [nm${}^{-1}$]")
     plt.ylabel("R")
-    
+
     plt.tight_layout()
     plt.savefig(filename)
 
@@ -166,27 +164,29 @@ def plotSpinAsymmetry(data_pp, data_mm, q, r_pp, r_mm, filename):
     Plot the simulated spin asymmetry as well its 
     experimental counterpart with errorbars
     """
-    
+
     # compute the errorbars of the spin asymmetry
     delta = numpy.sqrt(4 * (data_pp[1]**2 * data_mm[2]**2 + \
-            data_mm[1]**2 * data_pp[2]**2 ) / 
+            data_mm[1]**2 * data_pp[2]**2 ) /
                 ( data_pp[1] + data_mm[1] )**4 )
-    
+
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    
-    ax.errorbar( data_pp[0], 
-                (data_pp[1] - data_mm[1])/(data_pp[1] + data_mm[1]),
-                  xerr = data_pp[3], yerr = delta,
-                    fmt='.', markersize=0.75, linewidth=0.5 )
-    
+
+    ax.errorbar(data_pp[0], (data_pp[1] - data_mm[1])/(data_pp[1] + data_mm[1]),
+                xerr=data_pp[3],
+                yerr=delta,
+                fmt='.',
+                markersize=0.75,
+                linewidth=0.5)
+
     ax.plot(q, (r_pp - r_mm)/(r_pp + r_mm))
-    
+
     plt.gca().set_ylim((-0.3, 0.5))
-    
+
     plt.xlabel("Q [nm${}^{-1}$]")
     plt.ylabel("Spin asymmetry")
-    
+
     plt.tight_layout()
     plt.savefig(filename)
 
@@ -202,63 +202,59 @@ def normalizeData(data):
     normalizes it such that the maximum of the reflectivity is
     unity and rescales the q-axis to inverse nm
     """
-    
-    r0 = numpy.where( data[0] - numpy.roll(data[0], 1) == 0)
+
+    r0 = numpy.where(data[0] - numpy.roll(data[0], 1) == 0)
     data = numpy.delete(data, r0, 1)
-    
-    data[0] = data[0] / angstrom
-    data[3] = data[3] / angstrom
-    
-    norm = numpy.max( data[1] )
-    data[1]  = data[1] / norm
-    data[2]  = data[2] / norm
-    
-    so = numpy.argsort( data[0] )
-    data = data[:,so]
-    
+
+    data[0] = data[0]/angstrom
+    data[3] = data[3]/angstrom
+
+    norm = numpy.max(data[1])
+    data[1] = data[1]/norm
+    data[2] = data[2]/norm
+
+    so = numpy.argsort(data[0])
+    data = data[:, so]
+
     return data
 
+
 def filterData(data, qmin, qmax):
-    minIndex = numpy.argmin( numpy.abs( data[0] - qmin ) )
-    maxIndex = numpy.argmin( numpy.abs( data[0] - qmax ) )
-    
-    return data[:,minIndex:maxIndex+1]
+    minIndex = numpy.argmin(numpy.abs(data[0] - qmin))
+    maxIndex = numpy.argmin(numpy.abs(data[0] - qmax))
+
+    return data[:, minIndex:maxIndex + 1]
+
 
 def get_Experimental_data(qmin, qmax):
     input_Data = downloadAndExtractData()
     data_pp = normalizeData(input_Data[0])
     data_mm = normalizeData(input_Data[1])
-    
-    return ( filterData( data_pp, qmin, qmax) , 
-             filterData( data_mm, qmin, qmax) )
+
+    return (filterData(data_pp, qmin, qmax), filterData(data_mm, qmin, qmax))
+
 
 def downloadAndExtractData():
     url = "https://www.nist.gov/document/spinelfilmzip"
-    
+
     if not isfile("spinelfilm.zip"):
-      downloadfile = urlopen(url)
-      with open("spinelfilm.zip", 'wb') as outfile:
-        outfile.write(downloadfile.read())
-        
+        downloadfile = urlopen(url)
+        with open("spinelfilm.zip", 'wb') as outfile:
+            outfile.write(downloadfile.read())
+
     zipfile = ZipFile("spinelfilm.zip")
-      
+
     rawdata = zipfile.open("MAFO_Saturated.refl").read().decode("utf-8")
-    
-    table_pp = match(
-          r'.*# "polarization": "\+\+"\n#.*?\n# "units".*?\n(.*?)#.*', 
-          rawdata, DOTALL).group(1)
-    table_mm = match(
-          r'.*# "polarization": "\-\-"\n#.*?\n# "units".*?\n(.*?)#.*', 
-          rawdata, DOTALL).group(1)
-    
-    data_pp = numpy.genfromtxt(BytesIO(table_pp.encode()), 
-                               unpack=True)
-    data_mm = numpy.genfromtxt(BytesIO(table_mm.encode()), 
-                               unpack=True)
-    
+
+    table_pp = match(r'.*# "polarization": "\+\+"\n#.*?\n# "units".*?\n(.*?)#.*',
+                     rawdata, DOTALL).group(1)
+    table_mm = match(r'.*# "polarization": "\-\-"\n#.*?\n# "units".*?\n(.*?)#.*',
+                     rawdata, DOTALL).group(1)
+
+    data_pp = numpy.genfromtxt(BytesIO(table_pp.encode()), unpack=True)
+    data_mm = numpy.genfromtxt(BytesIO(table_mm.encode()), unpack=True)
+
     return (data_pp, data_mm)
-
-
 
 
 ####################################################################
@@ -267,121 +263,108 @@ def downloadAndExtractData():
 
 
 def run_fit_ba(q_axis, r_data, r_uncertainty, simulationFactory, startParams):
-    
+
     fit_objective = ba.FitObjective()
     fit_objective.setObjectiveMetric("chi2")
-  
-    fit_objective.addSimulationAndData( 
-        lambda params: simulationFactory[0](q_axis[0], params), 
-        r_data[0], r_uncertainty[0], 1.0)
-    fit_objective.addSimulationAndData( 
-        lambda params: simulationFactory[1](q_axis[1], params), 
-        r_data[1], r_uncertainty[1], 1.0)
+
+    fit_objective.addSimulationAndData(
+        lambda params: simulationFactory[0](q_axis[0], params), r_data[0],
+        r_uncertainty[0], 1.0)
+    fit_objective.addSimulationAndData(
+        lambda params: simulationFactory[1](q_axis[1], params), r_data[1],
+        r_uncertainty[1], 1.0)
 
     fit_objective.initPrint(10)
-    
+
     params = ba.Parameters()
     for name, p in startParams.items():
         params.add(name, p[0], min=p[1], max=p[2])
-    
+
     minimizer = ba.Minimizer()
-    
+
     result = minimizer.minimize(fit_objective.evaluate, params)
     fit_objective.finalize(result)
-        
-    return  { r.name():r.value for r in result.parameters() }
 
-
-
+    return {r.name(): r.value for r in result.parameters()}
 
 
 ####################################################################
 #                          Main Function                           #
 ####################################################################
 
-
 if __name__ == '__main__':
 
     if len(argv) > 1 and argv[1] == "fit":
         fixedParams = {
-                    # parameters can be moved here to keep them fixed
-                    }
-        fixedParams = { d:v[0] for d, v in fixedParams.items() }
+            # parameters can be moved here to keep them fixed
+        }
+        fixedParams = {d: v[0] for d, v in fixedParams.items()}
 
         startParams = {
-                    # own starting values
-                    "q_res":(0.0, 0, 0.1),
-                    "q_offset":(0, -0.002, 0.002),
-                      
-                    "rho_Mafo":(6.3649, 2, 7),
-                    "rhoM_Mafo":(0, 0, 2),
-                    "t_Mafo":(150, 60, 180),
-                    
-                    "r_Mao":( 1, 0, 12),
-                    "r_Mafo":(1, 0, 12),
-                    }
+            # own starting values
+            "q_res": (0.0, 0, 0.1),
+            "q_offset": (0, -0.002, 0.002),
+            "rho_Mafo": (6.3649, 2, 7),
+            "rhoM_Mafo": (0, 0, 2),
+            "t_Mafo": (150, 60, 180),
+            "r_Mao": (1, 0, 12),
+            "r_Mafo": (1, 0, 12),
+        }
         fit = True
-        
+
     else:
         startParams = {}
         fixedParams = {
-                      # parameters from our own fit run
-                      'q_res': 0.010542945012551425, 
-                      'q_offset': 7.971243487467318e-05, 
-                      
-                      'rho_Mafo': 6.370140108715461, 
-                      'rhoM_Mafo': 0.27399566816062926, 
-                      't_Mafo': 137.46913056084736, 
-                      
-                      'r_Mao': 8.60487712674644, 
-                      'r_Mafo': 3.7844265311293483
-                      }
-        
+            # parameters from our own fit run
+            'q_res': 0.010542945012551425,
+            'q_offset': 7.971243487467318e-05,
+            'rho_Mafo': 6.370140108715461,
+            'rhoM_Mafo': 0.27399566816062926,
+            't_Mafo': 137.46913056084736,
+            'r_Mao': 8.60487712674644,
+            'r_Mafo': 3.7844265311293483
+        }
+
         fit = False
 
-  
-    paramsInitial = {d:v[0] for d, v in startParams.items()}
+    paramsInitial = {d: v[0] for d, v in startParams.items()}
 
-    
-    def run_Simulation_pp( qzs, params ):
-        return run_simulation(qzs, params,                                 
-                        polarization = ba.kvector_t(0, 1, 0),
-                        analyzer     = ba.kvector_t(0, 1, 0))
+    def run_Simulation_pp(qzs, params):
+        return run_simulation(qzs,
+                              params,
+                              polarization=ba.kvector_t(0, 1, 0),
+                              analyzer=ba.kvector_t(0, 1, 0))
 
-    def run_Simulation_mm( qzs, params ):
-        return run_simulation(qzs, params,                                 
-                        polarization = ba.kvector_t(0, -1, 0),
-                        analyzer     = ba.kvector_t(0, -1, 0))
+    def run_Simulation_mm(qzs, params):
+        return run_simulation(qzs,
+                              params,
+                              polarization=ba.kvector_t(0, -1, 0),
+                              analyzer=ba.kvector_t(0, -1, 0))
 
     qzs = numpy.linspace(qmin, qmax, scan_size)
-    q_pp, r_pp = qr( run_Simulation_pp( qzs, paramsInitial ) )
-    q_mm, r_mm = qr( run_Simulation_mm( qzs, paramsInitial ) )
-    
+    q_pp, r_pp = qr(run_Simulation_pp(qzs, paramsInitial))
+    q_mm, r_mm = qr(run_Simulation_mm(qzs, paramsInitial))
+
     data_pp, data_mm = get_Experimental_data(qmin, qmax)
-    
-    plot([q_pp, q_mm], [r_pp, r_mm], 
-          [data_pp, data_mm], ["$++$", "$--$"], 
-          f'MAFO_Saturated_initial.pdf' )
-    
-    plotSpinAsymmetry(data_pp, data_mm, qzs, r_pp, r_mm, 
+
+    plot([q_pp, q_mm], [r_pp, r_mm], [data_pp, data_mm], ["$++$", "$--$"],
+         f'MAFO_Saturated_initial.pdf')
+
+    plotSpinAsymmetry(data_pp, data_mm, qzs, r_pp, r_mm,
                       "MAFO_Saturated_spin_asymmetry_initial.pdf")
-    
+
     if fit:
-        fitResult = run_fit_ba([data_pp[0], data_mm[0]], 
-                               [data_pp[1], data_mm[1]], 
-                               [data_pp[2], data_mm[2]], 
-                [run_Simulation_pp, run_Simulation_mm], startParams)
+        fitResult = run_fit_ba([data_pp[0], data_mm[0]], [data_pp[1], data_mm[1]],
+                               [data_pp[2], data_mm[2]],
+                               [run_Simulation_pp, run_Simulation_mm], startParams)
         print("Fit Result:")
         print(fitResult)
-        
-        q_pp, r_pp = qr( run_Simulation_pp( qzs, fitResult ) )
-        q_mm, r_mm = qr( run_Simulation_mm( qzs, fitResult ) )
-        
-        plot([q_pp, q_mm], [r_pp, r_mm], 
-              [data_pp, data_mm], ["$++$", "$--$"], 
-              f'MAFO_Saturated_fit.pdf' )
-    
-        plotSpinAsymmetry(data_pp, data_mm, qzs, r_pp, r_mm, 
+
+        q_pp, r_pp = qr(run_Simulation_pp(qzs, fitResult))
+        q_mm, r_mm = qr(run_Simulation_mm(qzs, fitResult))
+
+        plot([q_pp, q_mm], [r_pp, r_mm], [data_pp, data_mm], ["$++$", "$--$"],
+             f'MAFO_Saturated_fit.pdf')
+
+        plotSpinAsymmetry(data_pp, data_mm, qzs, r_pp, r_mm,
                           "MAFO_Saturated_spin_asymmetry_fit.pdf")
-    
-    
--- Examples/Python/fit56_SpecularAdvanced/Pt_layer_fit.py
+++ Examples/Python/fit56_SpecularAdvanced/Pt_layer_fit.py
@@ -11,7 +11,6 @@ from sys import argv
 import bornagain as ba
 from bornagain import angstrom
 
-
 # filename of the experimental data to be loaded
 filename = 'RvsQ_36563_36662.txt.gz'
 
@@ -26,10 +25,10 @@ scan_size = 1500
 sldPt = (6.3568e-06, 1.8967e-09)
 sldSi = (2.0728e-06, 2.3747e-11)
 
-
 ####################################################################
 #                  Create Sample and Simulation                    #
 ####################################################################
+
 
 def get_sample(params):
 
@@ -43,46 +42,46 @@ def get_sample(params):
 
     r_si = ba.LayerRoughness()
     r_si.setSigma(params["r_si/nm"])
-    
+
     r_pt = ba.LayerRoughness()
     r_pt.setSigma(params["r_pt/nm"])
-    
+
     multi_layer = ba.MultiLayer()
     multi_layer.addLayer(ambient_layer)
     multi_layer.addLayerWithTopRoughness(layer, r_pt)
-        
+
     multi_layer.addLayerWithTopRoughness(substrate_layer, r_si)
-    
+
     return multi_layer
 
 
 def get_simulation(q_axis, parameters):
-    
+
     q_axis = q_axis + parameters["q_offset"]
     scan = ba.QSpecScan(q_axis)
-    
+
     n_sig = 4.0
     n_samples = 25
 
     distr = ba.RangedDistributionGaussian(n_samples, n_sig)
     scan.setAbsoluteQResolution(distr, parameters["q_res/q"])
-    
+
     simulation = ba.SpecularSimulation()
     simulation.setBeamIntensity(parameters["intensity"])
-    
+
     simulation.setScan(scan)
     return simulation
 
 
 def run_simulation(q_axis, fitParams):
     parameters = dict(fitParams, **fixedParams)
-    
+
     sample = get_sample(parameters)
     simulation = get_simulation(q_axis, parameters)
 
     simulation.setSample(sample)
     simulation.runSimulation()
-    return simulation #.result()
+    return simulation  #.result()
 
 
 def qr(result, q_offset=0):
@@ -92,7 +91,7 @@ def qr(result, q_offset=0):
     """
     q = numpy.array(result.result().axis(ba.Axes.QSPACE)) - q_offset
     r = numpy.array(result.result().array(ba.Axes.QSPACE))
-    
+
     return q, r
 
 
@@ -100,30 +99,37 @@ def qr(result, q_offset=0):
 #                         Plot Handling                            #
 ####################################################################
 
+
 def plot(q, r, exp, filename, params=None):
     """
     helper function to plot a result
     """
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    
-    ax.errorbar( exp[0], exp[1], xerr=exp[3], yerr=exp[2], label="R", 
-                fmt='.', markersize=1., linewidth=0.6, color='r' )
-    
+
+    ax.errorbar(exp[0],
+                exp[1],
+                xerr=exp[3],
+                yerr=exp[2],
+                label="R",
+                fmt='.',
+                markersize=1.,
+                linewidth=0.6,
+                color='r')
+
     ax.plot(q, r, label="Simulation", color='C0', linewidth=0.5)
-    
+
     ax.set_yscale('log')
-    
+
     ax.set_xlabel("Q [nm$^{^-1}$]")
     ax.set_ylabel("R")
-    
+
     y = 0.5
     if params is not None:
-      for n, v in params.items():
-        plt.text(0.7, y, f"{n} = {v:.3g}", transform=ax.transAxes)
-        y += 0.05
-                 
-    
+        for n, v in params.items():
+            plt.text(0.7, y, f"{n} = {v:.3g}", transform=ax.transAxes)
+            y += 0.05
+
     plt.tight_layout()
     plt.savefig(filename)
 
@@ -132,59 +138,58 @@ def plot(q, r, exp, filename, params=None):
 #                          Data Handling                           #
 ####################################################################
 
+
 def get_Experimental_data(qmin, qmax):
     """
     read experimental data, remove some duplicate q-values
     recalculate q axis to inverse nm
     """
     data = numpy.genfromtxt(filename, unpack=True)
-    
-    r0 = numpy.where( data[0] - numpy.roll(data[0], 1) == 0)
+
+    r0 = numpy.where(data[0] - numpy.roll(data[0], 1) == 0)
     data = numpy.delete(data, r0, 1)
-    
-    data[0] = data[0] / angstrom
-    data[3] = data[3] / angstrom
-    
-    data[1]  = data[1]
-    data[2]  = data[2]
-    
-    so = numpy.argsort( data[0] )
-    
-    data = data[:,so]
-    
-    minIndex = numpy.argmin( numpy.abs( data[0] - qmin ) )
-    maxIndex = numpy.argmin( numpy.abs( data[0] - qmax ) )
-    
-    return data[:,minIndex:maxIndex+1]
+
+    data[0] = data[0]/angstrom
+    data[3] = data[3]/angstrom
+
+    data[1] = data[1]
+    data[2] = data[2]
+
+    so = numpy.argsort(data[0])
+
+    data = data[:, so]
+
+    minIndex = numpy.argmin(numpy.abs(data[0] - qmin))
+    maxIndex = numpy.argmin(numpy.abs(data[0] - qmax))
+
+    return data[:, minIndex:maxIndex + 1]
 
 
 ####################################################################
 #                          Fit Function                            #
 ####################################################################
 
-def run_fit_ba(q_axis, r_data, r_uncertainty, 
-               simulationFactory, startParams):
-    
+
+def run_fit_ba(q_axis, r_data, r_uncertainty, simulationFactory, startParams):
+
     fit_objective = ba.FitObjective()
     fit_objective.setObjectiveMetric("chi2")
-  
-    fit_objective.addSimulationAndData( 
-        lambda params: simulationFactory(q_axis, params), 
-        r_data, r_uncertainty, 1.0)
-    
+
+    fit_objective.addSimulationAndData(
+        lambda params: simulationFactory(q_axis, params), r_data, r_uncertainty, 1.0)
+
     fit_objective.initPrint(10)
-    
+
     params = ba.Parameters()
     for name, p in startParams.items():
         params.add(name, p[0], min=p[1], max=p[2])
-    
+
     minimizer = ba.Minimizer()
-    
+
     result = minimizer.minimize(fit_objective.evaluate, params)
     fit_objective.finalize(result)
-        
-    return  { r.name():r.value for r in result.parameters() }
 
+    return {r.name(): r.value for r in result.parameters()}
 
 
 ####################################################################
@@ -195,54 +200,49 @@ if __name__ == '__main__':
 
     if len(argv) > 1 and argv[1] == "fit":
         fixedParams = {
-                      # parameters can be moved here to keep them fixed
-                      }
-        fixedParams = { d:v[0] for d, v in fixedParams.items() }
+            # parameters can be moved here to keep them fixed
+        }
+        fixedParams = {d: v[0] for d, v in fixedParams.items()}
 
         startParams = {
-                        # own starting values
-                        "q_offset":(0.0, -0.02, 0.02),
-                        "q_res/q":(0.0, 0, 0.02),
-                        "t_pt/nm":(53, 40, 60),
-                        "r_si/nm":( 1.22, 0, 5),
-                        "r_pt/nm":( 0.25, 0, 5),
-                        "intensity":(1, 0, 2),
-                      }
+            # own starting values
+            "q_offset": (0.0, -0.02, 0.02),
+            "q_res/q": (0.0, 0, 0.02),
+            "t_pt/nm": (53, 40, 60),
+            "r_si/nm": (1.22, 0, 5),
+            "r_pt/nm": (0.25, 0, 5),
+            "intensity": (1, 0, 2),
+        }
         fit = True
-        
+
     else:
         startParams = {}
         fixedParams = {
-                      # parameters from our own fit run
-                      'q_offset': 0.015085985992837999, 
-                      'q_res/q': 0.010156450689003465, 
-                      't_pt/nm': 48.564838355355405, 
-                      'r_si/nm': 1.2857515425763575, 
-                      'r_pt/nm': 0.2868252673771518, 
-                      'intensity': 1.3156374978332654
-                      }
+            # parameters from our own fit run
+            'q_offset': 0.015085985992837999,
+            'q_res/q': 0.010156450689003465,
+            't_pt/nm': 48.564838355355405,
+            'r_si/nm': 1.2857515425763575,
+            'r_pt/nm': 0.2868252673771518,
+            'intensity': 1.3156374978332654
+        }
         fit = False
 
-  
-    paramsInitial = {d:v[0] for d, v in startParams.items()}
+    paramsInitial = {d: v[0] for d, v in startParams.items()}
 
-    qzs  = numpy.linspace(qmin, qmax, scan_size)
-    q, r = qr( run_simulation(qzs, paramsInitial), 
-              dict(paramsInitial, **fixedParams)["q_offset"] )
+    qzs = numpy.linspace(qmin, qmax, scan_size)
+    q, r = qr(run_simulation(qzs, paramsInitial),
+              dict(paramsInitial, **fixedParams)["q_offset"])
     data = get_Experimental_data(qmin, qmax)
-    
-    plot(q, r, data, f'PtLayerFit_initial.pdf', 
-                  dict(paramsInitial, **fixedParams))
-    
+
+    plot(q, r, data, f'PtLayerFit_initial.pdf', dict(paramsInitial, **fixedParams))
+
     if fit:
-        fitResult = run_fit_ba(data[0], data[1], data[2], 
-                                run_simulation, startParams)
-        
+        fitResult = run_fit_ba(data[0], data[1], data[2], run_simulation,
+                               startParams)
+
         print("Fit Result:")
         print(fitResult)
-        
-        q, r = qr( run_simulation(qzs, fitParams=fitResult), 
-                    fitResult["q_offset"] )
-        plot(q, r, data, f'PtLayerFit_fit.pdf', 
-                    dict(fitResult, **fixedParams))
-  
+
+        q, r = qr(run_simulation(qzs, fitParams=fitResult), fitResult["q_offset"])
+        plot(q, r, data, f'PtLayerFit_fit.pdf', dict(fitResult, **fixedParams))

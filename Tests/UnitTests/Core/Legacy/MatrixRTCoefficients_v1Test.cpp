5e0c296ac1b3ab519bb3dceaedb6a1e975a2ff55
Randolf Beerwerth <r.beerwerth@fz-juelich.de>
2020-11-30 16:17:04 +0100

    Rename MatrixRTCoefficientsTest MatrixRTCoefficients_v1Test

--- Tests/UnitTests/Core/Legacy/MatrixRTCoefficients_v1Test.cpp
+++ Tests/UnitTests/Core/Legacy/MatrixRTCoefficients_v1Test.cpp
@@ -1,60 +1,60 @@
 #include "Tests/UnitTests/Core/Legacy/MatrixRTCoefficients_v1.h"
 #include "Tests/GTestWrapper/google_test.h"
 
-class MatrixRTCoefficientsTest : public ::testing::Test {
+class MatrixRTCoefficients_v1Test : public ::testing::Test {
 protected:
     MatrixRTCoefficients_v1 mrtcDefault;
 };
 
-TEST_F(MatrixRTCoefficientsTest, T1plus) {
+TEST_F(MatrixRTCoefficients_v1Test, T1plus) {
     Eigen::Vector2cd vector = mrtcDefault.T1plus();
     EXPECT_EQ(complex_t(0.5, 0.0), vector(0));
     EXPECT_EQ(0.0, vector(1));
 }
 
-TEST_F(MatrixRTCoefficientsTest, T1min) {
+TEST_F(MatrixRTCoefficients_v1Test, T1min) {
     Eigen::Vector2cd vector = mrtcDefault.T1min();
     EXPECT_EQ(0.0, vector(0));
     EXPECT_EQ(complex_t(0.5, 0.0), vector(1));
 }
 
-TEST_F(MatrixRTCoefficientsTest, T2plus) {
+TEST_F(MatrixRTCoefficients_v1Test, T2plus) {
     Eigen::Vector2cd vector = mrtcDefault.T2plus();
     EXPECT_EQ(complex_t(0.5, 0.0), vector(0));
     EXPECT_EQ(0.0, vector(1));
 }
 
-TEST_F(MatrixRTCoefficientsTest, T2min) {
+TEST_F(MatrixRTCoefficients_v1Test, T2min) {
     Eigen::Vector2cd vector = mrtcDefault.T2min();
     EXPECT_EQ(0.0, vector(0));
     EXPECT_EQ(complex_t(0.5, 0.0), vector(1));
 }
 
-TEST_F(MatrixRTCoefficientsTest, R1plus) {
+TEST_F(MatrixRTCoefficients_v1Test, R1plus) {
     Eigen::Vector2cd vector = mrtcDefault.R1plus();
     EXPECT_EQ(complex_t(-0.5, 0.0), vector(0));
     EXPECT_EQ(0.0, vector(1));
 }
 
-TEST_F(MatrixRTCoefficientsTest, R1min) {
+TEST_F(MatrixRTCoefficients_v1Test, R1min) {
     Eigen::Vector2cd vector = mrtcDefault.R1min();
     EXPECT_EQ(0.0, vector(0));
     EXPECT_EQ(complex_t(-0.5, 0.0), vector(1));
 }
 
-TEST_F(MatrixRTCoefficientsTest, R2plus) {
+TEST_F(MatrixRTCoefficients_v1Test, R2plus) {
     Eigen::Vector2cd vector = mrtcDefault.R2plus();
     EXPECT_EQ(complex_t(-0.5, 0.0), vector(0));
     EXPECT_EQ(0.0, vector(1));
 }
 
-TEST_F(MatrixRTCoefficientsTest, R2min) {
+TEST_F(MatrixRTCoefficients_v1Test, R2min) {
     Eigen::Vector2cd vector = mrtcDefault.R2min();
     EXPECT_EQ(0.0, vector(0));
     EXPECT_EQ(complex_t(-0.5, 0.0), vector(1));
 }
 
-TEST_F(MatrixRTCoefficientsTest, getKz) {
+TEST_F(MatrixRTCoefficients_v1Test, getKz) {
     Eigen::Vector2cd vector = mrtcDefault.getKz();
     EXPECT_EQ(complex_t(0.0, 0.0), vector(0));
     EXPECT_EQ(complex_t(0.0, 0.0), vector(1));

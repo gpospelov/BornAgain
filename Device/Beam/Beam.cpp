9a7a376ae4d15f8bb6bc9ed321b8c3d335a79b93
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2020-12-07 13:12:55 +0100

    corr autoformatting of *.h.in file

--- Device/Beam/Beam.cpp
+++ Device/Beam/Beam.cpp
@@ -71,7 +71,7 @@ Beam& Beam::operator=(const Beam& other) {
 Beam::~Beam() = default;
 
 kvector_t Beam::getCentralK() const {
-    return M_TWOPI / _lambda * Direction(-alpha(), phi()).vector();
+    return M_TWOPI / m_wavelength * Direction(-direction().alpha(), direction().phi()).vector();
 }
 
 void Beam::setWavelength(double wavelength) {

bac6678af2a7313b8e7c8602dadfe91c622a47b6
Gennady Pospelov <g.pospelov@fz-juelich.de>
2020-12-15 22:11:25 +0100

    Copy qt-mvvm library

---
+++ ThirdParty/GUI/qt-mvvm/tests/libtestmachinery/google_test.h
@@ -0,0 +1,17 @@
+// ************************************************************************** //
+//
+//  Model-view-view-model framework for large GUI applications
+//
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @authors   see AUTHORS
+//
+// ************************************************************************** //
+
+#ifndef GOOGLETEST_H
+#define GOOGLETEST_H
+
+#include <gtest/gtest.h>
+#include <mvvm/model/customvariants.h>
+#include <mvvm/model/comparators.h>
+
+#endif
---
+++ ThirdParty/GUI/qt-mvvm/tests/testview/TestAll.cpp
@@ -0,0 +1,31 @@
+// ************************************************************************** //
+//
+//  Model-view-view-model framework for large GUI applications
+//
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @authors   see AUTHORS
+//
+// ************************************************************************** //
+
+#include "customplot_test_utils.h"
+#include "google_test.h"
+#include "qcustomplot.h"
+#include <QApplication>
+#include <QStandardItem>
+#include <gmock/gmock.h>
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    ::testing::InitGoogleMock(&argc, argv);
+
+    ModelView::Comparators::registerComparators();
+    qRegisterMetaType<QStandardItem*>("QStandardItem*");
+    qRegisterMetaType<QCPRange>("QCPRange");
+
+    // FIXME find the way not to run app for all tests which doesn't use QWidget.
+    // The problem here is because of ctest autodiscovery which runs given main at every test.
+    QApplication app(argc, argv);
+    Q_UNUSED(app)
+
+    return RUN_ALL_TESTS();
+}

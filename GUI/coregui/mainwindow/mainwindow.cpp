fc825c69bc1dd9b0e4f0bba833bf769fd4c7173c
Gennady Pospelov <gennady.pospelov@gmail.com>
2020-12-22 14:15:04 +0100

    More accurate suppression of Qt deprecated warning

--- GUI/coregui/mainwindow/mainwindow.cpp
+++ GUI/coregui/mainwindow/mainwindow.cpp
@@ -81,7 +81,12 @@ MainWindow::MainWindow()
     fillerButton->setEnabled(false);
     m_viewSelectionButtonsLayout->insertWidget(-1, fillerButton);
 
+#  if QT_VERSION >= QT_VERSION_CHECK(5, 15, 0)
     connect(m_viewSelectionButtons, &QButtonGroup::idClicked, this, &MainWindow::setCurrentView);
+#  else
+    connect(m_viewSelectionButtons, QOverload<int>::of(&QButtonGroup::buttonClicked), this,
+            &MainWindow::setCurrentView);
+#  endif
 
     m_statusBar->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Fixed);
 

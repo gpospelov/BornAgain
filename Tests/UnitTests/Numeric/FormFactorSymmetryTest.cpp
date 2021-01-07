f56f14b8d1c2974ea1e06473183d026260143a41
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2021-01-07 09:39:28 +0100

    cone6_steep: relax limit for Mac

--- Tests/UnitTests/Numeric/FormFactorSymmetryTest.cpp
+++ Tests/UnitTests/Numeric/FormFactorSymmetryTest.cpp
@@ -85,7 +85,7 @@ TEST_F(FFSymmetryTest, Cone6_steep)
 {
     FormFactorCone6 p(.23, 3.5, .999 * M_PI / 2);
     run_test(
-        &p, [](const cvector_t& q) -> cvector_t { return q.rotatedZ(-M_PI / 3); }, 8e-12, 1e-99,
+        &p, [](const cvector_t& q) -> cvector_t { return q.rotatedZ(-M_PI / 3); }, 2e-11, 1e-99,
         50);
 }
 
